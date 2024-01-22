3ac745d9f0d0507920554301edbca9be316ca32a
Ryan Schumacher <jschumacher@virtru.com>
2024-01-21 22:55:03 -0600

    Add migrate down command

--- internal/db/db_migration.go
+++ internal/db/db_migration.go
@@ -53,3 +53,39 @@ func (c *Client) RunMigrations() (int, error) {
 
 	return applied, nil
 }
+
+func (c *Client) MigrationDown() (int, error) {
+	var (
+		applied int
+	)
+
+	if !c.config.RunMigrations {
+		slog.Info("skipping migrations",
+			slog.String("reason", "runMigrations is false"),
+			slog.Bool("runMigrations", c.config.RunMigrations))
+		return applied, nil
+	}
+
+	pool, ok := c.PgxIface.(*pgxpool.Pool)
+	if !ok || pool == nil {
+		return applied, fmt.Errorf("failed to cast pgxpool.Pool")
+	}
+
+	conn := stdlib.OpenDBFromPool(pool)
+	defer conn.Close()
+
+	provider, err := goose.NewProvider(goose.DialectPostgres, conn, migrations.MigrationsFS)
+	if err != nil {
+		return applied, errors.Join(fmt.Errorf("failed to create goose provider"), err)
+	}
+
+	res, err := provider.Down(context.Background())
+	if err != nil {
+		return applied, errors.Join(fmt.Errorf("failed to run migrations"), err)
+	}
+	if res.Error != nil {
+		return applied, errors.Join(fmt.Errorf("failed to run migrations"), err)
+	}
+
+	return applied, nil
+}
--- migrations/20240118000000_create_new_tables.sql
+++ migrations/20240118000000_create_new_tables.sql
@@ -7,13 +7,14 @@ CREATE TYPE subject_mappings_operator AS ENUM ('UNSPECIFIED', 'IN', 'NOT_IN');
 
 CREATE TABLE IF NOT EXISTS opentdf.namespaces
 (
-    id UUID PRIMARY KEY,
+    -- generate on create
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS opentdf.attribute_definitions
 (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     namespace_id UUID NOT NULL REFERENCES opentdf.namespaces(id),
     name VARCHAR NOT NULL,
     rule attribute_definition_rule NOT NULL,
@@ -23,7 +24,7 @@ CREATE TABLE IF NOT EXISTS opentdf.attribute_definitions
 
 CREATE TABLE IF NOT EXISTS opentdf.attribute_values
 (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     attribute_definition_id UUID NOT NULL REFERENCES opentdf.attribute_definitions(id),
     value VARCHAR NOT NULL,
     members UUID[] NOT NULL,
@@ -33,7 +34,7 @@ CREATE TABLE IF NOT EXISTS opentdf.attribute_values
 
 CREATE TABLE IF NOT EXISTS opentdf.key_access_servers
 (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     key_access_server VARCHAR NOT NULL UNIQUE,
     public_key VARCHAR NOT NULL,
     metadata JSONB
@@ -55,7 +56,7 @@ CREATE TABLE IF NOT EXISTS opentdf.attribute_value_key_access_grants
 
 CREATE TABLE IF NOT EXISTS opentdf.resource_mappings
 (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     attribute_value_id UUID NOT NULL REFERENCES opentdf.attribute_values(id),
     name VARCHAR NOT NULL,
     terms VARCHAR[],
@@ -64,7 +65,7 @@ CREATE TABLE IF NOT EXISTS opentdf.resource_mappings
 
 CREATE TABLE IF NOT EXISTS opentdf.subject_mappings
 (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     attribute_value_id UUID NOT NULL REFERENCES opentdf.attribute_values(id),
     operator subject_mappings_operator NOT NULL,
     subject_attribute VARCHAR NOT NULL,
@@ -75,15 +76,15 @@ CREATE TABLE IF NOT EXISTS opentdf.subject_mappings
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS opentdf.key_access_servers;
 DROP TABLE IF EXISTS opentdf.subject_mappings;
 DROP TABLE IF EXISTS opentdf.resource_mappings;
 DROP TABLE IF EXISTS opentdf.attribute_value_key_access_grants;
 DROP TABLE IF EXISTS opentdf.attribute_definition_key_access_grants;
+DROP TABLE IF EXISTS opentdf.key_access_servers;
 DROP TABLE IF EXISTS opentdf.attribute_values;
 DROP TABLE IF EXISTS opentdf.attribute_definitions;
 DROP TABLE IF EXISTS opentdf.namespaces;
 
-DELETE TYPE attribute_definition_rule;
-DELETE TYPE subject_mappings_operator;
+DROP TYPE attribute_definition_rule;
+DROP TYPE subject_mappings_operator;
 -- +goose StatementEnd

8d3a6214b4919dbd08acdda775184e8dc4a23848
jakedoublev <jake.vanvorhis@virtru.com>
2024-01-23 08:39:59 -0800

    move all row scanning to db layer

--- internal/db/namespaces.go
+++ internal/db/namespaces.go
@@ -2,27 +2,43 @@ package db
 
 import (
 	"context"
+	"log/slog"
 
 	sq "github.com/Masterminds/squirrel"
-	"github.com/jackc/pgx/v5"
 	"github.com/opentdf/opentdf-v2-poc/sdk/namespaces"
+	"github.com/opentdf/opentdf-v2-poc/services"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-const (
-	NamespacesTable = "namespaces"
-)
+var NamespacesTable = tableName(TableNamespaces)
 
 func getNamespaceSql(id string) (string, []interface{}, error) {
 	return newStatementBuilder().
 		Select("*").
 		From(NamespacesTable).
-		Where(sq.Eq{"id": id}).
+		Where(sq.Eq{tableField(NamespacesTable, "id"): id}).
 		ToSql()
 }
 
-func (c Client) GetNamespace(ctx context.Context, id string) (pgx.Row, error) {
+func (c Client) GetNamespace(ctx context.Context, id string) (*namespaces.Namespace, error) {
 	sql, args, err := getNamespaceSql(id)
-	return c.queryRow(ctx, sql, args, err)
+	if err != nil {
+		slog.Error(services.ErrGettingResource, slog.String("error", err.Error()))
+		return nil, err
+	}
+	row, err := c.queryRow(ctx, sql, args, err)
+	if err != nil {
+		slog.Error(services.ErrGettingResource, slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	var namespace *namespaces.Namespace
+	if err := row.Scan(&namespace.Id, &namespace.Name); err != nil {
+		slog.Error(services.ErrGettingResource, slog.String("error", err.Error()))
+		return nil, status.Error(codes.Internal, services.ErrGettingResource)
+	}
+	return namespace, nil
 }
 
 func listNamespacesSql() (string, []interface{}, error) {
@@ -32,9 +48,28 @@ func listNamespacesSql() (string, []interface{}, error) {
 		ToSql()
 }
 
-func (c Client) ListNamespaces(ctx context.Context) (pgx.Rows, error) {
+func (c Client) ListNamespaces(ctx context.Context) ([]*namespaces.Namespace, error) {
+	namespacesList := []*namespaces.Namespace{}
+
 	sql, args, err := listNamespacesSql()
-	return c.query(ctx, sql, args, err)
+	if err != nil {
+		slog.Error(services.ErrListingResource, slog.String("error", err.Error()))
+		return nil, err
+	}
+	rows, err := c.query(ctx, sql, args, err)
+	if err != nil {
+		slog.Error(services.ErrListingResource, slog.String("error", err.Error()))
+		return nil, err
+	}
+	for rows.Next() {
+		var namespace namespaces.Namespace
+		if err := rows.Scan(&namespace.Id, &namespace.Name); err != nil {
+			slog.Error(services.ErrListingResource, slog.String("error", err.Error()))
+			return nil, err
+		}
+		namespacesList = append(namespacesList, &namespace)
+	}
+	return namespacesList, nil
 }
 
 func createNamespaceSql(namespace *namespaces.Namespace) (string, []interface{}, error) {
@@ -42,26 +77,30 @@ func createNamespaceSql(namespace *namespaces.Namespace) (string, []interface{},
 		Insert(NamespacesTable).
 		Columns("name").
 		Values(namespace.Name).
+		Suffix("RETURNING \"id\"").
 		ToSql()
 }
 
-func (c Client) CreateNamespace(ctx context.Context, namespace *namespaces.Namespace) (pgx.Row, error) {
+func (c Client) CreateNamespace(ctx context.Context, namespace *namespaces.Namespace) (string, error) {
 	sql, args, err := createNamespaceSql(namespace)
-	if e := c.exec(ctx, sql, args, err); e != nil {
-		return nil, e
+	var id string
+	if r, e := c.queryRow(ctx, sql, args, err); e != nil {
+		return "", e
+	} else if e := r.Scan(&id); e != nil {
+		return "", e
 	}
-	return c.GetNamespace(ctx, namespace.Id)
+	return id, nil
 }
 
 func updateNamespaceSql(namespace *namespaces.Namespace) (string, []interface{}, error) {
 	return newStatementBuilder().
 		Update(NamespacesTable).
 		Set("name", namespace.Name).
-		Where(sq.Eq{"id": namespace.Id}).
+		Where(sq.Eq{tableField(NamespacesTable, "id"): namespace.Id}).
 		ToSql()
 }
 
-func (c Client) UpdateNamespace(ctx context.Context, namespace *namespaces.Namespace) (pgx.Row, error) {
+func (c Client) UpdateNamespace(ctx context.Context, namespace *namespaces.Namespace) (*namespaces.Namespace, error) {
 	sql, args, err := updateNamespaceSql(namespace)
 	if e := c.exec(ctx, sql, args, err); e != nil {
 		return nil, e

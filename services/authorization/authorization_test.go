53302b0323dc63b360eb667be217de7c8ec02329
Elizabeth Healy <ehealy@virtru.com>
2024-02-07 15:53:15 -0500

    add test suite stub

---
+++ services/authorization/authorization_test.go
@@ -0,0 +1,32 @@
+package authorization
+
+import (
+	"log/slog"
+
+	"github.com/opentdf/opentdf-v2-poc/internal/db"
+	"github.com/pashagolub/pgxmock/v3"
+	"github.com/stretchr/testify/suite"
+)
+
+type AuthorizationSuite struct {
+	suite.Suite
+	mock            pgxmock.PgxPoolIface
+	authorizationServer *AuthorizationService
+}
+
+func (suite *AuthorizationSuite) SetupTest() {
+	mock, err := pgxmock.NewPool()
+	if err != nil {
+		slog.Error("failed to create mock database connection", slog.String("error", err.Error()))
+	}
+	suite.mock = mock
+	suite.authorizationServer = &AuthorizationService{}
+}
+
+func (suite *AuthorizationSuite) TearDownSuite() {
+	suite.mock.Close()
+}
+
+func TestAuthorizationSuite(t *testing.T) {
+	suite.Run(t, new(AuthorizationSuite))
+}

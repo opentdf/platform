ed181cae82887ad630912a7844e99696e366024b
David Mihalcik <dmihalcik@virtru.com>
2024-02-16 15:57:07 -0500

    Update main_test.go

--- integration/main_test.go
+++ integration/main_test.go
@@ -70,7 +70,7 @@ func TestMain(m *testing.M) {
 	req := tc.GenericContainerRequest{
 		ProviderType: providerType,
 		ContainerRequest: tc.ContainerRequest{
-			Image:        "postgres:15-alpine",
+			Image:        "public.ecr.aws/docker/library/postgres:15-alpine",
 			Name:         "testcontainer-postgres",
 			ExposedPorts: []string{"5432/tcp"},
 

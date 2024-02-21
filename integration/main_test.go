5752db2d1319d1a12fdde4ff0684baf747e192f9
Elizabeth Healy <ehealy@virtru.com>
2024-02-21 11:37:33 -0500

    undo alpine changes

--- integration/main_test.go
+++ integration/main_test.go
@@ -70,7 +70,7 @@ func TestMain(m *testing.M) {
 	req := tc.GenericContainerRequest{
 		ProviderType: providerType,
 		ContainerRequest: tc.ContainerRequest{
-			Image:        "public.ecr.aws/docker/library/postgres:15-alpine3.18",
+			Image:        "public.ecr.aws/docker/library/postgres:15-alpine",
 			Name:         "testcontainer-postgres",
 			ExposedPorts: []string{"5432/tcp"},
 

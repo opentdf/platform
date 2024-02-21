9fb7aa9ef15e1207025ab84ffce84bb274bd64f5
David Mihalcik <dmihalcik@virtru.com>
2024-02-21 09:24:46 -0500

    fix(auth): Implement oauth creds

--- sdk/go.mod
+++ sdk/go.mod
@@ -7,16 +7,20 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.4.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1
+	golang.org/x/oauth2 v0.15.0
 	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17
 	google.golang.org/grpc v1.60.1
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
+	cloud.google.com/go/compute v1.23.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto v0.0.0-20231106174013-bbf56f31fb17 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
 )
--- sdk/options.go
+++ sdk/options.go
@@ -1,8 +1,13 @@
 package sdk
 
 import (
+	"context"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials/oauth"
 )
 
 // Functional option for SDK constructor
@@ -23,13 +28,25 @@ func WithInsecureConn() Option {
 // WithToken returns an Option that sets up authentication with an OAuth2 access token.
 func WithToken(token string) Option {
 	return func(c *options) {
-		c.dialOptions = append(c.dialOptions, grpc.WithPerRPCCredentials(nil))
+		perRPC := oauth.TokenSource{TokenSource: oauth2.StaticTokenSource(&oauth2.Token{
+			AccessToken: token,
+		})}
+		c.dialOptions = append(c.dialOptions, grpc.WithPerRPCCredentials(perRPC))
 	}
 }
 
 // WithClientCredentials returns an Option that sets up authentication with client credentials.
-func WithClientCredentials(clientID, clientSecret string) Option {
+func WithClientCredentials(ctx context.Context, clientID, clientSecret string, scopes []string, tokenUrl string) Option {
+	conf := &clientcredentials.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		Scopes:       scopes,
+		TokenURL:     tokenUrl,
+	}
+	perRPC := oauth.TokenSource{TokenSource: conf.TokenSource(ctx)}
+
 	return func(c *options) {
-		c.dialOptions = append(c.dialOptions, grpc.WithPerRPCCredentials(nil))
+		// TODO Use wire in the token. See https://grpc.io/docs/guides/auth/
+		c.dialOptions = append(c.dialOptions, grpc.WithPerRPCCredentials(perRPC))
 	}
 }

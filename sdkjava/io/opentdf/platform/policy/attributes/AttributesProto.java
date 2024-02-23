573cb269548361679de96d6243971de3ca851ea4
Timothy Tschampel <ttschampel@virtru.com>
2024-02-23 12:20:12 -0800

    exclude 3rd party protos from java generation

--- sdkjava/io/opentdf/platform/policy/attributes/AttributesProto.java
+++ sdkjava/io/opentdf/platform/policy/attributes/AttributesProto.java
@@ -381,9 +381,9 @@ public final class AttributesProto {
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
         new com.google.protobuf.Descriptors.FileDescriptor[] {
-          io.opentdf.platform.buf.validate.ValidateProto.getDescriptor(),
+          build.buf.validate.ValidateProto.getDescriptor(),
           io.opentdf.platform.common.CommonProto.getDescriptor(),
-          io.opentdf.platform.google.api.AnnotationsProto.getDescriptor(),
+          com.google.api.AnnotationsProto.getDescriptor(),
           com.google.protobuf.WrappersProto.getDescriptor(),
           io.opentdf.platform.kasregistry.KeyAccessServerRegistryProto.getDescriptor(),
           io.opentdf.platform.policy.namespaces.NamespacesProto.getDescriptor(),
@@ -594,13 +594,13 @@ public final class AttributesProto {
         new java.lang.String[] { "ValueKeyAccessServer", });
     com.google.protobuf.ExtensionRegistry registry =
         com.google.protobuf.ExtensionRegistry.newInstance();
-    registry.add(io.opentdf.platform.buf.validate.ValidateProto.field);
-    registry.add(io.opentdf.platform.google.api.AnnotationsProto.http);
+    registry.add(build.buf.validate.ValidateProto.field);
+    registry.add(com.google.api.AnnotationsProto.http);
     com.google.protobuf.Descriptors.FileDescriptor
         .internalUpdateFileDescriptor(descriptor, registry);
-    io.opentdf.platform.buf.validate.ValidateProto.getDescriptor();
+    build.buf.validate.ValidateProto.getDescriptor();
     io.opentdf.platform.common.CommonProto.getDescriptor();
-    io.opentdf.platform.google.api.AnnotationsProto.getDescriptor();
+    com.google.api.AnnotationsProto.getDescriptor();
     com.google.protobuf.WrappersProto.getDescriptor();
     io.opentdf.platform.kasregistry.KeyAccessServerRegistryProto.getDescriptor();
     io.opentdf.platform.policy.namespaces.NamespacesProto.getDescriptor();

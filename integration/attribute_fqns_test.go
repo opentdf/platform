4cb3d89fa3273c40f07a0bb38bbbb99d18d300be
jakedoublev <jake.vanvorhis@virtru.com>
2024-02-23 12:26:12 -0500

    add integration tests and update readme with new rpc name

--- integration/attribute_fqns_test.go
+++ integration/attribute_fqns_test.go
@@ -2,10 +2,12 @@ package integration
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"testing"
 
 	"github.com/opentdf/platform/protocol/go/policy/attributes"
+	"github.com/opentdf/platform/services/policy/db"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
@@ -97,23 +99,52 @@ func (s *AttributeFqnSuite) TestCreateAttributeValue() {
 	s.Equal(fqnBuilder(n.Name, a.Name, v.Value), fqn.Fqn)
 }
 
-// Test Get one attribute by fqn
-func (s *AttributeFqnSuite) TestGetAttributeByFqn() {
-	fqnFixture := "example.com/attr/attr1/value/value1"
-	a := fixtures.GetAttributeValueKey(fqnFixture)
-	fqn, err := s.db.PolicyClient.GetAttributeByFqn(s.ctx, fqnFixture)
+// Test Get one attribute by the FQN of one of its values
+func (s *AttributeFqnSuite) TestGetAttributeByFqn_WithAttrValueFqn() {
+	fqnFixtureKey := "example.com/attr/attr1/value/value1"
+	fullFqn := fmt.Sprintf("https://%s", fqnFixtureKey)
+	valueFixture := fixtures.GetAttributeValueKey(fqnFixtureKey)
+
+	attr, err := s.db.PolicyClient.GetAttributeByFqn(s.ctx, fullFqn)
 	s.NoError(err)
-	s.Equal(a.Id, fqn.Id)
-	s.Equal(fqnFixture, fqn.Fqn)
-	s.Contains(fqn.Values, a.Value)
+
+	// there should be only one value
+	s.Equal(1, len(attr.Values))
+
+	// the value should match the fixture
+	av := attr.Values[0]
+	s.Equal(attr.Id, valueFixture.AttributeDefinitionId)
+	s.Equal(av.Id, valueFixture.Id)
+	s.Equal(av.Value, valueFixture.Value)
+}
+
+// Test Get one attribute by the FQN of the attribute definition
+func (s *AttributeFqnSuite) TestGetAttributeByFqn_WithAttrFqn() {
+	fqnFixtureKey := "example.net/attr/attr1"
+	fullFqn := fmt.Sprintf("https://%s", fqnFixtureKey)
+	attrFixture := fixtures.GetAttributeKey(fqnFixtureKey)
+
+	attr, err := s.db.PolicyClient.GetAttributeByFqn(s.ctx, fullFqn)
+	s.NoError(err)
+
+	// the number of values should match the fixture
+	s.Equal(2, len(attr.Values))
+
+	// the attribute should match the fixture
+	s.Equal(attr.Id, attrFixture.Id)
+	s.Equal(attr.Name, attrFixture.Name)
+	s.Equal(attr.Rule.String(), fmt.Sprintf("ATTRIBUTE_RULE_TYPE_ENUM_%s", attrFixture.Rule))
+	s.Equal(attr.Active.Value, attrFixture.Active)
 }
 
 // Test multiple get attributes by multiple fqns
-func (s *AttributeFqnSuite) TestGetAttributesByFqns() {
-	namespace := "https://testing_multiple_fqns.get"
+func (s *AttributeFqnSuite) TestGetAttributesByValueFqns() {
+	namespace := "testing_multiple_fqns.get"
 	attr := "test_attr"
-	value := "test_value"
-	fqn := fqnBuilder(namespace, attr, value)
+	value1 := "test_value"
+	value2 := "test_value_2"
+	fqn1 := fqnBuilder(namespace, attr, value1)
+	fqn2 := fqnBuilder(namespace, attr, value2)
 
 	// Create namespace
 	nsId, err := s.db.PolicyClient.CreateNamespace(s.ctx, namespace)
@@ -127,23 +158,65 @@ func (s *AttributeFqnSuite) TestGetAttributesByFqns() {
 	})
 	assert.NoError(s.T(), err)
 
-	// Create attribute value
-	v, err := s.db.PolicyClient.CreateAttributeValue(s.ctx, a.Id, &attributes.ValueCreateUpdate{
-		Value: value,
+	// Create attribute value1
+	v1, err := s.db.PolicyClient.CreateAttributeValue(s.ctx, a.Id, &attributes.ValueCreateUpdate{
+		Value: value1,
 	})
 	assert.NoError(s.T(), err)
 
-	// Get attributes by fqns
-	fqns := []string{fqn}
-	attrs, err := s.db.PolicyClient.GetAttributesByFqns(s.ctx, fqns)
+	// Get attributes by fqns with a solo value
+	fqns := []string{fqn1}
+	attrs, err := s.db.PolicyClient.GetAttributesByValueFqns(s.ctx, fqns)
 	assert.NoError(s.T(), err)
+
+	// Verify attribute1 is sole attribute
 	assert.Len(s.T(), attrs, 1)
-	val, ok := attrs[fqn]
+	val, ok := attrs[fqn1]
 	assert.True(s.T(), ok)
 	assert.Equal(s.T(), a.Id, val.Id)
-	assert.Equal(s.T(), fqn, val.Fqn)
-	assert.Equal(s.T(), a.Id, val.Values[0].AttributeId)
-	assert.Equal(s.T(), v.Id, val.Values[0].Id)
-	assert.Equal(s.T(), v.Value, val.Values[0].Value)
-	assert.Equal(s.T(), fqn, val.Values[0].Fqn)
+	assert.Equal(s.T(), v1.Id, val.Values[0].Id)
+	assert.Equal(s.T(), v1.Value, val.Values[0].Value)
+
+	// Create attribute value2
+	v2, err := s.db.PolicyClient.CreateAttributeValue(s.ctx, a.Id, &attributes.ValueCreateUpdate{
+		Value: value2,
+	})
+	assert.NoError(s.T(), err)
+	fmt.Println(v2)
+
+	// Get attributes by fqns with two values
+	fqns = []string{fqn1, fqn2}
+	attrs, err = s.db.PolicyClient.GetAttributesByValueFqns(s.ctx, fqns)
+	assert.NoError(s.T(), err)
+	assert.Len(s.T(), attrs, 2)
+
+	val, ok = attrs[fqn2]
+	assert.True(s.T(), ok)
+	assert.Equal(s.T(), a.Id, val.Id)
+
+	for _, v := range val.Values {
+		if v.Id == v1.Id {
+			assert.Equal(s.T(), v1.Id, v.Id)
+			assert.Equal(s.T(), v1.Value, v.Value)
+		} else if v.Id == v2.Id {
+			assert.Equal(s.T(), v2.Id, v.Id)
+			assert.Equal(s.T(), v2.Value, v.Value)
+		} else {
+			assert.Fail(s.T(), "unexpected value", v)
+		}
+	}
+}
+
+func (s *AttributeFqnSuite) TestGetAttributesByValueFqns_Fails_WithNonValueFqns(){
+	nsFqn := fqnBuilder("example.com", "", "")
+	attrFqn := fqnBuilder("example.com", "attr1", "")
+	v, err := s.db.PolicyClient.GetAttributesByValueFqns(s.ctx, []string{nsFqn})
+	assert.Error(s.T(), err)
+	assert.Nil(s.T(), v)
+	assert.ErrorIs(s.T(), err, db.ErrFqnMissingValue)
+
+	v, err = s.db.PolicyClient.GetAttributesByValueFqns(s.ctx, []string{attrFqn})
+	assert.Error(s.T(), err)
+	assert.Nil(s.T(), v)
+	assert.ErrorIs(s.T(), err, db.ErrFqnMissingValue)
 }

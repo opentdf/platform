284b8e79aa51d652be4df2628cccd305850553ac
Sean Trantalis <strantalis@virtru.com>
2024-02-26 14:07:44 -0500

    feat(core): add grpc health service (#261)
    This new service implements the
    `google.golang.org/grpc/health/grpc_health_v1` service and exposes it
    via grpc and http.
    
    The only weird issue is that the message `HealthCheckRequest` has a
    field called `Service` which is relevant if we wanted to check the
    health of each service. For right now this takes advantage of that field
    so you can just pass `liveness` or `readiness` where `readiness`
    currently pings the database.
     
    ```sh
    curl http://localhost:8081/healthz\?service\=readiness
    {
      "status":"SERVING"
    }
    
    grpcurl -plaintext -d '{"service": "readiness"}' localhost:9000 grpc.health.v1.Health.Check
    {
      "status": "SERVING"
    }
    ```
    
    Closes #89

--- internal/server/server.go
+++ internal/server/server.go
@@ -18,6 +18,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
 )
 
@@ -100,7 +101,14 @@ func NewOpenTDFServer(config Config) (*OpenTDFServer, error) {
 		grpcOpts...,
 	)
 
-	mux := runtime.NewServeMux()
+	grpcInprocess := &inProcessServer{
+		ln:  fasthttputil.NewInmemoryListener(),
+		srv: grpc.NewServer(),
+	}
+
+	mux := runtime.NewServeMux(
+		runtime.WithHealthzEndpoint(healthpb.NewHealthClient(grpcInprocess.Conn())),
+	)
 
 	// Enable grpc reflection
 	if config.Grpc.ReflectionEnabled {
@@ -130,10 +138,7 @@ func NewOpenTDFServer(config Config) (*OpenTDFServer, error) {
 		HTTPServer:        httpServer,
 		GrpcServer:        grpcServer,
 		grpcServerAddress: fmt.Sprintf(":%d", config.Grpc.Port),
-		GrpcInProcess: &inProcessServer{
-			ln:  fasthttputil.NewInmemoryListener(),
-			srv: grpc.NewServer(),
-		},
+		GrpcInProcess:     grpcInprocess,
 	}, nil
 }
 

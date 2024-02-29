706d49569c44e1392e1b448202ccdc0f9f7bbb3b
jakedoublev <jake.vanvorhis@virtru.com>
2024-02-29 12:02:11 -0500

    working migration of subject mapping into condition column

--- migrations/20240228000000_add_subject_condition_sets.sql
+++ migrations/20240228000000_add_subject_condition_sets.sql
@@ -14,30 +14,55 @@ CREATE TABLE IF NOT EXISTS subject_mapping_condition_set_pivot (
     subject_condition_set_id UUID REFERENCES subject_condition_set(id) ON DELETE CASCADE
 );
 
-INSERT INTO subject_condition_set(metadata, condition) VALUES (
-    SELECT JSON_AGG(JSON_BUILD_OBJECT(
-        'created_at', subject_mappings.created_at,
-        'updated_at', subject_mappings.updated_at,
-    )),
-    SELECT JSON_AGG(JSON_BUILD_OBJECT(
-        'subject_sets', JSON_BUILD_ARRAY(
+INSERT INTO subject_condition_set(metadata, condition) SELECT
+    JSON_BUILD_OBJECT(
+        'created_at', metadata::json->'created_at',
+        'updated_at', metadata::json->'updated_at'
+    ),
+    JSON_BUILD_OBJECT(
+    'subject_sets',
+        JSON_BUILD_ARRAY(
             JSON_BUILD_OBJECT(
                 'condition_groups', JSON_BUILD_ARRAY(
                     JSON_BUILD_OBJECT(
                         'boolean_operator', 'AND',
-                        JSON_BUILD_ARRAY(
+                        'conditions', JSON_BUILD_ARRAY(
                             JSON_BUILD_OBJECT(
-                                'operator', subject_mappings.operator,
-                                'subject_attribute', subject_mappings.subject_attribute,
-                                'subject_attribute_values', subject_mappings.subject_attribute_values
+                                'operator', operator,
+                                'subject_external_field', subject_attribute,
+                                'subject_external_values', subject_attribute_values
                             )
                         )
                     )
                 )
             )
-        ),
-    ))
-)
+        )
+    )
+FROM subject_mappings;
+/* Example of built JSON:
+{
+    "subject_sets": [
+        {
+        "condition_groups": [
+            {
+                "conditions": [
+                    {
+                        "operator": "IN",
+                        "subject_external_field": "subject_attribute1",
+                        "subject_external_values": [
+                            "value1",
+                            "value2"
+                        ]
+                    }
+                ],
+                "boolean_operator": "AND"
+            }
+        ]
+        }
+    ]
+}
+*/
+
 
 ALTER TABLE IF EXISTS subject_mappings DROP COLUMN operator, DROP COLUMN subject_attribute, DROP COLUMN subject_attribute_values;
 ALTER TABLE IF EXISTS subject_mappings ADD COLUMN subject_condition_set_pivot_ids UUID[], ADD COLUMN actions VARCHAR[];

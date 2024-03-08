c4dd18f3aa6147b97b5699837efabda11a5a819b
Morgan Kleene <mkleene@virtru.com>
2024-03-08 17:09:13 -0500

    adding a token appears to work from the client side

--- sdk/outgoing_interceptor.go
+++ sdk/outgoing_interceptor.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"crypto/rand"
 	"encoding/base64"
-	"errors"
 	"fmt"
 	"log/slog"
 	"time"
@@ -21,24 +20,19 @@ type outgoingInterceptor struct {
 }
 
 func (i outgoingInterceptor) addCredentials(ctx context.Context, method string, req, reply any, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-	md, ok := metadata.FromOutgoingContext(ctx)
-	if !ok {
-		slog.Error("couldn't get metadata from outgoing context")
-	}
-
 	newMetadata := make([]string, 0)
 	accessToken, err := i.tokenSource.AccessToken()
-	if err != nil {
-		slog.Error("error getting access token: %w. Request will be unauthenticated", err)
-	} else {
+	if err == nil {
 		newMetadata = append(newMetadata, "Authorization", fmt.Sprintf("Bearer %s", accessToken))
+	} else {
+		slog.Error("error getting access token: %w. Request will be unauthenticated", err)
 	}
 
-	dpopTok, err := i.getDPOPToken(md)
-	if err != nil {
-		slog.Error("error adding dpop token to outgoing request", err)
-	} else {
+	dpopTok, err := i.getDPOPToken(method)
+	if err == nil {
 		newMetadata = append(newMetadata, "DPoP", dpopTok)
+	} else {
+		slog.Error("error adding dpop token to outgoing request. Request will not have DPoP token", err)
 	}
 
 	newCtx := metadata.AppendToOutgoingContext(ctx, newMetadata...)
@@ -46,19 +40,7 @@ func (i outgoingInterceptor) addCredentials(ctx context.Context, method string, 
 	return invoker(newCtx, method, req, reply, cc, opts...)
 }
 
-func (i outgoingInterceptor) getDPOPToken(md metadata.MD) (string, error) {
-	httpMethod := "POST"
-	methods := md.Get("method")
-	if len(methods) > 0 {
-		httpMethod = methods[0]
-	}
-	paths := md.Get("path")
-	if len(paths) == 0 {
-		return "", errors.New("couldn't get a path to sign in the DPOP token")
-	}
-
-	path := paths[0]
-
+func (i outgoingInterceptor) getDPOPToken(method string) (string, error) {
 	tok, err := i.tokenSource.MakeToken(func(key jwk.Key) ([]byte, error) {
 		jtiBytes := make([]byte, 14)
 		_, err := rand.Read(jtiBytes)
@@ -66,15 +48,20 @@ func (i outgoingInterceptor) getDPOPToken(md metadata.MD) (string, error) {
 			return nil, fmt.Errorf("error creating jti for dpop jwt: %w", err)
 		}
 
+		publicKey, err := key.PublicKey()
+		if err != nil {
+			return nil, fmt.Errorf("error getting public key from DPOP key: %w", err)
+		}
+
 		headers := jws.NewHeaders()
-		headers.Set(jws.JWKKey, key)
+		headers.Set(jws.JWKKey, publicKey)
 		headers.Set(jws.TypeKey, "dpop+jwt")
 		headers.Set(jws.AlgorithmKey, key.Algorithm())
 		headers.Set("jti", base64.StdEncoding.EncodeToString(jtiBytes))
 
 		dpopTok, err := jwt.NewBuilder().
-			Claim("htu", path).
-			Claim("htm", httpMethod).
+			Claim("htu", method).
+			Claim("htm", "POST").
 			IssuedAt(time.Now()).
 			Build()
 
@@ -83,6 +70,7 @@ func (i outgoingInterceptor) getDPOPToken(md metadata.MD) (string, error) {
 		}
 
 		signedToken, err := jwt.Sign(dpopTok, jwt.WithKey(key.Algorithm(), key, jws.WithProtectedHeaders(headers)))
+		fmt.Printf("%s\n\n\n", signedToken)
 		if err != nil {
 			return nil, fmt.Errorf("error signing dpop jwt: %w", err)
 		}

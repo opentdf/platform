6548f55625201aead80347c4e48da3127559c6e4
Sean Trantalis <strantalis@virtru.com>
2024-03-11 20:14:25 -0400

    fix(archive): remove 10gb zip file test (#373)

--- sdk/internal/archive/tdf3_writer_reader_test.go
+++ sdk/internal/archive/tdf3_writer_reader_test.go
@@ -115,54 +115,6 @@ var TDF3Tests = []TDF3Entry{ //nolint:gochecknoglobals // This global is used as
 		payloadSize: 3 * oneGB,
 		tdfSize:     3145729933,
 	},
-	{
-		manifest: `{
-	"encryptionInformation": {
-		"integrityInformation": {
-			"encryptedSegmentSizeDefault": 1048604,
-			"rootSignature": {
-				"alg": "HS256",
-				"sig": "ZjEwYWRjMzJkNzVhMmNkMzljYTU3ZDg3YTJjNjMyMGYwOTZkYjZhZDY4ZTE1Y2Y1MzRlNTdjNjBhNjdlNWUwMQ=="
-			},
-			"segmentHashAlg": "GMAC",
-			"segmentSizeDefault": 1048576,
-			"segments": [
-				{
-					"encryptedSegmentSize": 228,
-					"hash": "YWRkNDhhZWM0Y2VhNmQwZjU5Y2ViOTc5MmFhYzdlOTI=",
-					"segmentSize": 200
-				}
-			]
-		},
-		"keyAccess": [
-			{
-				"encryptedMetadata": "eyJjaXBoZXJ0ZXh0IjoidkwyOUVVb1IyOFpVNStiMzFDdE1iNFFVODF5dVhPTnM3SUtDYlZNcDloZkg3dCs2UFRPaG00VFAwbVRDc3R3UEFkeU1ucHltbk4rWWNON0hmbytDIiwiaXYiOiJ2TDI5RVVvUjI4WlU1K2IzIn0=",
-				"policyBinding": "Zjk1Mjg2ZDljMzYwNGE5ZmU3YWE2M2UzOWRmMjA5MGU2OTJkYTZiYjExNjFkZmZjNTI2N2JkMWY5M2Y3MzIzZQ==",
-				"protocol": "kas",
-				"type": "wrapped",
-				"url": "http://localhost:65432/api/kas",
-				"wrappedKey": "ARu5wnJPNDaivQymXKOogyC2n11QP4Jf8ZYtrAcYQnUmE9hsjQD2R+48js5T1LkNLp5TzaRREF5sSk5/dhlBge/YXVcT42d5lNp0SecAF68dsso/aXq+G2sRJFVWdYKAtc32mr8KJiPisHtPlPFPM7u37lU0YX93lsqIxiUPn6qkxkD4cEozvA9UgB8YZ8alJtNACnpbOUebJeRLkHbxXM7DzW4gur/lu88lRUtCdaHNBeSOTCgWi2oqTU70asyoFQVVD7R80xKblam5k/B3PKhCkerZkDwyy5D4eODbbqKpGfbluW6NWEM+HtYnJFa+2kJB51yqylsbUnfpWEBQDA=="
-			}
-		],
-		"method": {
-			"algorithm": "AES-256-GCM",
-			"isStreamable": true,
-			"iv": "vL29EUoR28ZU5+b3"
-		},
-		"policy": "eyJib2R5Ijp7ImRhdGFBdHRyaWJ1dGVzIjpbXSwiZGlzc2VtIjpbXX0sInV1aWQiOiJlMDk0NmVhNC1mZDMzLTQ3ODktODM3Ny1hMzhiMjNhOTc1MmIifQ==",
-		"type": "split"
-	},
-	"payload": {
-		"isEncrypted": true,
-		"mimeType": "application/octet-stream",
-		"protocol": "zip",
-		"type": "reference",
-		"url": "0.payload"
-	}
-}`,
-		payloadSize: 10 * oneGB,
-		tdfSize:     10485762121,
-	},
 }
 
 func TestTDF3Writer_and_Reader(t *testing.T) { // Create tdf files

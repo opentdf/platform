4d55056bd2148dd3de0f50083ac1732b29a9d148
jakedoublev <jake.vanvorhis@virtru.com>
2024-03-13 14:57:32 -0400

    remove dummy proto

--- protocol/go/policy/selectors.pb.go
+++ protocol/go/policy/selectors.pb.go
@@ -201,53 +201,6 @@ func (x *AttributeValueSelector) GetWithAttribute() *AttributeValueSelector_Attr
 	return nil
 }
 
-type DummyProto struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	HelloWorld string `protobuf:"bytes,1,opt,name=hello_world,json=helloWorld,proto3" json:"hello_world,omitempty"`
-}
-
-func (x *DummyProto) Reset() {
-	*x = DummyProto{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_policy_selectors_proto_msgTypes[3]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *DummyProto) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*DummyProto) ProtoMessage() {}
-
-func (x *DummyProto) ProtoReflect() protoreflect.Message {
-	mi := &file_policy_selectors_proto_msgTypes[3]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use DummyProto.ProtoReflect.Descriptor instead.
-func (*DummyProto) Descriptor() ([]byte, []int) {
-	return file_policy_selectors_proto_rawDescGZIP(), []int{3}
-}
-
-func (x *DummyProto) GetHelloWorld() string {
-	if x != nil {
-		return x.HelloWorld
-	}
-	return ""
-}
-
 type AttributeNamespaceSelector_AttributeSelector struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -260,7 +213,7 @@ type AttributeNamespaceSelector_AttributeSelector struct {
 func (x *AttributeNamespaceSelector_AttributeSelector) Reset() {
 	*x = AttributeNamespaceSelector_AttributeSelector{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_policy_selectors_proto_msgTypes[4]
+		mi := &file_policy_selectors_proto_msgTypes[3]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -273,7 +226,7 @@ func (x *AttributeNamespaceSelector_AttributeSelector) String() string {
 func (*AttributeNamespaceSelector_AttributeSelector) ProtoMessage() {}
 
 func (x *AttributeNamespaceSelector_AttributeSelector) ProtoReflect() protoreflect.Message {
-	mi := &file_policy_selectors_proto_msgTypes[4]
+	mi := &file_policy_selectors_proto_msgTypes[3]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -316,7 +269,7 @@ type AttributeNamespaceSelector_AttributeSelector_ValueSelector struct {
 func (x *AttributeNamespaceSelector_AttributeSelector_ValueSelector) Reset() {
 	*x = AttributeNamespaceSelector_AttributeSelector_ValueSelector{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_policy_selectors_proto_msgTypes[5]
+		mi := &file_policy_selectors_proto_msgTypes[4]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -329,7 +282,7 @@ func (x *AttributeNamespaceSelector_AttributeSelector_ValueSelector) String() st
 func (*AttributeNamespaceSelector_AttributeSelector_ValueSelector) ProtoMessage() {}
 
 func (x *AttributeNamespaceSelector_AttributeSelector_ValueSelector) ProtoReflect() protoreflect.Message {
-	mi := &file_policy_selectors_proto_msgTypes[5]
+	mi := &file_policy_selectors_proto_msgTypes[4]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -375,7 +328,7 @@ type AttributeDefinitionSelector_NamespaceSelector struct {
 func (x *AttributeDefinitionSelector_NamespaceSelector) Reset() {
 	*x = AttributeDefinitionSelector_NamespaceSelector{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_policy_selectors_proto_msgTypes[6]
+		mi := &file_policy_selectors_proto_msgTypes[5]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -388,7 +341,7 @@ func (x *AttributeDefinitionSelector_NamespaceSelector) String() string {
 func (*AttributeDefinitionSelector_NamespaceSelector) ProtoMessage() {}
 
 func (x *AttributeDefinitionSelector_NamespaceSelector) ProtoReflect() protoreflect.Message {
-	mi := &file_policy_selectors_proto_msgTypes[6]
+	mi := &file_policy_selectors_proto_msgTypes[5]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -417,7 +370,7 @@ type AttributeDefinitionSelector_ValueSelector struct {
 func (x *AttributeDefinitionSelector_ValueSelector) Reset() {
 	*x = AttributeDefinitionSelector_ValueSelector{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_policy_selectors_proto_msgTypes[7]
+		mi := &file_policy_selectors_proto_msgTypes[6]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -430,7 +383,7 @@ func (x *AttributeDefinitionSelector_ValueSelector) String() string {
 func (*AttributeDefinitionSelector_ValueSelector) ProtoMessage() {}
 
 func (x *AttributeDefinitionSelector_ValueSelector) ProtoReflect() protoreflect.Message {
-	mi := &file_policy_selectors_proto_msgTypes[7]
+	mi := &file_policy_selectors_proto_msgTypes[6]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -479,7 +432,7 @@ type AttributeValueSelector_AttributeSelector struct {
 func (x *AttributeValueSelector_AttributeSelector) Reset() {
 	*x = AttributeValueSelector_AttributeSelector{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_policy_selectors_proto_msgTypes[8]
+		mi := &file_policy_selectors_proto_msgTypes[7]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -492,7 +445,7 @@ func (x *AttributeValueSelector_AttributeSelector) String() string {
 func (*AttributeValueSelector_AttributeSelector) ProtoMessage() {}
 
 func (x *AttributeValueSelector_AttributeSelector) ProtoReflect() protoreflect.Message {
-	mi := &file_policy_selectors_proto_msgTypes[8]
+	mi := &file_policy_selectors_proto_msgTypes[7]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -531,7 +484,7 @@ type AttributeValueSelector_AttributeSelector_NamespaceSelector struct {
 func (x *AttributeValueSelector_AttributeSelector_NamespaceSelector) Reset() {
 	*x = AttributeValueSelector_AttributeSelector_NamespaceSelector{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_policy_selectors_proto_msgTypes[9]
+		mi := &file_policy_selectors_proto_msgTypes[8]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -544,7 +497,7 @@ func (x *AttributeValueSelector_AttributeSelector_NamespaceSelector) String() st
 func (*AttributeValueSelector_AttributeSelector_NamespaceSelector) ProtoMessage() {}
 
 func (x *AttributeValueSelector_AttributeSelector_NamespaceSelector) ProtoReflect() protoreflect.Message {
-	mi := &file_policy_selectors_proto_msgTypes[9]
+	mi := &file_policy_selectors_proto_msgTypes[8]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -650,20 +603,17 @@ var file_policy_selectors_proto_rawDesc = []byte{
 	0x63, 0x74, 0x6f, 0x72, 0x2e, 0x4e, 0x61, 0x6d, 0x65, 0x73, 0x70, 0x61, 0x63, 0x65, 0x53, 0x65,
 	0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x52, 0x0d, 0x77, 0x69, 0x74, 0x68, 0x4e, 0x61, 0x6d, 0x65,
 	0x73, 0x70, 0x61, 0x63, 0x65, 0x1a, 0x13, 0x0a, 0x11, 0x4e, 0x61, 0x6d, 0x65, 0x73, 0x70, 0x61,
-	0x63, 0x65, 0x53, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x22, 0x2d, 0x0a, 0x0a, 0x44, 0x75,
-	0x6d, 0x6d, 0x79, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x1f, 0x0a, 0x0b, 0x68, 0x65, 0x6c, 0x6c,
-	0x6f, 0x5f, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0a, 0x68,
-	0x65, 0x6c, 0x6c, 0x6f, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x42, 0x94, 0x01, 0x0a, 0x1a, 0x69, 0x6f,
-	0x2e, 0x6f, 0x70, 0x65, 0x6e, 0x74, 0x64, 0x66, 0x2e, 0x70, 0x6c, 0x61, 0x74, 0x66, 0x6f, 0x72,
-	0x6d, 0x2e, 0x70, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0x42, 0x0e, 0x53, 0x65, 0x6c, 0x65, 0x63, 0x74,
-	0x6f, 0x72, 0x73, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x50, 0x01, 0x5a, 0x2e, 0x67, 0x69, 0x74, 0x68,
-	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6f, 0x70, 0x65, 0x6e, 0x74, 0x64, 0x66, 0x2f, 0x70,
-	0x6c, 0x61, 0x74, 0x66, 0x6f, 0x72, 0x6d, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x6f, 0x6c,
-	0x2f, 0x67, 0x6f, 0x2f, 0x70, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0xa2, 0x02, 0x03, 0x50, 0x58, 0x58,
-	0xaa, 0x02, 0x06, 0x50, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0xca, 0x02, 0x06, 0x50, 0x6f, 0x6c, 0x69,
-	0x63, 0x79, 0xe2, 0x02, 0x12, 0x50, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0x5c, 0x47, 0x50, 0x42, 0x4d,
-	0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0xea, 0x02, 0x06, 0x50, 0x6f, 0x6c, 0x69, 0x63, 0x79,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x63, 0x65, 0x53, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x6f, 0x72, 0x42, 0x94, 0x01, 0x0a, 0x1a, 0x69,
+	0x6f, 0x2e, 0x6f, 0x70, 0x65, 0x6e, 0x74, 0x64, 0x66, 0x2e, 0x70, 0x6c, 0x61, 0x74, 0x66, 0x6f,
+	0x72, 0x6d, 0x2e, 0x70, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0x42, 0x0e, 0x53, 0x65, 0x6c, 0x65, 0x63,
+	0x74, 0x6f, 0x72, 0x73, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x50, 0x01, 0x5a, 0x2e, 0x67, 0x69, 0x74,
+	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6f, 0x70, 0x65, 0x6e, 0x74, 0x64, 0x66, 0x2f,
+	0x70, 0x6c, 0x61, 0x74, 0x66, 0x6f, 0x72, 0x6d, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x6f,
+	0x6c, 0x2f, 0x67, 0x6f, 0x2f, 0x70, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0xa2, 0x02, 0x03, 0x50, 0x58,
+	0x58, 0xaa, 0x02, 0x06, 0x50, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0xca, 0x02, 0x06, 0x50, 0x6f, 0x6c,
+	0x69, 0x63, 0x79, 0xe2, 0x02, 0x12, 0x50, 0x6f, 0x6c, 0x69, 0x63, 0x79, 0x5c, 0x47, 0x50, 0x42,
+	0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0xea, 0x02, 0x06, 0x50, 0x6f, 0x6c, 0x69, 0x63,
+	0x79, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -678,26 +628,25 @@ func file_policy_selectors_proto_rawDescGZIP() []byte {
 	return file_policy_selectors_proto_rawDescData
 }
 
-var file_policy_selectors_proto_msgTypes = make([]protoimpl.MessageInfo, 10)
+var file_policy_selectors_proto_msgTypes = make([]protoimpl.MessageInfo, 9)
 var file_policy_selectors_proto_goTypes = []interface{}{
 	(*AttributeNamespaceSelector)(nil),                                 // 0: policy.AttributeNamespaceSelector
 	(*AttributeDefinitionSelector)(nil),                                // 1: policy.AttributeDefinitionSelector
 	(*AttributeValueSelector)(nil),                                     // 2: policy.AttributeValueSelector
-	(*DummyProto)(nil),                                                 // 3: policy.DummyProto
-	(*AttributeNamespaceSelector_AttributeSelector)(nil),               // 4: policy.AttributeNamespaceSelector.AttributeSelector
-	(*AttributeNamespaceSelector_AttributeSelector_ValueSelector)(nil), // 5: policy.AttributeNamespaceSelector.AttributeSelector.ValueSelector
-	(*AttributeDefinitionSelector_NamespaceSelector)(nil),              // 6: policy.AttributeDefinitionSelector.NamespaceSelector
-	(*AttributeDefinitionSelector_ValueSelector)(nil),                  // 7: policy.AttributeDefinitionSelector.ValueSelector
-	(*AttributeValueSelector_AttributeSelector)(nil),                   // 8: policy.AttributeValueSelector.AttributeSelector
-	(*AttributeValueSelector_AttributeSelector_NamespaceSelector)(nil), // 9: policy.AttributeValueSelector.AttributeSelector.NamespaceSelector
+	(*AttributeNamespaceSelector_AttributeSelector)(nil),               // 3: policy.AttributeNamespaceSelector.AttributeSelector
+	(*AttributeNamespaceSelector_AttributeSelector_ValueSelector)(nil), // 4: policy.AttributeNamespaceSelector.AttributeSelector.ValueSelector
+	(*AttributeDefinitionSelector_NamespaceSelector)(nil),              // 5: policy.AttributeDefinitionSelector.NamespaceSelector
+	(*AttributeDefinitionSelector_ValueSelector)(nil),                  // 6: policy.AttributeDefinitionSelector.ValueSelector
+	(*AttributeValueSelector_AttributeSelector)(nil),                   // 7: policy.AttributeValueSelector.AttributeSelector
+	(*AttributeValueSelector_AttributeSelector_NamespaceSelector)(nil), // 8: policy.AttributeValueSelector.AttributeSelector.NamespaceSelector
 }
 var file_policy_selectors_proto_depIdxs = []int32{
-	4, // 0: policy.AttributeNamespaceSelector.with_attributes:type_name -> policy.AttributeNamespaceSelector.AttributeSelector
-	6, // 1: policy.AttributeDefinitionSelector.with_namespace:type_name -> policy.AttributeDefinitionSelector.NamespaceSelector
-	7, // 2: policy.AttributeDefinitionSelector.with_values:type_name -> policy.AttributeDefinitionSelector.ValueSelector
-	8, // 3: policy.AttributeValueSelector.with_attribute:type_name -> policy.AttributeValueSelector.AttributeSelector
-	5, // 4: policy.AttributeNamespaceSelector.AttributeSelector.with_values:type_name -> policy.AttributeNamespaceSelector.AttributeSelector.ValueSelector
-	9, // 5: policy.AttributeValueSelector.AttributeSelector.with_namespace:type_name -> policy.AttributeValueSelector.AttributeSelector.NamespaceSelector
+	3, // 0: policy.AttributeNamespaceSelector.with_attributes:type_name -> policy.AttributeNamespaceSelector.AttributeSelector
+	5, // 1: policy.AttributeDefinitionSelector.with_namespace:type_name -> policy.AttributeDefinitionSelector.NamespaceSelector
+	6, // 2: policy.AttributeDefinitionSelector.with_values:type_name -> policy.AttributeDefinitionSelector.ValueSelector
+	7, // 3: policy.AttributeValueSelector.with_attribute:type_name -> policy.AttributeValueSelector.AttributeSelector
+	4, // 4: policy.AttributeNamespaceSelector.AttributeSelector.with_values:type_name -> policy.AttributeNamespaceSelector.AttributeSelector.ValueSelector
+	8, // 5: policy.AttributeValueSelector.AttributeSelector.with_namespace:type_name -> policy.AttributeValueSelector.AttributeSelector.NamespaceSelector
 	6, // [6:6] is the sub-list for method output_type
 	6, // [6:6] is the sub-list for method input_type
 	6, // [6:6] is the sub-list for extension type_name
@@ -748,18 +697,6 @@ func file_policy_selectors_proto_init() {
 			}
 		}
 		file_policy_selectors_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*DummyProto); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_policy_selectors_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AttributeNamespaceSelector_AttributeSelector); i {
 			case 0:
 				return &v.state
@@ -771,7 +708,7 @@ func file_policy_selectors_proto_init() {
 				return nil
 			}
 		}
-		file_policy_selectors_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+		file_policy_selectors_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AttributeNamespaceSelector_AttributeSelector_ValueSelector); i {
 			case 0:
 				return &v.state
@@ -783,7 +720,7 @@ func file_policy_selectors_proto_init() {
 				return nil
 			}
 		}
-		file_policy_selectors_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+		file_policy_selectors_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AttributeDefinitionSelector_NamespaceSelector); i {
 			case 0:
 				return &v.state
@@ -795,7 +732,7 @@ func file_policy_selectors_proto_init() {
 				return nil
 			}
 		}
-		file_policy_selectors_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
+		file_policy_selectors_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AttributeDefinitionSelector_ValueSelector); i {
 			case 0:
 				return &v.state
@@ -807,7 +744,7 @@ func file_policy_selectors_proto_init() {
 				return nil
 			}
 		}
-		file_policy_selectors_proto_msgTypes[8].Exporter = func(v interface{}, i int) interface{} {
+		file_policy_selectors_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AttributeValueSelector_AttributeSelector); i {
 			case 0:
 				return &v.state
@@ -819,7 +756,7 @@ func file_policy_selectors_proto_init() {
 				return nil
 			}
 		}
-		file_policy_selectors_proto_msgTypes[9].Exporter = func(v interface{}, i int) interface{} {
+		file_policy_selectors_proto_msgTypes[8].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*AttributeValueSelector_AttributeSelector_NamespaceSelector); i {
 			case 0:
 				return &v.state
@@ -838,7 +775,7 @@ func file_policy_selectors_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_policy_selectors_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   10,
+			NumMessages:   9,
 			NumExtensions: 0,
 			NumServices:   0,
 		},

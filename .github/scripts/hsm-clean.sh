85d76b2ce8f49eb00b7957b5f39c3ad23d347063
Dave Mihalcik <dmihalcik@virtru.com>
2024-03-18 21:55:26 -0400

    ci(hsm): Updates hsm init env variables (#424)
    - Use custom pin during CI
    - This should validate that the environment variable PIN overrides any
    found in the .yaml config

--- .github/scripts/hsm-clean.sh
+++ .github/scripts/hsm-clean.sh
@@ -4,13 +4,8 @@
 
 set -ex
 
-PKCS11_MODULE_PATH=/lib/softhsm/libsofthsm2.so
-if which brew; then
-  PKCS11_MODULE_PATH=$(brew --prefix)/lib/softhsm/libsofthsm2.so
-fi
-
 softhsm2-util --show-slots | sed -n "s/^.*Serial number[^0-9a-f]*\([0-9a-f]*\)$/\1/p" | while read -r slot; do
-  if [ ! -z $slot ]; then
-    softhsm2-util --delete-token --serial $slot
+  if [ -n "${slot}" ]; then
+    softhsm2-util --delete-token --serial "${slot}"
   fi
 done

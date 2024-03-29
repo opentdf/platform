bbe44aae31fd19b5b68e228fd17e03c34dc6cfcf
Paul Flynn <pflynn@virtru.com>
2024-03-29 10:27:31 -0400

    feat(authorization): pr fix

--- internal/idpplugin/keycloak_plugin.go
+++ internal/idpplugin/keycloak_plugin.go
@@ -91,7 +91,7 @@ func EntityResolution(ctx context.Context,
 				resolvedEntities,
 				&authorization.IdpEntityRepresentation{
 					OriginalId:      ident.GetId(),
-					AdditionalProps: jsonEntities
+					AdditionalProps: jsonEntities,
 				},
 			)
 			return &authorization.IdpPluginResponse{

4831bbcd86b364b9e9ff56a17225f9ba320b4075
Dave Mihalcik <dmihalcik@virtru.com>
2024-03-29 12:06:54 -0400

    ci(tdf): Check decrypt output (#459)

--- examples/cmd/decrypt.go
+++ examples/cmd/decrypt.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"strings"
@@ -53,6 +54,6 @@ func decrypt(cmd *cobra.Command, args []string) error {
 	}
 
 	//Print decrypted string
-	cmd.Println(buf.String())
+	fmt.Println(buf.String())
 	return nil
 }

657c2b5d061bd6be374be350b99a11b9d1abac12
sujan kota <sujankota@gmail.com>
2024-04-02 10:48:17 -0400

    refactor(crypto): move crypto from sdk/internal/crypto to lib/crypto

--- sdk/kas_client.go
+++ sdk/kas_client.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/lestrrat-go/jwx/v2/jwt"
+	"github.com/opentdf/platform/lib/crypto"
 	kas "github.com/opentdf/platform/protocol/go/kas"
 	"github.com/opentdf/platform/sdk/auth"
-	"github.com/opentdf/platform/sdk/internal/crypto"
 	"google.golang.org/grpc"
 )
 

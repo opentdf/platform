5390a39b3734fa7f860f931999515bc3a66a5bde
Paul Flynn <pflynn@virtru.com>
2024-04-05 14:09:03 -0400

    test(authorization): readme

--- policies/entitlements/README.md
+++ policies/entitlements/README.md
@@ -1,5 +1,50 @@
 # OpenTDF Platform OPA rego policies
 
+## Testing
+
+Use `../policies_test.go` for testing rego functions
+
+A failed assertation will appear below.  The second Error Trace is the test case with the failure.
+
+```text
+=== RUN   TestRegoConditionSimple
+compile.go:3116: conditions-test.rego:9: comprehension index: no index vars
+compile.go:3086: conditions-test.rego:11: comprehension index: unsafe vars: [__local0__ __local33__]
+compile.go:3086: conditions-test.rego:13: comprehension index: unsafe vars: [__local1__ __local34__]
+compile.go:3116: entitlements.rego:5: comprehension index: no index vars
+    policies_test.go:221: 
+        	Error Trace:	/Users/abc/Projects/opentdf/platform/policies/policies_test.go:221
+        	            				/Users/abc/Projects/opentdf/platform/policies/policies_test.go:198
+        	Error:      	Not equal: 
+        	            	expected: false
+        	            	actual  : true
+        	Test:       	TestRegoConditionSimple
+--- FAIL: TestRegoConditionSimple (0.00s)
+```
+
+An invalid input JSON will appear as
+
+```text
+=== RUN   TestRegoConditionSimple
+    policies_test.go:198: 
+        	Error Trace:	/Users/abc/Projects/opentdf/platform/policies/policies_test.go:198
+        	Error:      	Received unexpected error:
+        	            	invalid character 'a' looking for beginning of object key string
+        	Test:       	TestRegoConditionSimple
+```
+
+An invalid .rego will appear as
+
+```text
+=== RUN   TestRegoConditionSimple
+    policies_test.go:220: 
+        	Error Trace:	/Users/pflynn/Projects/opentdf/platform/policies/policies_test.go:220
+        	            				/Users/pflynn/Projects/opentdf/platform/policies/policies_test.go:202
+        	Error:      	Received unexpected error:
+        	            	3 errors occurred
+        	            	entitlements.rego:6: rego_parse_error: var cannot be used for rule name
+```
+
 ## entitlements.rego
 
 This is the default rego policy that will parse a JWT, 
--- policies/policies_test.go
+++ policies/policies_test.go
@@ -12,22 +12,104 @@ import (
 )
 
 func TestRegoEntitlementsEmpty(t *testing.T) {
-	var input interface{}
-	_ = json.Unmarshal(
-		[]byte("{}"),
-		&input,
-	)
 	expected := []interface{}{}
+	inputJSON := `{}`
+	var input interface{}
+	err := json.Unmarshal([]byte(inputJSON), &input)
+	assert.NoError(t, err) //nolint:testifylint
 	EvaluateRegoEntitlements(t, input, expected)
 }
 
 func TestRegoEntitlementsSimple(t *testing.T) {
-	var input interface{}
-	_ = json.Unmarshal(
-		[]byte("{\n  \"attribute_mappings\": {\n    \"https://brightonhealthclinic.org/attr/healthrecordtype/value/basicpatientinfo\": {\n      \"attribute\": {\n        \"id\": \"46fd500e-6839-4cc0-8b29-75665bf98e3a\",\n        \"namespace\": {\n          \"id\": \"f1f12166-8b22-47d6-829a-66e68b533eb2\",\n          \"name\": \"brightonhealthclinic.org\"\n        },\n        \"name\": \"healthrecordtype\",\n        \"rule\": 2,\n        \"values\": [\n          {\n            \"id\": \"356b7dd3-6abb-453c-8354-6915705fabcb\",\n            \"value\": \"basicpatientinfo\",\n            \"fqn\": \"https://brightonhealthclinic.org/attr/healthrecordtype/value/basicpatientinfo\",\n            \"active\": {\n              \"value\": true\n            }\n          }\n        ],\n        \"active\": {\n          \"value\": true\n        },\n        \"metadata\": {}\n      },\n      \"value\": {\n        \"id\": \"356b7dd3-6abb-453c-8354-6915705fabcb\",\n        \"value\": \"basicpatientinfo\",\n        \"fqn\": \"https://brightonhealthclinic.org/attr/healthrecordtype/value/basicpatientinfo\",\n        \"active\": {\n          \"value\": true\n        },\n        \"subject_mappings\": [\n          {\n            \"id\": \"5fb9f643-b7ea-4d53-8b23-f2b61a7ca38a\",\n            \"subject_condition_set\": {\n              \"id\": \"eb688795-fa0a-4014-8f4e-0f770c7bdab6\",\n              \"subject_sets\": [\n                {\n                  \"condition_groups\": [\n                    {\n                      \"conditions\": [\n                        {\n                          \"subject_external_field\": \"groups\",\n                          \"operator\": 1,\n                          \"subject_external_values\": [\n                            \"/medical\"\n                          ]\n                        },\n                        {\n                          \"subject_external_field\": \"roles\",\n                          \"operator\": 1,\n                          \"subject_external_values\": [\n                            \"nurse\",\n                            \"doctor\"\n                          ]\n                        }\n                      ],\n                      \"boolean_operator\": 1\n                    }\n                  ]\n                }\n              ],\n              \"metadata\": {}\n            },\n            \"actions\": [\n              {\n                \"Value\": {\n                  \"Standard\": 1\n                }\n              }\n            ],\n            \"metadata\": {}\n          }\n        ]\n      }\n    }\n  },\n  \"entity\": {\n    \"jwt\": \"eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3bEN5Vlo4Zm9jaTBOSk1JclZGZ01jZFc1cGlRQVUwVV9KZ1BNZ2V1RFI0In0.eyJleHAiOjE3MTE1MTI2MjEsImlhdCI6MTcxMTQ3NjYyMSwiYXV0aF90aW1lIjoxNzExNDc2NjIxLCJqdGkiOiI2YzJlMjk0Mi1iNmE4LTQ2MTYtOTYwZi0xN2I2NzA3MDFiMzgiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0Ojg4ODgvYXV0aC9yZWFsbXMvYnJpZ2h0b25oZWFsdGhjbGluaWMiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNWJkOTllODgtMDkyMS00MzVjLTkxOTktYjZmZTZmYjU3MGY4IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYWNjb3VudC1jb25zb2xlIiwibm9uY2UiOiJlYTMwMjk1OC1jMzgyLTQ0NGItYjNlNy0zODFhMjAzODE4YTciLCJzZXNzaW9uX3N0YXRlIjoiODQ0NjMxMjgtYjg0NS00ZDM1LTljM2MtNmJhNjFiZWEwZTYyIiwiYWNyIjoiMSIsInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiODQ0NjMxMjgtYjg0NS00ZDM1LTljM2MtNmJhNjFiZWEwZTYyIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJyb2xlcyI6Im51cnNlIiwibmFtZSI6Ik9saXZpYSBHcmVlbiIsImdyb3VwcyI6WyIvbWVkaWNhbCJdLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJvZ3JlZW4iLCJnaXZlbl9uYW1lIjoiT2xpdmlhIiwiZmFtaWx5X25hbWUiOiJHcmVlbiIsImVtYWlsIjoib2dyZWVuQGJyaWdodG9uaGVhbHRoY2xpbmljLm9yZyJ9.2MWhwiCdUChYPCvP-Q08Z1RvwO2a0Q3axR0agWAA-KToirV7bRdcMGJUDxEYzxCKmZYTcR3nNjeTF-WzK1ZsiasimsOVeqoCv8Q2YbA19KqxusaTrRtHcSLCVM8BWYGMBbrEzgn06MDYXjHVhAjAYytl20dImRjUPukFhG6XI8LIoX8sCRwb4gKb0fweIUaX6lj3TCvRw9NakIyN1Jotd3CBBZMFJ7fid1_HX8rWJ7HWZNdvILLMxv3euLO2IFFnnCaq6_etMCNeSzEqg3jei5gwOIcbPzERt2fpmrTX4OhFbUOJvI3MU-4uMJ68muKNCW0nlMEjUbYfCcjbSRUDjA\",\n    \"id\": \"\"\n  },\n  \"idp\": null\n}\n"),
-		&input,
-	)
 	expected := []interface{}{"https://brightonhealthclinic.org/attr/healthrecordtype/value/basicpatientinfo"}
+	inputJSON := `{
+  "attribute_mappings": {
+    "https://brightonhealthclinic.org/attr/healthrecordtype/value/basicpatientinfo": {
+      "attribute": {
+        "id": "46fd500e-6839-4cc0-8b29-75665bf98e3a",
+        "namespace": {
+          "id": "f1f12166-8b22-47d6-829a-66e68b533eb2",
+          "name": "brightonhealthclinic.org"
+        },
+        "name": "healthrecordtype",
+        "rule": 2,
+        "values": [
+          {
+            "id": "356b7dd3-6abb-453c-8354-6915705fabcb",
+            "value": "basicpatientinfo",
+            "fqn": "https://brightonhealthclinic.org/attr/healthrecordtype/value/basicpatientinfo",
+            "active": {
+              "value": true
+            }
+          }
+        ],
+        "active": {
+          "value": true
+        },
+        "metadata": {}
+      },
+      "value": {
+        "id": "356b7dd3-6abb-453c-8354-6915705fabcb",
+        "value": "basicpatientinfo",
+        "fqn": "https://brightonhealthclinic.org/attr/healthrecordtype/value/basicpatientinfo",
+        "active": {
+          "value": true
+        },
+        "subject_mappings": [
+          {
+            "id": "5fb9f643-b7ea-4d53-8b23-f2b61a7ca38a",
+            "subject_condition_set": {
+              "id": "eb688795-fa0a-4014-8f4e-0f770c7bdab6",
+              "subject_sets": [
+                {
+                  "condition_groups": [
+                    {
+                      "conditions": [
+                        {
+                          "subject_external_field": "groups",
+                          "operator": 1,
+                          "subject_external_values": [
+                            "/medical"
+                          ]
+                        },
+                        {
+                          "subject_external_field": "roles",
+                          "operator": 1,
+                          "subject_external_values": [
+                            "nurse",
+                            "doctor"
+                          ]
+                        }
+                      ],
+                      "boolean_operator": 1
+                    }
+                  ]
+                }
+              ],
+              "metadata": {}
+            },
+            "actions": [
+              {
+                "Value": {
+                  "Standard": 1
+                }
+              }
+            ],
+            "metadata": {}
+          }
+        ]
+      }
+    }
+  },
+  "entity": {
+    "jwt": "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI3bEN5Vlo4Zm9jaTBOSk1JclZGZ01jZFc1cGlRQVUwVV9KZ1BNZ2V1RFI0In0.eyJleHAiOjE3MTE1MTI2MjEsImlhdCI6MTcxMTQ3NjYyMSwiYXV0aF90aW1lIjoxNzExNDc2NjIxLCJqdGkiOiI2YzJlMjk0Mi1iNmE4LTQ2MTYtOTYwZi0xN2I2NzA3MDFiMzgiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0Ojg4ODgvYXV0aC9yZWFsbXMvYnJpZ2h0b25oZWFsdGhjbGluaWMiLCJhdWQiOiJhY2NvdW50Iiwic3ViIjoiNWJkOTllODgtMDkyMS00MzVjLTkxOTktYjZmZTZmYjU3MGY4IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYWNjb3VudC1jb25zb2xlIiwibm9uY2UiOiJlYTMwMjk1OC1jMzgyLTQ0NGItYjNlNy0zODFhMjAzODE4YTciLCJzZXNzaW9uX3N0YXRlIjoiODQ0NjMxMjgtYjg0NS00ZDM1LTljM2MtNmJhNjFiZWEwZTYyIiwiYWNyIjoiMSIsInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiXX19LCJzY29wZSI6Im9wZW5pZCBlbWFpbCBwcm9maWxlIiwic2lkIjoiODQ0NjMxMjgtYjg0NS00ZDM1LTljM2MtNmJhNjFiZWEwZTYyIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJyb2xlcyI6Im51cnNlIiwibmFtZSI6Ik9saXZpYSBHcmVlbiIsImdyb3VwcyI6WyIvbWVkaWNhbCJdLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJvZ3JlZW4iLCJnaXZlbl9uYW1lIjoiT2xpdmlhIiwiZmFtaWx5X25hbWUiOiJHcmVlbiIsImVtYWlsIjoib2dyZWVuQGJyaWdodG9uaGVhbHRoY2xpbmljLm9yZyJ9.2MWhwiCdUChYPCvP-Q08Z1RvwO2a0Q3axR0agWAA-KToirV7bRdcMGJUDxEYzxCKmZYTcR3nNjeTF-WzK1ZsiasimsOVeqoCv8Q2YbA19KqxusaTrRtHcSLCVM8BWYGMBbrEzgn06MDYXjHVhAjAYytl20dImRjUPukFhG6XI8LIoX8sCRwb4gKb0fweIUaX6lj3TCvRw9NakIyN1Jotd3CBBZMFJ7fid1_HX8rWJ7HWZNdvILLMxv3euLO2IFFnnCaq6_etMCNeSzEqg3jei5gwOIcbPzERt2fpmrTX4OhFbUOJvI3MU-4uMJ68muKNCW0nlMEjUbYfCcjbSRUDjA",
+    "id": ""
+  },
+  "idp": null
+}`
+	var input interface{}
+	err := json.Unmarshal([]byte(inputJSON), &input)
+	assert.NoError(t, err) //nolint:testifylint
 	EvaluateRegoEntitlements(t, input, expected)
 }
 
@@ -55,16 +137,17 @@ func EvaluateRegoEntitlements(t *testing.T, input interface{}, expected []interf
 	}
 }
 
+// TestRegoEntitlementsKeycloakEmpty tests builtin instantiation only
 func TestRegoEntitlementsKeycloakEmpty(t *testing.T) {
-	var input interface{}
-	_ = json.Unmarshal(
-		[]byte("{}"),
-		&input,
-	)
 	expected := []interface{}{}
+	inputJSON := `{}`
+	var input interface{}
+	err := json.Unmarshal([]byte(inputJSON), &input)
+	assert.NoError(t, err) //nolint:testifylint
 	EvaluateRegoEntitlementsKeycloak(t, input, expected)
 }
 
+// EvaluateRegoEntitlementsKeycloak tests the keycloak builtin - better in an integration test
 func EvaluateRegoEntitlementsKeycloak(t *testing.T, input interface{}, expected []interface{}) {
 	// instantiate built-ins
 	idpplugin.KeycloakBuiltins()
@@ -92,29 +175,38 @@ func EvaluateRegoEntitlementsKeycloak(t *testing.T, input interface{}, expected 
 }
 
 func TestRegoConditionEmpty(t *testing.T) {
-	var input interface{}
-	_ = json.Unmarshal(
-		[]byte("{}"),
-		&input,
-	)
 	expected := false
+	inputJSON := `{}`
+	var input interface{}
+	_ = json.Unmarshal([]byte(inputJSON), &input)
 	EvaluateRegoConditions(t, input, expected)
 }
 
 func TestRegoConditionSimple(t *testing.T) {
-	var input interface{}
-	_ = json.Unmarshal(
-		[]byte("{\"payload\":{\"groups\":[\"groupA\"]},\"condition\":{\"subject_external_field\":\"groups\",\"operator\":1,\"subject_external_values\":[\"groupA\"]}}"),
-		&input,
-	)
 	expected := true
+	inputJSON := `{
+  "payload": {
+	"groups": ["groupA"]
+  },
+  "condition": {
+	"subject_external_field": "groups",
+	"operator": 1,
+	"subject_external_values": [
+	  "groupA"
+	]
+  }
+}`
+	var input interface{}
+	err := json.Unmarshal([]byte(inputJSON), &input)
+	assert.NoError(t, err) //nolint:testifylint
 	EvaluateRegoConditions(t, input, expected)
 }
 
+// EvaluateRegoConditions uses a conditions test file to do function level testing
 func EvaluateRegoConditions(t *testing.T, input interface{}, expected bool) {
 	ctx := context.Background()
 	policy, err := os.ReadFile("entitlements/entitlements.rego")
-	if assert.NoError(t, err) {
+	if assert.NoError(t, err) { //nolint:nestif,nestif testify linted
 		policyTest, err := os.ReadFile("entitlements/conditions-test.rego")
 		if assert.NoError(t, err) {
 			regoObj := rego.New(

ac6431c7163312494308aa73090a96e4e4bc6d05
Krish Suchak <suchak.krish@gmail.com>
2024-04-08 13:19:35 -0400

    get namespace

--- service/policy/db/namespaces.go
+++ service/policy/db/namespaces.go
@@ -76,7 +76,7 @@ func getNamespaceSql(id string, opts namespaceSelectOptions) (string, []interfac
 		t.Field("id"),
 		t.Field("name"),
 		t.Field("active"),
-		t.Field("metadata"),
+		"JSON_STRIP_NULLS(JSON_BUILD_OBJECT('labels', metadata->'labels', 'created_at', \"created_at\", 'updated_at', \"updated_at\")) as metadata",
 	}
 
 	if opts.withFqn {

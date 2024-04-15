008aa26b04f1b502376772f88cf6e430f65121c5
Morgan Kleene <mkleene@virtru.com>
2024-04-15 09:37:19 -0400

    Update options.go

--- sdk/options.go
+++ sdk/options.go
@@ -70,7 +70,8 @@ func WithCustomAuthorizationConnection(conn *grpc.ClientConn) Option {
 // WithTokenExchange specifies that the SDK should obtain its
 // access token by exchanging the given token for a new one. If
 // the subjectToken is for another client keycloak requires that
-// this ClientID is included in the audience
+// the ClientID that was granted the `subjectToken` is include
+// in the `audience`
 func WithTokenExchange(subjectToken string, audience []string) Option {
 	return func(c *config) {
 		c.tokenExchange = &oauth.TokenExchangeInfo{

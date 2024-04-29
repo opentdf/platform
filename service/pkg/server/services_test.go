019a3bf37d534359950110f1b077ef4f860f1c60
Ryan Schumacher <jschumacher@virtru.com>
2024-04-29 12:11:02 -0500

    feat(core): add service scoped database clients (#647)
    This PR introduces a significant change to how we perceive database
    clients within the platform. The original implementation has a concept
    of a single database client which services could use. As work was being
    executed, it became clear that the policy service was one (if not only)
    service which required a database client.
    
    Recently, we learned that downstream projects that wrap OpenTDF required
    access to a database client, but this posed a couple concerns:
    
    1. Downstream clients need the ability to add migrations and have those
    bound to the migration CLI tool as well as the auto-migration feature
    2. We need to ensure a bad actor cannot poison a PEP and thus have easy
    access to mutate the policy outside an audited and authenticated flow
    3. We need to ensure PEPs are interacting with services via contracts
    and not by direct database access, which could slow down development
    
    ## Supported features
    
    - create a new database client for each service / PEP
    - this sets us up to support limiting scope in the future by giving
    theoretical access to managing multiple DB user identities per instance
    of the platform
    - scope the database client to a schema scoped to the service namespace
    - this will give us theoretical control over [limiting the
    access](https://stackoverflow.com/a/17355059/79201) of the database user
    - this might need to change to a database per service depending on the
    access controls needed
    - add support for service based migrations
    - having all services use the same migrations directory is brittle since
    clients are scoped to schemas we can support multiple migration
    directories
    - additionally, when deploying a policy service apart from another PEP
    we don't want to automatically migrate a schema that won't be used
    - migration commands now support service and/or `--all`
    - this enables developers to migrate up and down as they are exploring
    the process of supporting a new feature
    - this gives us the opportunity to enhance the tool to support
    migrations for services that are currently "enabled"
    
    ## Unsupported features
    
    - robust support for service auto-migration based on server registration
    - rigorous performance testing
    - DX review

---
+++ service/pkg/server/services_test.go
@@ -0,0 +1,168 @@
+package server
+
+import (
+	"context"
+	"testing"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/opentdf/platform/service/internal/config"
+	"github.com/opentdf/platform/service/internal/db"
+	"github.com/opentdf/platform/service/pkg/serviceregistry"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc"
+)
+
+type spyTestService struct {
+	wasCalled  bool
+	callParams []any
+}
+
+type mockTestServiceOptions struct {
+	namespace          string
+	serviceName        string
+	serviceHandlerType any
+	serviceObject      any
+	serviceHandler     func(ctx context.Context, mux *runtime.ServeMux, server any) error
+	dbRegister         serviceregistry.DBRegister
+}
+
+func mockTestServiceRegistry(opts mockTestServiceOptions) (func() error, *spyTestService) {
+	spy := &spyTestService{}
+	mockTestServiceDefaults := mockTestServiceOptions{
+		namespace:          "test",
+		serviceName:        "TestService",
+		serviceHandlerType: (*interface{})(nil),
+		serviceHandler: func(_ context.Context, _ *runtime.ServeMux, _ any) error {
+			return nil
+		},
+	}
+
+	namespace := mockTestServiceDefaults.namespace
+	serviceName := mockTestServiceDefaults.serviceName
+	serviceHandler := mockTestServiceDefaults.serviceHandler
+	serviceHandlerType := mockTestServiceDefaults.serviceHandlerType
+
+	if opts.namespace != "" {
+		namespace = opts.namespace
+	}
+	if opts.serviceName != "" {
+		serviceName = opts.serviceName
+	}
+	if opts.serviceHandler != nil {
+		serviceHandler = opts.serviceHandler
+	}
+
+	return func() error {
+		return serviceregistry.RegisterService(serviceregistry.Registration{
+			Namespace: namespace,
+			ServiceDesc: &grpc.ServiceDesc{
+				ServiceName: serviceName,
+				HandlerType: serviceHandlerType,
+			},
+			RegisterFunc: func(srp serviceregistry.RegistrationParams) (any, serviceregistry.HandlerServer) {
+				return opts.serviceObject, func(ctx context.Context, mux *runtime.ServeMux, server any) error {
+					spy.wasCalled = true
+					spy.callParams = append(spy.callParams, srp, ctx, mux, server)
+					return serviceHandler(ctx, mux, server)
+				}
+			},
+
+			DB: opts.dbRegister,
+		})
+	}, spy
+}
+
+type ServiceTestSuite struct {
+	suite.Suite
+}
+
+func TestServiceTestSuite(t *testing.T) {
+	suite.Run(t, new(StartTestSuite))
+}
+
+func (suite *ServiceTestSuite) BeforeTest(_, _ string) {
+	serviceregistry.RegisteredServices = make(serviceregistry.NamespaceMap)
+}
+
+func (suite *ServiceTestSuite) TestRegisterServicesIsSuccessful() {
+	t := suite.T()
+	err := registerServices()
+	assert.NoError(t, err)
+}
+
+func (suite *ServiceTestSuite) TestStartServicesWithVariousCases() {
+	t := suite.T()
+	ctx := context.Background()
+
+	// Test service which will be enabled
+	registerTest, testSpy := mockTestServiceRegistry(mockTestServiceOptions{})
+	err := registerTest()
+	require.NoError(t, err)
+
+	// Test service with DB which will be enabled
+	registerTestWithDB, testWithDBSpy := mockTestServiceRegistry(mockTestServiceOptions{
+		namespace:   "test_with_db",
+		serviceName: "TestWithDBService",
+		dbRegister: serviceregistry.DBRegister{
+			Required: true,
+		},
+	})
+	err = registerTestWithDB()
+	require.NoError(t, err)
+
+	// FooBar service which won't be enabled
+	registerFoobar, foobarSpy := mockTestServiceRegistry(mockTestServiceOptions{
+		namespace:   "foobar",
+		serviceName: "FooBarService",
+	})
+	err = registerFoobar()
+	require.NoError(t, err)
+
+	otdf, err := mockOpenTDFServer()
+	require.NoError(t, err)
+
+	cF, services, err := startServices(ctx, config.Config{
+		DB: db.Config{
+			Host:          "localhost",
+			Port:          5432,
+			Database:      "opentdf",
+			User:          "",
+			Password:      "",
+			RunMigrations: false,
+		},
+		Services: map[string]serviceregistry.ServiceConfig{
+			"test": {
+				Enabled: true,
+			},
+			"test_with_db": {
+				Enabled: true,
+			},
+			"foobar": {
+				Enabled: false,
+			},
+		},
+	}, otdf, nil, nil)
+	require.NoError(t, err)
+	require.NotNil(t, cF)
+	assert.Lenf(t, services, 2, "expected 2 services enabled, got %d", len(services))
+
+	// Expecting a test service with no DBClient
+	assert.True(t, testSpy.wasCalled)
+	regParams, ok := testSpy.callParams[0].(serviceregistry.RegistrationParams)
+	require.True(t, ok)
+	assert.Nil(t, regParams.DBClient)
+
+	// Expecting a test service with a DBClient
+	assert.True(t, testWithDBSpy.wasCalled)
+	regParams, ok = testWithDBSpy.callParams[0].(serviceregistry.RegistrationParams)
+	require.True(t, ok)
+	assert.NotNil(t, regParams.DBClient)
+
+	// Not expecting a foobar service since it's not enabled
+	assert.False(t, foobarSpy.wasCalled)
+
+	// call close function
+	cF()
+}
--- service/policy/policy.go
+++ service/policy/policy.go
@@ -1,17 +1,30 @@
 package policy
 
 import (
+	"embed"
+
 	"github.com/opentdf/platform/service/pkg/serviceregistry"
 	"github.com/opentdf/platform/service/policy/attributes"
+	"github.com/opentdf/platform/service/policy/db/migrations"
 	"github.com/opentdf/platform/service/policy/kasregistry"
 	"github.com/opentdf/platform/service/policy/namespaces"
 	"github.com/opentdf/platform/service/policy/resourcemapping"
 	"github.com/opentdf/platform/service/policy/subjectmapping"
 )
 
+var Migrations *embed.FS
+
+func init() {
+	Migrations = &migrations.FS
+}
+
 func NewRegistrations() []serviceregistry.Registration {
 	registrations := []serviceregistry.Registration{}
 	namespace := "policy"
+	dbRegister := serviceregistry.DBRegister{
+		Required:   true,
+		Migrations: Migrations,
+	}
 
 	for _, r := range []serviceregistry.Registration{
 		attributes.NewRegistration(),
@@ -21,6 +34,7 @@ func NewRegistrations() []serviceregistry.Registration {
 		kasregistry.NewRegistration(),
 	} {
 		r.Namespace = namespace
+		r.DB = dbRegister
 		registrations = append(registrations, r)
 	}
 

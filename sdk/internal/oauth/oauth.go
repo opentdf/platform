edd5343b82ffff77b04f8a74ebbab0fd63a03ebc
Paul Flynn <pflynn@virtru.com>
2024-04-30 12:12:43 -0400

    feat(sdk): test

--- sdk/internal/oauth/oauth.go
+++ sdk/internal/oauth/oauth.go
@@ -313,11 +313,6 @@ func DoCertExchange(ctx context.Context, client *http.Client, tokenEndpoint stri
 	if err != nil {
 		return nil, err
 	}
-	transport, ok := client.Transport.(*http.Transport)
-	if !ok {
-		return nil, fmt.Errorf("client.Transport is not a *http.Transport")
-	}
-	transport.TLSClientConfig = exchangeInfo.TLSConfig
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("error making request to IdP for certificate exchange: %w", err)

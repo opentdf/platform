fcdde8786345a64d2b5a3b2a9b6c40c5c6c69c38
Nick Trevino <ntrevino@virtru.com>
2024-05-06 17:58:04 -0600

    Fix examples.

--- examples/cmd/encrypt.go
+++ examples/cmd/encrypt.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/opentdf/platform/sdk"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var encryptCmd = &cobra.Command{
@@ -28,12 +30,24 @@ func encrypt(cmd *cobra.Command, args []string) error {
 
 	plainText := args[0]
 	strReader := strings.NewReader(plainText)
+	platformEndpoint := cmd.Context().Value(RootConfigKey).(*ExampleConfig).PlatformEndpoint
+	kasUrl := fmt.Sprintf("http://%s", platformEndpoint)
 
 	// Create new offline client
-	client, err := sdk.New(cmd.Context().Value(RootConfigKey).(*ExampleConfig).PlatformEndpoint,
+	client, err := sdk.New(
+		platformEndpoint,
 		sdk.WithInsecurePlaintextConn(),
 		sdk.WithClientCredentials("opentdf-sdk", "secret", nil),
 		sdk.WithTokenEndpoint("http://localhost:8888/auth/realms/opentdf/protocol/openid-connect/token"),
+		sdk.WithKnownKas([]sdk.KASInfo{
+			sdk.KASInfo{
+				// examples assume insecure http
+				DialOptions: []grpc.DialOption{
+					grpc.WithTransportCredentials(insecure.NewCredentials()),
+				},
+				URL:       kasUrl,
+			},
+		}),
 	)
 	if err != nil {
 		return err
@@ -47,12 +61,8 @@ func encrypt(cmd *cobra.Command, args []string) error {
 
 	tdf, err := client.CreateTDF(tdfFile, strReader,
 		//sdk.WithDataAttributes("https://example.com/attr/attr1/value/value1"),
-		sdk.WithKasInformation(
-			sdk.KASInfo{
-				// examples assume insecure http
-				URL:       fmt.Sprintf("http://%s", cmd.Flag("platformEndpoint").Value.String()),
-				PublicKey: "",
-			}))
+		client.WithKnownKas(kasUrl),
+	)
 	if err != nil {
 		return err
 	}

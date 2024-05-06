41535bfc9e65d02ded47f903611d54d8577ead5b
Nick Trevino <ntrevino@virtru.com>
2024-05-06 17:23:26 -0600

    CCR-2825: Do not verify when no keys are provided.
    Previously an error such as this would be output:
    msg="unable to verify parse token" err="jwt.Parse: no keys for verification are provided (use jwt.WithVerify(false) to explicitly skip)"

--- service/kas/access/rewrap.go
+++ service/kas/access/rewrap.go
@@ -87,7 +87,7 @@ func verifySignedRequestToken(ctx context.Context, in *kaspb.RewrapRequest) (*Re
 		// 1. auth is disabled so we can't get a DPoP JWK
 		// 2. auth is enabled _but_ we aren't requiring DPoP
 		// in either case letting the request through makes sense
-		token, err = jwt.Parse([]byte(in.GetSignedRequestToken()), jwt.WithValidate(false))
+		token, err = jwt.Parse([]byte(in.GetSignedRequestToken()), jwt.WithVerify(false))
 		if err != nil {
 			slog.WarnContext(ctx, "unable to verify parse token", "err", err)
 			return nil, err401("could not parse token")

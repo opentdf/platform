24335e7ae28f05b2bb6e6473ecff763252bcea21
jakedoublev <jake.vanvorhis@virtru.com>
2024-05-14 10:55:02 -0700

    put back fixtures provisioning db schema from config

--- service/internal/fixtures/db.go
+++ service/internal/fixtures/db.go
@@ -20,7 +20,7 @@ type DBInterface struct {
 
 func NewDBInterface(cfg config.Config) DBInterface {
 	config := cfg.DB
-
+	config.Schema = cfg.DB.Schema
 	c, err := db.New(context.Background(), config)
 	if err != nil {
 		slog.Error("issue creating database client", slog.String("error", err.Error()))

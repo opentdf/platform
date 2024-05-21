d9e2d11acb454823eaa7c6248160da5a4135f1bc
Pat Mancuso <pmancuso@virtru.com>
2024-05-21 18:16:45 -0400

    Add another config test

--- sdk/nanotdf_config_test.go
+++ sdk/nanotdf_config_test.go
@@ -5,9 +5,11 @@ import (
 	"testing"
 )
 
+// TestNanoTDFConfig1 - Create a new config, verify that the config contains valid PEMs for the key pair
 func TestNanoTDFConfig1(t *testing.T) {
 
-	conf, err := NewNanoTDFConfig()
+	var s SDK
+	conf, err := s.NewNanoTDFConfig()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -26,5 +28,30 @@ func TestNanoTDFConfig1(t *testing.T) {
 	}
 	if len(privateKey) == 0 {
 		t.Fatal("no private key")
+	}
+}
+
+// TestNanoTDFConfig2 - set kas url, retrieve kas url, verify value is correct
+func TestNanoTDFConfig2(t *testing.T) {
+	const (
+		kasUrl = "https://test.virtru.com"
+	)
+
+	var s SDK
+	conf, err := s.NewNanoTDFConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = conf.SetKasUrl(kasUrl)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	readKasUrl, err := conf.kasURL.getUrl()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if readKasUrl != kasUrl {
+		t.Fatalf("expect %s, got %s", kasUrl, readKasUrl)
 	}
 }

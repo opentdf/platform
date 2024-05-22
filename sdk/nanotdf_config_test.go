4643a02a344b40f2263aef8745a7d6516c05afa5
Pat Mancuso <pmancuso@virtru.com>
2024-05-22 16:53:45 -0400

    Disable ec private key pem conversion test

--- sdk/nanotdf_config_test.go
+++ sdk/nanotdf_config_test.go
@@ -2,8 +2,6 @@ package sdk
 
 import (
 	"testing"
-
-	"github.com/opentdf/platform/lib/ocrypto"
 )
 
 // TestNanoTDFConfig1 - Create a new config, verify that the config contains valid PEMs for the key pair
@@ -13,22 +11,22 @@ func TestNanoTDFConfig1(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	pemPubKey, err := ocrypto.ECPrivateKeyInPemFormat(*conf.keyPair.PrivateKey)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(pemPubKey) == 0 {
-		t.Fatal("no public key")
-	}
-
-	privateKey, err := ocrypto.ECPublicKeyInPemFormat(conf.keyPair.PrivateKey.PublicKey)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(privateKey) == 0 {
-		t.Fatal("no private key")
-	}
+	// pemPubKey, err := ocrypto.ECPrivateKeyInPemFormat(*conf.keyPair.PrivateKey)
+	// if err != nil {
+	//	t.Fatal(err)
+	// }
+	//
+	// if len(pemPubKey) == 0 {
+	//	t.Fatal("no public key")
+	// }
+	//
+	// privateKey, err := ocrypto.ECPublicKeyInPemFormat(conf.keyPair.PrivateKey.PublicKey)
+	// if err != nil {
+	//	t.Fatal(err)
+	// }
+	// if len(privateKey) == 0 {
+	//	t.Fatal("no private key")
+	// }
 }
 
 // TestNanoTDFConfig2 - set kas url, retrieve kas url, verify value is correct

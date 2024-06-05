ebcabad361f14f464674f5afb4ad781fe19c5f6f
David Mihalcik <dmihalcik@virtru.com>
2024-06-04 20:55:28 -0400

    feat(core): Adds 'fixture create key' command WIP

---
+++ lib/ocrypto/keymgmt/internal/cmd/createkey.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func Execute() {
+	cmd := &cobra.Command{
+		Use:   "keymgmt",
+		Short: "Manage KAS keypairs for OpenTDF",
+	}
+
+	cmd.AddCommand(&cobra.Command{
+		Use:   "init",
+		Short: "Create new keypair(s) with the given algorithms",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := someFunc(); err != nil {
+				return err
+			}
+			return nil
+		},
+	})
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func generateRsaKeyPair() (*rsa.PrivateKey, *rsa.PublicKey) {
+	privkey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		
+	}
+	return privkey, &privkey.PublicKey
+}
+
+func ExportRsaPrivateKeyAsPemStr(privkey *rsa.PrivateKey) string {
+	privkey_bytes := x509.MarshalPKCS1PrivateKey(privkey)
+	privkey_pem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: privkey_bytes,
+		},
+	)
+	return string(privkey_pem)
+}
+
+func ParseRsaPrivateKeyFromPemStr(privPEM string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(privPEM))
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return priv, nil
+}
+
+func ExportRsaPublicKeyAsPemStr(pubkey *rsa.PublicKey) (string, error) {
+	pubkey_bytes, err := x509.MarshalPKIXPublicKey(pubkey)
+	if err != nil {
+		return "", err
+	}
+	pubkey_pem := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: pubkey_bytes,
+		},
+	)
+
+	return string(pubkey_pem), nil
+}
+
+func ParseRsaPublicKeyFromPemStr(pubPEM string) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode([]byte(pubPEM))
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	switch pub := pub.(type) {
+	case *rsa.PublicKey:
+		return pub, nil
+	default:
+		break // fall through
+	}
+	return nil, errors.New("Key type is not RSA")
+}
+
+func main() {
+
+	// Create the keys
+	priv, pub := GenerateRsaKeyPair()
+
+	// Export the keys to pem string
+	priv_pem := ExportRsaPrivateKeyAsPemStr(priv)
+	pub_pem, _ := ExportRsaPublicKeyAsPemStr(pub)
+
+	// Import the keys from pem string
+	priv_parsed, _ := ParseRsaPrivateKeyFromPemStr(priv_pem)
+	pub_parsed, _ := ParseRsaPublicKeyFromPemStr(pub_pem)
+
+	// Export the newly imported keys
+	priv_parsed_pem := ExportRsaPrivateKeyAsPemStr(priv_parsed)
+	pub_parsed_pem, _ := ExportRsaPublicKeyAsPemStr(pub_parsed)
+
+	fmt.Println(priv_parsed_pem)
+	fmt.Println(pub_parsed_pem)
+
+	// Check that the exported/imported keys match the original keys
+	if priv_pem != priv_parsed_pem || pub_pem != pub_parsed_pem {
+		fmt.Println("Failure: Export and Import did not result in same Keys")
+	} else {
+		fmt.Println("Success")
+	}
+}
---
+++ lib/ocrypto/keymgmt/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/opentdf/platform/lib/ocrypto/keymgmt/internal/cmd"
+
+// Utility for creating keypairs for KAS managememnt.
+// To sign a keypair,
+func main() {
+	cmd.Execute()
+}

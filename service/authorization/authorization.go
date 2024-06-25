ba1cf89c98396460385b6b4f72986cab55976693
Elizabeth Healy <ehealy@virtru.com>
2024-06-25 12:56:02 -0400

    check for status instead of string

--- service/authorization/authorization.go
+++ service/authorization/authorization.go
@@ -177,7 +177,7 @@ func (as *AuthorizationService) GetDecisions(ctx context.Context, req *authoriza
 			if err != nil {
 				// if attribute an FQN does not exist
 				// return deny for all entity chains aginst this RA set and continue to next
-				if strings.Contains(err.Error(), db.ErrTextNotFound) {
+				if errors.Is(err, db.StatusifyError(db.ErrNotFound, "")) {
 					for _, ec := range dr.GetEntityChains() {
 						decisionResp := &authorization.DecisionResponse{
 							Decision:      authorization.DecisionResponse_DECISION_DENY,

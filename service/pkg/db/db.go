8d7fbf28e0d2c6c5935874811a6c1af09321ae80
jakedoublev <jake.vanvorhis@virtru.com>
2024-06-28 08:10:44 -0700

    set the search_path for the client

--- service/pkg/db/db.go
+++ service/pkg/db/db.go
@@ -132,6 +132,14 @@ func New(ctx context.Context, config Config, o ...OptsFunc) (*Client, error) {
 		}
 	}
 
+	// Set the Client search_path to the schema
+	q := fmt.Sprintf("SET search_path TO %s", config.Schema)
+	if _, err := c.Pgx.Exec(ctx, q); err != nil {
+		return nil, fmt.Errorf("failed to SET search_path for db Client schema to [%s]: %w", config.Schema, err)
+	} else {
+		slog.Info("successfully set database client search_path", slog.String("schema", config.Schema))
+	}
+
 	return &c, nil
 }
 

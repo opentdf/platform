fc98bcb7cc46de1c67fedcc03a6edb9576ae204b
David Mihalcik <dmihalcik@virtru.com>
2024-07-12 16:25:29 -0400

    Update attributes.go

--- examples/cmd/attributes.go
+++ examples/cmd/attributes.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"net/url"
+	"regexp"
 	"strings"
 
 	"github.com/opentdf/platform/protocol/go/policy"
 	"github.com/opentdf/platform/protocol/go/policy/attributes"
 	"github.com/opentdf/platform/protocol/go/policy/namespaces"
+	"github.com/opentdf/platform/sdk"
 	"github.com/spf13/cobra"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/encoding/protojson"
 )
 
 var (
@@ -34,15 +34,16 @@ func init() {
 	}
 	list := &cobra.Command{
 		Use:     "list",
+		Args:    cobra.NoArgs,
 		Aliases: []string{"ls"},
 		Short:   "list available policy attributes",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return listAttributes(cmd)
 		},
 	}
-	add.Flags().StringVarP(&ns, "namespace", "N", "https://example.com/", "space separated namespace uris to search")
-
 	attributes.AddCommand(add)
+
+	list.Flags().StringVarP(&ns, "namespace", "N", "", "space separated namespace uris to search")
 	attributes.AddCommand(list)
 
 	ExamplesCmd.AddCommand(attributes)
@@ -74,81 +75,109 @@ func listAttributes(cmd *cobra.Command) error {
 		nsuris = strings.Split(ns, " ")
 	}
 	for _, n := range nsuris {
+		u, err := url.Parse(n)
+		if err != nil {
+			return err
+		}
 		lsr, err := s.Attributes.ListAttributes(ctx, &attributes.ListAttributesRequest{
-			Namespace: n,
+			// namespace here must be the namespace name
+			Namespace: u.Host,
 		})
 		if err != nil {
 			return err
 		}
 		slog.Info(fmt.Sprintf("found %d attributes in namespace", len(lsr.GetAttributes())), "ns", n)
 		for _, a := range lsr.GetAttributes() {
-			fmt.Println("%v", a)
+			fmt.Printf("%s\n", a.GetFqn())
+			for _, v := range a.GetValues() {
+				fmt.Printf("\t%s\n", v.GetFqn())
+			}
 		}
 	}
 	return nil
 }
 
 func addAttributes(cmd *cobra.Command, fqns ...string) error {
-	s, err := sdk.New(platformEndpoint, sdk.WithInsecurePlaintextConn())
+	s, err := newSDK()
 	if err != nil {
 		slog.Error("could not connect", slog.String("error", err.Error()))
 		return err
 	}
 	defer s.Close()
 
-	var exampleNamespace *policy.Namespace
-	slog.Info("listing namespaces")
-	listResp, err := s.Namespaces.ListNamespaces(context.Background(), &namespaces.ListNamespacesRequest{})
-	if err != nil {
-		return err
-	}
-	slog.Info(fmt.Sprintf("found %d namespaces", len(listResp.Namespaces)))
-	for _, ns := range listResp.GetNamespaces() {
-		slog.Info(fmt.Sprintf("existing namespace; name: %s, id: %s", ns.Name, ns.Id))
-		if ns.Name == "example" {
-			exampleNamespace = ns
+	vre := regexp.MustCompile(`^(https?://[\w./]+)/attr/(\S*)/value/(\S*)$`)
+	are := regexp.MustCompile(`^(https?://[\w./]+)/attr/([^/\s]*)$`)
+	nre := regexp.MustCompile(`^(https?://[\w./]+)/?$`)
+	for _, u := range fqns {
+		e := fmt.Errorf("not a valid attribute fqn [%s]", u)
+		if m := vre.FindStringSubmatch(u); len(m) == 4 && len(m[0]) > 0 {
+			auth := m[1]
+			attr, err := url.PathUnescape(m[2])
+			if err != nil {
+				return e
+			}
+			val, err := url.PathUnescape(m[3])
+			if err != nil {
+				return e
+			}
+			aid, err := upsertAttr(cmd.Context(), s, auth, attr)
+			if err != nil {
+				return err
+			}
+			cavr, err := s.Attributes.CreateAttributeValue(cmd.Context(), &attributes.CreateAttributeValueRequest{
+				AttributeId: aid,
+				Value:       val,
+			})
+			if err != nil {
+				return err
+			}
+			slog.Info("created attribute value", "passedin", u, "computed", cavr.GetValue().GetFqn())
+		} else if m := are.FindStringSubmatch(u); len(m) == 3 && len(m[0]) > 0 {
+			auth := m[1]
+			attr, err := url.PathUnescape(m[2])
+			if err != nil {
+				return err
+			}
+			aid, err := upsertAttr(cmd.Context(), s, auth, attr)
+			if err != nil {
+				return err
+			}
+			slog.Info("created attribute", "passedin", u, "id", aid)
+		} else if m := nre.FindStringSubmatch(u); len(m) == 2 && len(m[0]) > 0 {
+			slog.Error("unimplemented: create namespace")
+			return e
+		} else {
+			return e
 		}
 	}
+	return nil
+}
 
-	if exampleNamespace == nil {
-		slog.Info("creating new namespace")
-		resp, err := s.Namespaces.CreateNamespace(context.Background(), &namespaces.CreateNamespaceRequest{
-			Name: "example.io",
+func upsertAttr(ctx context.Context, s *sdk.SDK, auth, attr string) (string, error) {
+	fqn := fmt.Sprintf("%s/attr/%s", auth, url.PathEscape(attr))
+	av, err :=
+		s.Attributes.GetAttributeValuesByFqns(ctx, &attributes.GetAttributeValuesByFqnsRequest{
+			Fqns: []string{fqn},
+			WithValue: &policy.AttributeValueSelector{
+				WithKeyAccessGrants: true,
+			},
 		})
-		if err != nil {
-			return err
-		}
-		exampleNamespace = resp.Namespace
+	if err != nil {
+		return "", err
 	}
-
-	slog.Info("creating new attribute with hierarchy rule")
-	_, err = s.Attributes.CreateAttribute(context.Background(), &attributes.CreateAttributeRequest{
-		Name:        "IntellectualProperty",
-		NamespaceId: exampleNamespace.Id,
-		Rule:        *policy.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_HIERARCHY.Enum(),
-		// Values: []*attributes.ValueCreateUpdate{
-		// 	{Value: "TradeSecret"},
-		// 	{Value: "Proprietary"},
-		// 	{Value: "BusinessSensitive"},
-		// 	{Value: "Open"},
-		// },
+	if len(av.GetFqnAttributeValues()) == 1 {
+		return av.GetFqnAttributeValues()[fqn].GetAttribute().GetId(), nil
+	}
+	u, err := url.Parse(auth)
+	if err != nil {
+		return "", err
+	}
+	a, err := s.Attributes.CreateAttribute(ctx, &attributes.CreateAttributeRequest{
+		Name: u.Host,
 	})
 	if err != nil {
-		if returnStatus, ok := status.FromError(err); ok && returnStatus.Code() == codes.AlreadyExists {
-			slog.Info("attribute already exists")
-		} else {
-			slog.Error("could not create attribute", slog.String("error", err.Error()))
-			return err
-		}
-	} else {
-		slog.Info("attribute created")
+		return "", err
 	}
 
-	allAttr, err := s.Attributes.ListAttributes(context.Background(), &attributes.ListAttributesRequest{})
-	if err != nil {
-		slog.Error("could not list attributes", slog.String("error", err.Error()))
-		return err
-	}
-	slog.Info(fmt.Sprintf("list attributes response: %s", protojson.Format(allAttr)))
-	return nil
+	return a.Attribute.GetId(), nil
 }

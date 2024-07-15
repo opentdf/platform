f0afb536ce08d3f8a75a7b3a73d866b6edc49d84
David Mihalcik <dmihalcik@virtru.com>
2024-07-15 07:51:50 -0400

    Update attributes.go

--- examples/cmd/attributes.go
+++ examples/cmd/attributes.go
@@ -16,7 +16,10 @@ import (
 )
 
 var (
-	ns string
+	ns     string
+	attr   string
+	rule   string
+	values string
 )
 
 func init() {
@@ -27,11 +30,16 @@ func init() {
 
 	add := &cobra.Command{
 		Use:  "add",
-		Args: cobra.MinimumNArgs(1),
+		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return addAttributes(cmd, args...)
+			return addAttribute(cmd)
 		},
 	}
+	add.Flags().StringVarP(&attr, "attr", "a", "", "attribute prefix, e.g. http://name.space/attr/name")
+	add.Flags().StringVarP(&rule, "rule", "", "allof", "attribute type, either allof, anyof, or hierarchy")
+	add.Flags().StringVarP(&values, "values", "v", "", "list of attribute values")
+	attributes.AddCommand(add)
+
 	list := &cobra.Command{
 		Use:     "list",
 		Args:    cobra.NoArgs,
@@ -41,7 +49,6 @@ func init() {
 			return listAttributes(cmd)
 		},
 	}
-	attributes.AddCommand(add)
 
 	list.Flags().StringVarP(&ns, "namespace", "N", "", "space separated namespace uris to search")
 	attributes.AddCommand(list)
@@ -97,7 +104,7 @@ func listAttributes(cmd *cobra.Command) error {
 	return nil
 }
 
-func addAttributes(cmd *cobra.Command, fqns ...string) error {
+func addAttribute(cmd *cobra.Command) error {
 	s, err := newSDK()
 	if err != nil {
 		slog.Error("could not connect", slog.String("error", err.Error()))
@@ -105,79 +112,52 @@ func addAttributes(cmd *cobra.Command, fqns ...string) error {
 	}
 	defer s.Close()
 
-	vre := regexp.MustCompile(`^(https?://[\w./]+)/attr/(\S*)/value/(\S*)$`)
 	are := regexp.MustCompile(`^(https?://[\w./]+)/attr/([^/\s]*)$`)
-	nre := regexp.MustCompile(`^(https?://[\w./]+)/?$`)
-	for _, u := range fqns {
-		e := fmt.Errorf("not a valid attribute fqn [%s]", u)
-		if m := vre.FindStringSubmatch(u); len(m) == 4 && len(m[0]) > 0 {
-			auth := m[1]
-			attr, err := url.PathUnescape(m[2])
-			if err != nil {
-				return e
-			}
-			val, err := url.PathUnescape(m[3])
-			if err != nil {
-				return e
-			}
-			aid, err := upsertAttr(cmd.Context(), s, auth, attr)
-			if err != nil {
-				return err
-			}
-			cavr, err := s.Attributes.CreateAttributeValue(cmd.Context(), &attributes.CreateAttributeValueRequest{
-				AttributeId: aid,
-				Value:       val,
-			})
-			if err != nil {
-				return err
-			}
-			slog.Info("created attribute value", "passedin", u, "computed", cavr.GetValue().GetFqn())
-		} else if m := are.FindStringSubmatch(u); len(m) == 3 && len(m[0]) > 0 {
-			auth := m[1]
-			attr, err := url.PathUnescape(m[2])
-			if err != nil {
-				return err
-			}
-			aid, err := upsertAttr(cmd.Context(), s, auth, attr)
-			if err != nil {
-				return err
-			}
-			slog.Info("created attribute", "passedin", u, "id", aid)
-		} else if m := nre.FindStringSubmatch(u); len(m) == 2 && len(m[0]) > 0 {
-			slog.Error("unimplemented: create namespace")
-			return e
-		} else {
-			return e
+	if m := are.FindStringSubmatch(attr); len(m) == 3 && len(m[0]) > 0 {
+		auth := m[1]
+		authuri, err := url.Parse(auth)
+		if err != nil {
+			return err
 		}
+		attr, err := url.PathUnescape(m[2])
+		nsname := authuri.Host
+		if err != nil {
+			return err
+		}
+		aid, err := upsertAttr(cmd.Context(), s, nsname, attr, strings.Split(values, " "))
+		if err != nil {
+			return err
+		}
+		slog.Info("created attribute", "passedin", attr, "id", aid)
+	} else {
+		return fmt.Errorf("not a valid attribute fqn [%s]", attr)
 	}
 	return nil
 }
 
-func upsertAttr(ctx context.Context, s *sdk.SDK, auth, attr string) (string, error) {
-	fqn := fmt.Sprintf("%s/attr/%s", auth, url.PathEscape(attr))
+func ruler() policy.AttributeRuleTypeEnum {
+	switch strings.ToLower(rule) {
+	case "allof":
+		return policy.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_ALL_OF
+	case "anyof":
+		return policy.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_ALL_OF
+	case "hierarchy":
+		return policy.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_ALL_OF
+	default:
+		return policy.AttributeRuleTypeEnum_ATTRIBUTE_RULE_TYPE_ENUM_ALL_OF
+	}
+}
+
+func upsertAttr(ctx context.Context, s *sdk.SDK, auth, name string, values []string) (string, error) {
 	av, err :=
-		s.Attributes.GetAttributeValuesByFqns(ctx, &attributes.GetAttributeValuesByFqnsRequest{
-			Fqns: []string{fqn},
-			WithValue: &policy.AttributeValueSelector{
-				WithKeyAccessGrants: true,
-			},
+		s.Attributes.CreateAttribute(ctx, &attributes.CreateAttributeRequest{
+			NamespaceId: auth,
+			Name:        name,
+			Rule:        ruler(),
+			Values:      values,
 		})
 	if err != nil {
 		return "", err
 	}
-	if len(av.GetFqnAttributeValues()) == 1 {
-		return av.GetFqnAttributeValues()[fqn].GetAttribute().GetId(), nil
-	}
-	u, err := url.Parse(auth)
-	if err != nil {
-		return "", err
-	}
-	a, err := s.Attributes.CreateAttribute(ctx, &attributes.CreateAttributeRequest{
-		Name: u.Host,
-	})
-	if err != nil {
-		return "", err
-	}
-
-	return a.Attribute.GetId(), nil
+	return av.Attribute.GetId(), nil
 }

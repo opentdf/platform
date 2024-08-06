a9d440e56c72d23ac8390f72a8e1246b69a2935f
David Mihalcik <dmihalcik@virtru.com>
2024-08-06 11:51:21 -0400

    feat(core): Lets key init make jwk sets

--- service/cmd/keys.go
+++ service/cmd/keys.go
@@ -1,18 +1,21 @@
 package cmd
 
 import (
+	"crypto"
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/json"
 	"encoding/pem"
 	"fmt"
 	"math/big"
 	"os"
 	"time"
 
+	"github.com/lestrrat-go/jwx/v2/jwk"
 	"github.com/spf13/cobra"
 )
 
@@ -30,7 +33,7 @@ func init() {
 	initCmd := &cobra.Command{
 		Use:  "init",
 		Args: cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
+		RunE: func(_ *cobra.Command, _ []string) error {
 			return keysInit()
 		},
 	}
@@ -42,8 +45,7 @@ func init() {
 }
 
 func CertTemplate() (*x509.Certificate, error) {
-	// generate a random serial number (a real cert authority would have some logic behind this)
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128) //nolint:mnd // random 16 byte serial number
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	if err != nil {
 		return nil, fmt.Errorf("failed to generate serial number [%w]", err)
@@ -53,13 +55,13 @@ func CertTemplate() (*x509.Certificate, error) {
 		SerialNumber:          serialNumber,
 		Subject:               pkix.Name{CommonName: "kas"},
 		NotBefore:             time.Now(),
-		NotAfter:              time.Now().Add(time.Hour * 24 * 30 * 365), // a year or so
+		NotAfter:              time.Now().Add(time.Hour * 24 * 30 * 365), //nolint:mnd // About a year to expire
 		BasicConstraintsValid: true,
 	}
 	return &tmpl, nil
 }
 
-func storeKeyPair(pub, priv any, private_file, public_file string) error {
+func storeKeyPair(priv, pub any, privateFile, publicFile string) error {
 	privateBytes, err := x509.MarshalPKCS8PrivateKey(priv)
 	if err != nil {
 		return fmt.Errorf("unable to marshal private key [%w]", err)
@@ -70,7 +72,7 @@ func storeKeyPair(pub, priv any, private_file, public_file string) error {
 			Bytes: privateBytes,
 		},
 	)
-	if err := os.WriteFile(private_file, keyPEM, 0600); err != nil {
+	if err := os.WriteFile(privateFile, keyPEM, 0o400); err != nil {
 		return fmt.Errorf("unable to store key [%w]", err)
 	}
 
@@ -95,21 +97,63 @@ func storeKeyPair(pub, priv any, private_file, public_file string) error {
 		},
 	)
 
-	if err := os.WriteFile(public_file, pubPEM, 0666); err != nil {
+	if err := os.WriteFile(publicFile, pubPEM, 0o400); err != nil {
 		return fmt.Errorf("unable to store rsa public key [%w]", err)
 	}
 	return nil
 }
 
+func storeTo(priv, pub jwk.Set, k interface{ Public() crypto.PublicKey }, kid string) error {
+	privJWK, err := jwk.FromRaw(k)
+	if err != nil {
+		return fmt.Errorf("unable to convert private key [%s]: [%w]", kid, err)
+	}
+	if err := privJWK.Set("kid", kid); err != nil {
+		return fmt.Errorf("unable to set kid [%s]: [%w]", kid, err)
+	}
+	if err := priv.AddKey(privJWK); err != nil {
+		return fmt.Errorf("unable to store private key [%s]: [%w]", kid, err)
+	}
+
+	pubJWK, err := jwk.FromRaw(k.Public())
+	if err != nil {
+		return fmt.Errorf("unable to convert public key [%s]: [%w]", kid, err)
+	}
+	if err := pubJWK.Set("kid", kid); err != nil {
+		return fmt.Errorf("unable to set public key kid [%s]: [%w]", kid, err)
+	}
+	if err := pub.AddKey(pubJWK); err != nil {
+		return fmt.Errorf("unable to store public key [%s]: [%w]", kid, err)
+	}
+	return nil
+}
+
+func storeJSON(f string, o any, perm os.FileMode) error {
+	s, err := json.Marshal(o)
+	if err != nil {
+		return fmt.Errorf("unable to marshal key to write to file [%s]: [%w]", f, err)
+	}
+	if err := os.WriteFile(f, s, perm); err != nil {
+		return fmt.Errorf("unable to store key to file [%s]: [%w]", f, err)
+	}
+	return nil
+}
+
 func keysInit() error {
+	jwksPriv := jwk.NewSet()
+	jwksPub := jwk.NewSet()
+
 	// openssl req -x509 -nodes -newkey RSA:2048
 	//  -subj "/CN=kas" -keyout "$opt_output/kas-private.pem" -out "$opt_output/kas-cert.pem" -days 365
 	// Generate RSA key.
-	keyRSA, err := rsa.GenerateKey(rand.Reader, 2048)
+	keyRSA, err := rsa.GenerateKey(rand.Reader, 2048) //nolint:mnd // 256 byte key
 	if err != nil {
 		return fmt.Errorf("unable to generate rsa key [%w]", err)
 	}
-	if err := storeKeyPair(keyRSA.Public(), keyRSA, output+"/kas-private.pem", output+"/kas-cert.pem"); err != nil {
+	if err := storeKeyPair(keyRSA, keyRSA.Public(), output+"/kas-private.pem", output+"/kas-cert.pem"); err != nil {
+		return err
+	}
+	if err := storeTo(jwksPriv, jwksPub, keyRSA, "r1"); err != nil {
 		return err
 	}
 
@@ -117,9 +161,20 @@ func keysInit() error {
 	// openssl req -x509 -nodes -newkey ec:ecparams.tmp -subj "/CN=kas" -keyout "$opt_output/kas-ec-private.pem" -out "$opt_output/kas-ec-cert.pem" -days 365
 	keyEC, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
-		return fmt.Errorf("failed to generate new ECDSA private key: %s\n", err)
+		return fmt.Errorf("failed to generate ECDSA private key [%w]", err)
 	}
-	if err := storeKeyPair(keyEC.Public(), keyEC, output+"/kas-ec-private.pem", output+"/kas-ec-cert.pem"); err != nil {
+	if err := storeKeyPair(keyEC, keyEC.Public(), output+"/kas-ec-private.pem", output+"/kas-ec-cert.pem"); err != nil {
+		return err
+	}
+	if err := storeTo(jwksPriv, jwksPub, keyEC, "e1"); err != nil {
+		return err
+	}
+
+	// Store jwk sets kas-public-jwk-set.json and kas-private-jwk-set.json
+	if err := storeJSON(output+"/kas-public-jwk-set.json", jwksPub, 0o640); err != nil { //nolint:mnd // u+rw,g+r
+		return err
+	}
+	if err := storeJSON(output+"/kas-private-jwk-set.json", jwksPriv, 0o600); err != nil { //nolint:mnd // u+rw
 		return err
 	}
 

5ad3a97d3c7c05b02d2b34ed0ec6e4aada1fa13d
Tyler Biscoe <biscoe@virtru.com>
2024-08-12 13:49:23 -0400

    manifest

--- sdk/manifest.go
+++ sdk/manifest.go
@@ -20,7 +20,6 @@ type IntegrityInformation struct {
 }
 
 type KeyAccess struct {
-
 	KeyType           string      `json:"type"`
 	KasURL            string      `json:"url"`
 	Protocol          string      `json:"protocol"`

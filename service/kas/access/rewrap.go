d8b1cc2d191a3c39cac513b77fa6a549e83ec133
Tyler Biscoe <biscoe@virtru.com>
2024-08-12 14:39:50 -0400

    revert rewrap - not sure why this got out of sync

--- service/kas/access/rewrap.go
+++ service/kas/access/rewrap.go
@@ -351,10 +351,7 @@ func (p *Provider) tdf3Rewrap(ctx context.Context, body *RequestBody, entity *en
 	// Audit the TDF3 Rewrap
 	kasPolicy := ConvertToAuditKasPolicy(*policy)
 
-	policyBinding, err := extractPolicyBinding(body.KeyAccess.PolicyBinding)
-	if err != nil {
-		return nil, err
-	}
+	policyBinding, _ := extractPolicyBinding(body.KeyAccess.PolicyBinding)
 
 	auditEventParams := audit.RewrapAuditEventParams{
 		Policy:        kasPolicy,

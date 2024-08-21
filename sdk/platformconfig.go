e6e76bf24a2e587817582ccb113d9e78a92b4060
Jake Van Vorhis <83739412+jakedoublev@users.noreply.github.com>
2024-08-21 12:03:11 -0700

    fix(sdk): well-known warning logs and public client id error (#1415)
    Resolves #1414

--- sdk/platformconfig.go
+++ sdk/platformconfig.go
@@ -1,13 +1,8 @@
 package sdk
 
-import (
-	"log/slog"
-)
-
 func (c PlatformConfiguration) getIdpConfig() map[string]interface{} {
 	idpCfg, err := c["idp"].(map[string]interface{})
 	if !err {
-		slog.Warn("idp configuration not found in well-known configuration")
 		idpCfg = map[string]interface{}{}
 	}
 	return idpCfg
@@ -17,7 +12,6 @@ func (c PlatformConfiguration) Issuer() (string, error) {
 	idpCfg := c.getIdpConfig()
 	value, ok := idpCfg["issuer"].(string)
 	if !ok {
-		slog.Warn("issuer not found in well-known idp configuration")
 		return "", ErrPlatformIssuerNotFound
 	}
 	return value, nil
@@ -27,7 +21,6 @@ func (c PlatformConfiguration) AuthzEndpoint() (string, error) {
 	idpCfg := c.getIdpConfig()
 	value, ok := idpCfg["authorization_endpoint"].(string)
 	if !ok {
-		slog.Warn("authorization_endpoint not found in well-known idp configuration")
 		return "", ErrPlatformAuthzEndpointNotFound
 	}
 	return value, nil
@@ -37,7 +30,6 @@ func (c PlatformConfiguration) TokenEndpoint() (string, error) {
 	idpCfg := c.getIdpConfig()
 	value, ok := idpCfg["token_endpoint"].(string)
 	if !ok {
-		slog.Warn("token_endpoint not found in well-known idp configuration")
 		return "", ErrPlatformTokenEndpointNotFound
 	}
 	return value, nil
@@ -47,8 +39,7 @@ func (c PlatformConfiguration) PublicClientID() (string, error) {
 	idpCfg := c.getIdpConfig()
 	value, ok := idpCfg["public_client_id"].(string)
 	if !ok {
-		slog.Warn("public_client_id not found in well-known idp configuration")
-		return "", ErrPlatformTokenEndpointNotFound
+		return "", ErrPlatformPublicClientIDNotFound
 	}
 	return value, nil
 }

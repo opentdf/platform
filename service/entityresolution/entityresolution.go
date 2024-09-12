075302b77ae208e2025f4eb67ce8fbfa85345c41
Krish Suchak <suchak.krish@gmail.com>
2024-09-12 17:13:08 -0400

    convert entity resolution service

--- service/entityresolution/entityresolution.go
+++ service/entityresolution/entityresolution.go
@@ -2,10 +2,12 @@ package entityresolution
 
 import (
 	"context"
+	"net/http"
 
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"connectrpc.com/connect"
 	"github.com/mitchellh/mapstructure"
 	"github.com/opentdf/platform/protocol/go/entityresolution"
+	"github.com/opentdf/platform/protocol/go/entityresolution/entityresolutionconnect"
 	keycloak "github.com/opentdf/platform/service/entityresolution/keycloak"
 	"github.com/opentdf/platform/service/logger"
 	"github.com/opentdf/platform/service/pkg/serviceregistry"
@@ -29,20 +31,21 @@ func NewRegistration() serviceregistry.Registration {
 			}
 
 			srp.Logger.Debug("entity_resolution configuration", "config", inputIdpConfig)
-
-			return &EntityResolutionService{idpConfig: inputIdpConfig, logger: srp.Logger}, func(ctx context.Context, mux *runtime.ServeMux, server any) error {
-				return entityresolution.RegisterEntityResolutionServiceHandlerServer(ctx, mux, server.(entityresolution.EntityResolutionServiceServer)) //nolint:forcetypeassert // allow type assert, following other services
+			es := &EntityResolutionService{idpConfig: inputIdpConfig, logger: srp.Logger}
+			return es, func(ctx context.Context, mux *http.ServeMux, server any) {
+				path, handler := entityresolutionconnect.NewEntityResolutionServiceHandler(es)
+				mux.Handle(path, handler)
 			}
 		},
 	}
 }
 
-func (s EntityResolutionService) ResolveEntities(ctx context.Context, req *entityresolution.ResolveEntitiesRequest) (*entityresolution.ResolveEntitiesResponse, error) {
-	resp, err := keycloak.EntityResolution(ctx, req, s.idpConfig, s.logger)
-	return &resp, err
+func (s EntityResolutionService) ResolveEntities(ctx context.Context, req *connect.Request[entityresolution.ResolveEntitiesRequest]) (*connect.Response[entityresolution.ResolveEntitiesResponse], error) {
+	resp, err := keycloak.EntityResolution(ctx, req.Msg, s.idpConfig, s.logger)
+	return &connect.Response[entityresolution.ResolveEntitiesResponse]{Msg: &resp}, err
 }
 
-func (s EntityResolutionService) CreateEntityChainFromJwt(ctx context.Context, req *entityresolution.CreateEntityChainFromJwtRequest) (*entityresolution.CreateEntityChainFromJwtResponse, error) {
-	resp, err := keycloak.CreateEntityChainFromJwt(ctx, req, s.idpConfig, s.logger)
-	return &resp, err
+func (s EntityResolutionService) CreateEntityChainFromJwt(ctx context.Context, req *connect.Request[entityresolution.CreateEntityChainFromJwtRequest]) (*connect.Response[entityresolution.CreateEntityChainFromJwtResponse], error) {
+	resp, err := keycloak.CreateEntityChainFromJwt(ctx, req.Msg, s.idpConfig, s.logger)
+	return &connect.Response[entityresolution.CreateEntityChainFromJwtResponse]{Msg: &resp}, err
 }

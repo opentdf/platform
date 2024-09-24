98c16655b28fd80a887853106458e85e73e9b75a
davidbkirk <david.kirk@virtru.com>
2024-09-24 12:49:28 -0400

    updated len(operands) error messages

--- service/kas/access/accessPdp.go
+++ service/kas/access/accessPdp.go
@@ -82,7 +82,7 @@ func checkTemporalConditions(ctx context.Context, attributes []string, logger lo
 		switch operator {
 		case "after": // temporal/value/after::2024-11-05T12:00:00Z
 			if len(operands) != oneOperand {
-				return false, fmt.Errorf("invalid operands for 'after'")
+				return false, fmt.Errorf("Invaild number of operands; 'before' operator expects one operand, %d received", len(operands))
 			}
 
 			afterTime, err := time.Parse(layout, operands[0])
@@ -97,7 +97,7 @@ func checkTemporalConditions(ctx context.Context, attributes []string, logger lo
 
 		case "before": // temporal/value/before::2024-11-05T12:00:00Z
 			if len(operands) != oneOperand {
-				return false, fmt.Errorf("invalid operands for 'before'")
+				return false, fmt.Errorf("Invaild number of operands; 'before' operator expects one operand, %d received", len(operands))
 			}
 			beforeTime, err := time.Parse(layout, operands[0])
 			if err != nil {
@@ -110,7 +110,7 @@ func checkTemporalConditions(ctx context.Context, attributes []string, logger lo
 			}
 		case "duration": // temporal/value/duration::2024-11-05T12:00:00Z::3600 (3600 seconds = 1 hour duration)
 			if len(operands) != twoOperands {
-				return false, fmt.Errorf("invalid operands for 'duration'")
+				return false, fmt.Errorf("Invaild number of operands;'duration' operator expects two operands, %d received", len(operands))
 			}
 
 			startTime, err := time.Parse(layout, operands[0])
@@ -130,7 +130,7 @@ func checkTemporalConditions(ctx context.Context, attributes []string, logger lo
 			}
 		case "between": // temporal/value/between::2024-11-04T12:00:00Z::2024-11-05T12:00:00Z
 			if len(operands) != twoOperands {
-				return false, fmt.Errorf("invalid operands for 'between'")
+				return false, fmt.Errorf("Invaild number of operands;'between' operator expects two operands, %d received", len(operands))
 			}
 			startTime, err := time.Parse(layout, operands[0])
 			if err != nil {
@@ -148,7 +148,7 @@ func checkTemporalConditions(ctx context.Context, attributes []string, logger lo
 			}
 
 		default:
-			return false, fmt.Errorf("unknown operator: %s", operator)
+			return false, fmt.Errorf("unknown temporal operator: %s", operator)
 		}
 	}
 	// Conditions satisfied, access granted

9e9eb418455d5762e244cd7896984b362a1e5353
jakedoublev <jake.vanvorhis@virtru.com>
2024-10-14 12:45:39 -0700

    comments on new column

--- service/policy/db/migrations/20241014000000_add_kas_name_to_registry.sql
+++ service/policy/db/migrations/20241014000000_add_kas_name_to_registry.sql
@@ -4,10 +4,14 @@
 ALTER TABLE IF EXISTS key_access_servers
   ADD COLUMN IF NOT EXISTS name VARCHAR UNIQUE;
 
+COMMENT ON COLUMN key_access_servers.name IS 'Optional common name of the KAS';
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+
+COMMENT ON COLUMN key_access_servers.name IS NULL;
 
 ALTER TABLE IF EXISTS key_access_servers
   DROP COLUMN IF EXISTS name;

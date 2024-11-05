184a733154943abab7fd2a3715dc25b63dfa622e
Ryan Yanulites <ryanulites@virtru.com>
2024-11-05 08:42:13 -0700

    fix(policy): REVERT PR #1663 - SPIKE transactions support (#1719)

--- service/policy/db/policy.go
+++ service/policy/db/policy.go
@@ -1,9 +1,6 @@
 package db
 
 import (
-	"context"
-
-	"github.com/jackc/pgx/v5"
 	"github.com/opentdf/platform/protocol/go/common"
 	"github.com/opentdf/platform/service/logger"
 	"github.com/opentdf/platform/service/pkg/db"
@@ -20,18 +17,6 @@ type PolicyDBClient struct {
 	*db.Client
 	logger *logger.Logger
 	*Queries
-}
-
-func (c *PolicyDBClient) BeginTx(ctx context.Context) (pgx.Tx, error) {
-	tx, err := c.Client.Pgx.Begin(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return tx, nil
-}
-
-func (c *PolicyDBClient) WithTx(tx pgx.Tx) *PolicyDBClient {
-	return &PolicyDBClient{c.Client, c.logger, c.Queries.WithTx(tx)}
 }
 
 func NewClient(c *db.Client, logger *logger.Logger) PolicyDBClient {

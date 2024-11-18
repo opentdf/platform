1661d7f81a08f89d0417e2facf789aa2ac0590ab
Jake Van Vorhis <83739412+jakedoublev@users.noreply.github.com>
2024-11-18 07:28:46 -0800

    Update service/internal/auth/authn.go

--- service/internal/auth/authn.go
+++ service/internal/auth/authn.go
@@ -197,7 +197,7 @@ func normalizeURL(o string, u *url.URL) string {
 	return ou.String()
 }
 
-// deprecated
+// Deprecated: currently still respected within SDK but will be removed
 func (a *Authentication) ExtendAuthzDefaultPolicy(policies [][]string) error {
 	return a.enforcer.ExtendDefaultPolicy(policies)
 }

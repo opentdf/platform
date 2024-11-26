d4b501c66f105a2c90ccc5bfa631b4b063e96f3e
Sean Trantalis <strantalis@virtru.com>
2024-11-26 16:32:09 -0500

    fix: properly set casbin authz policy (#1776)
    ### Proposed Changes
    
    When calling the SetPolicy function it was overwrittng what ever was
    defined within the yaml configuration. This pr addresses this by
    changing the current functionality to overwrite the default policy while
    still allowing the policy in configuration to take precedence.
    
    The pull request also introduces two new fields `UsernameClaim` and
    `GroupsClaim` aligning more with openid based claim names and not
    keycloak realm roles. The authz policy will pull the username and list
    of groups and run them through the casbin enforce function now. A user
    can now scope a policy to a username.
    
    ```
    p, strantalis, policy.attributes.*, read, allow
    ```
    
    ### Checklist
    
    - [ ] I have added or updated unit tests
    - [ ] I have added or updated integration tests (if appropriate)
    - [ ] I have added or updated documentation
    
    ### Testing Instructions

--- service/pkg/server/start.go
+++ service/pkg/server/start.go
@@ -63,6 +63,16 @@ func Start(f ...StartOptions) error {
 		cfg.Server.Auth.PublicRoutes = startConfig.PublicRoutes
 	}
 
+	// Set Default Policy
+	if startConfig.bultinPolicyOverride != "" {
+		cfg.Server.Auth.Policy.Builtin = startConfig.bultinPolicyOverride
+	}
+
+	// Set Casbin Adapter
+	if startConfig.casbinAdapter != nil {
+		cfg.Server.Auth.Policy.Adapter = startConfig.casbinAdapter
+	}
+
 	logger.Debug("config loaded", slog.Any("config", cfg))
 
 	// Create new server for grpc & http. Also will support in process grpc potentially too
@@ -74,34 +84,6 @@ func Start(f ...StartOptions) error {
 		return fmt.Errorf("issue creating opentdf server: %w", err)
 	}
 	defer otdf.Stop()
-
-	// Append the authz policies
-	if len(startConfig.authzDefaultPolicyExtension) > 0 {
-		if otdf.AuthN == nil {
-			err := errors.New("authn not enabled")
-			logger.Error("issue adding authz policies", "error", err)
-			return fmt.Errorf("issue adding authz policies: %w", err)
-		}
-		err := otdf.AuthN.ExtendAuthzDefaultPolicy(startConfig.authzDefaultPolicyExtension)
-		if err != nil {
-			logger.Error("issue adding authz policies", slog.String("error", err.Error()))
-			return fmt.Errorf("issue adding authz policies: %w", err)
-		}
-	}
-
-	// Set the authz policy
-	if startConfig.authzPolicy != "" {
-		if otdf.AuthN == nil {
-			err := errors.New("authn not enabled")
-			logger.Error("issue setting authz policy", slog.String("error", err.Error()))
-			return fmt.Errorf("issue setting authz policy: %w", err)
-		}
-		err := otdf.AuthN.SetAuthzPolicy(startConfig.authzPolicy)
-		if err != nil {
-			logger.Error("issue setting authz policy", slog.String("error", err.Error()))
-			return fmt.Errorf("issue setting authz policy: %w", err)
-		}
-	}
 
 	// Initialize the service registry
 	logger.Debug("initializing service registry")

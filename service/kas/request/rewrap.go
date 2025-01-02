5620d0d84c387c2d063dbcbb737229e3add70b20
Dominic Reed <dominic.reed@virtru.com>
2025-01-02 13:58:36 -0800

    feat(core): bulk rewrap
    go structs of new rewrap API
    
    
    
    
    Summary:
    
    Test Plan:

---
+++ service/kas/request/rewrap.go
@@ -0,0 +1,31 @@
+package request
+
+import "github.com/opentdf/platform/protocol/go/kas"
+
+type PolicyRequest struct {
+	Id string `json:"id"`
+	Body string `json:"body"`
+}
+
+type KeyAccessObjectRequest struct {
+	KeyAccessObjectId string `json:"keyAccessObjectId"`
+	KeyAccess `json:"keyAccessObject"`
+
+	// For Platform Use
+	Processed bool `json:"-"`
+	SymmetricKey []byte `json:"-"`
+}
+
+type RewrapRequests struct {
+	KeyAccessObjectRequests []*KeyAccessObjectRequest `json:"keyAccessObjects"`
+	Policy PolicyRequest  `json:"policy"`
+	Algorithm       string    `json:"algorithm,omitempty"`
+
+	// For Platform Use
+	Results *kas.RewrapResult `json:"-"`
+}
+
+type RequestBody struct {
+	Requests []*RewrapRequests `json:"requests"`
+	ClientPublicKey string `json:"ClientPublicKey"`
+}

fa60b4fd935a8e64a1f85ad110ce9e310cd9c6da
David Mihalcik <dmihalcik@virtru.com>
2025-01-08 09:55:40 -0500

    feat(core): Expose version info
    - Adds sdk.Version and sdk.TDFSpecVersion strings for library integrators to inspect
    - Adds `version` subcommand to platform service
    - The values should be updated automatically with release please
       - Note this means the version of main will be pinned to the last release

---
+++ sdk/version.go
@@ -0,0 +1,6 @@
+package sdk
+
+const (
+	Version        = "0.3.23" // SDK version // x-release-please-version
+	TDFSpecVersion = "4.2.2"  // Vesion of TDF Spec currently targeted by the SDK
+)
---
+++ service/cmd/version.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+const Version = "0.4.36" // Service Version // x-release-please-version
+
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Platform version information",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			cmd.Println(Version)
+			return nil
+		},
+	})
+}

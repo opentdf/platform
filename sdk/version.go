51591fd65988c9186ab1dc93d5eb3a0e39799ef4
David Mihalcik <dmihalcik@virtru.com>
2025-01-17 11:44:18 -0500

    Update version.go

--- sdk/version.go
+++ sdk/version.go
@@ -2,5 +2,5 @@ package sdk
 
 const (
 	Version        = "0.3.25" // SDK version // x-release-please-version
-	TDFSpecVersion = "4.2.2"  // Vesion of TDF Spec currently targeted by the SDK
+	TDFSpecVersion = "4.3.0"  // Vesion of TDF Spec currently targeted by the SDK
 )

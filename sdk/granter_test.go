64bc0d13d517366ce1eefa620433d020f4c6a6b0
David Mihalcik <dmihalcik@virtru.com>
2025-03-13 15:08:37 -0400

    linter fixes

--- sdk/granter_test.go
+++ sdk/granter_test.go
@@ -3,7 +3,9 @@ package sdk
 import (
 	"context"
 	"fmt"
+	"maps"
 	"regexp"
+	"slices"
 	"strings"
 	"testing"
 
@@ -11,7 +13,6 @@ import (
 	"github.com/opentdf/platform/protocol/go/policy/attributes"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"golang.org/x/exp/maps"
 	"google.golang.org/grpc"
 )
 
@@ -380,7 +381,8 @@ func TestConfigurationServicePutGet(t *testing.T) {
 			grants, err := newGranterFromAttributes(newKasKeyCache(), v...)
 			require.NoError(t, err)
 			assert.Len(t, grants.grants, tc.size)
-			assert.Subset(t, policyToStringKeys(tc.policy), maps.Keys(grants.grants))
+			foundGrantNames := slices.AppendSeq(make([]string, 0, len(grants.grants)), maps.Keys(grants.grants))
+			assert.Subset(t, policyToStringKeys(tc.policy), foundGrantNames)
 			actualKases := make(map[string]bool)
 			for _, g := range grants.grants {
 				require.NotNil(t, g)
@@ -388,7 +390,8 @@ func TestConfigurationServicePutGet(t *testing.T) {
 					actualKases[k] = true
 				}
 			}
-			assert.ElementsMatch(t, tc.kases, maps.Keys(actualKases))
+			foundKasNames := slices.AppendSeq(make([]string, 0, len(actualKases)), maps.Keys(actualKases))
+			assert.ElementsMatch(t, tc.kases, foundKasNames)
 		})
 	}
 }

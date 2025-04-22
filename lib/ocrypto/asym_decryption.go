19bdae2db08b201e9033479ad5e16a9c262fca89
David Mihalcik <dmihalcik@virtru.com>
2025-04-22 14:00:58 -0400

    chore(core): Introduce new hal interfaces
    Introduces SecurityProvider and KeyLookup interfaces
    
    cleanups
    
    context support
    
    consolidate
    
    consolidate and syntax error free
    
    nanofix
    
    fixes
    
    fixes
    
    some renames and consolidation
    
    more light rename
    
    removed stray direct usage of unwrapped key
    
    nonbreaking changes to ocrypto
    
    lint fixes
    
    Update rewrap.go
    
    fix error tyep
    
    cleanups
    
    export interface
    
    don't re-export ocrypto
    
    work toward lookup first?
    
    Update kas.go
    
    starting work to expose injectable trust stores
    
    rename trust.KeyDetails.Mode to System()

--- lib/ocrypto/asym_decryption.go
+++ lib/ocrypto/asym_decryption.go
@@ -123,6 +123,9 @@ func NewECDecryptor(sk *ecdh.PrivateKey) (ECDecryptor, error) {
 
 	return ECDecryptor{sk, salt, nil}, nil
 }
+func NewSaltedECDecryptor(sk *ecdh.PrivateKey, salt, info []byte) (ECDecryptor, error) {
+	return ECDecryptor{sk, salt, info}, nil
+}
 
 func NewSaltedECDecryptor(sk *ecdh.PrivateKey, salt, info []byte) (ECDecryptor, error) {
 	return ECDecryptor{sk, salt, info}, nil

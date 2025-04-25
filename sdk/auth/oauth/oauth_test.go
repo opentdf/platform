8daabdb8c3bc0d22676083ad295d807d50b3148e
Elizabeth Healy <ehealy@virtru.com>
2025-04-25 13:25:39 -0400

    generate new ca, new jks, sign certs

--- sdk/auth/oauth/oauth_test.go
+++ sdk/auth/oauth/oauth_test.go
@@ -67,7 +67,7 @@ func (s *OAuthSuite) TearDownSuite() {
 	_ = s.keycloakContainer.Terminate(context.Background())
 }
 
-//go:embed testdata/keycloak-ca.pem
+//go:embed testdata/new-ca.crt
 var ca []byte
 
 func (s *OAuthSuite) TestCertExchangeFromKeycloak() {
@@ -75,7 +75,7 @@ func (s *OAuthSuite) TestCertExchangeFromKeycloak() {
 		ClientID:   "opentdf-sdk",
 		ClientAuth: "secret",
 	}
-	cert, err := tls.LoadX509KeyPair("testdata/sampleuser.crt", "testdata/sampleuser.key")
+	cert, err := tls.LoadX509KeyPair("testdata/new-sampleuser.crt", "testdata/sampleuser.key")
 	rootCAs, _ := x509.SystemCertPool()
 	rootCAs.AppendCertsFromPEM(ca)
 	s.Require().NoError(err)
@@ -551,8 +551,8 @@ func setupKeycloak(ctx context.Context, t *testing.T) (tc.Container, string, str
 			"--spi-truststore-file-hostname-verification-policy=ANY",
 		},
 		Files: []tc.ContainerFile{
-			{HostFilePath: "testdata/ca.jks", ContainerFilePath: "/truststore/truststore.jks", FileMode: int64(0o777)},
-			{HostFilePath: "testdata/localhost.crt", ContainerFilePath: "/etc/x509/tls/localhost.crt", FileMode: int64(0o777)},
+			{HostFilePath: "testdata/new-ca.jks", ContainerFilePath: "/truststore/truststore.jks", FileMode: int64(0o777)},
+			{HostFilePath: "testdata/new-localhost.crt", ContainerFilePath: "/etc/x509/tls/localhost.crt", FileMode: int64(0o777)},
 			{HostFilePath: "testdata/localhost.key", ContainerFilePath: "/etc/x509/tls/localhost.key", FileMode: int64(0o777)},
 		},
 		Env: map[string]string{

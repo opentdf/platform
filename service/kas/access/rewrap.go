aab15af78d160d6ab3c5943b6c807bc4dd19aab1
Ryan Schumacher <jschumacher@virtru.com>
2025-05-07 13:44:30 -0500

    Fix lint again

--- service/kas/access/rewrap.go
+++ service/kas/access/rewrap.go
@@ -908,6 +908,7 @@ func extractNanoPolicy(symmetricKey trust.ProtectedKey, header sdk.NanoTDFHeader
 			return nil, fmt.Errorf("error unmarshalling encrypted policy: %w", err)
 		}
 		return &policy, nil
+	case sdk.NanoTDFPolicyModeRemote, sdk.NanoTDFPolicyModeEncryptedPolicyKeyAccess:
 	default:
 		return nil, errors.Join(fmt.Errorf("unsupported policy mode: %d", header.PolicyMode), ErrNanoTDFPolicyModeUnsupported)
 	}

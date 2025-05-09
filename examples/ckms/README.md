d0f49bbd857065bb917cbae5fa09953673019d0a
David Mihalcik <dmihalcik@virtru.com>
2025-05-09 08:48:00 -0400

    WIP CKMS EXAMPLE(s)

---
+++ examples/ckms/README.md
@@ -0,0 +1,74 @@
+# Sample CKMS Integration with OpenBao
+
+This folder includes an example CKMS extension for OpenTDF.
+It uses OpenBao to store KAS asymmetric key data,
+provided via a `trust.KeyService` plugin.
+
+## Starting Vault
+
+Start up vault, configured to run in dev mode with local storage.
+
+```sh
+vault server -dev -dev-root-token-id root -dev-tls
+```
+
+Copy the configuration details somewhere.
+Copy and paste the environment variable configuration into a new shell.
+
+```sh
+export VAULT_ADDR='https://127.0.0.1:8200'
+export VAULT_CACERT='/var/folders/fb/.../T/vault-.../vault-ca.pem'
+```
+
+Validate vault is running, and log in
+
+```sh
+vault status
+echo root | vault login -
+```
+
+Let's create some roles, policies, and tokens that apply them:
+
+```sh
+vault secrets enable -path=secrets kv-v2
+
+vault policy write kas-admin ./examples/ckms/vault/policy-admin.hcl
+vault policy write kas-service ./examples/ckms/vault/policy-service.hcl
+vault policy write kas-viewer ./examples/ckms/vault/policy-viewer.hcl
+
+vault token create -policy="kas-admin" -policy="kas-viewer"
+# Use this token to create and delete KAS keys
+vault login [token from above]
+vault kv put secrets/rsa_keys/r1 private="$(cat kas-private.pem | base64)" public="$(cat kas-cert.pem)"
+
+
+# Use this role to from within KAS
+vault auth enable approle
+vault write auth/approle/role/kas policies="kas-service,kas-viewer"
+vault read auth/approle/role/kas/role-id
+vault write -f auth/approle/role/kas/secret-id
+## Use the role_id and secret_id from the above outputs to create a token with this:
+vault write auth/approle/login role_id=<ROLE_ID> secret_id=<SECRET_ID>
+
+```
+
+vault write auth/approle/login role_id=aa034a00-04c3-edd4-a9e2-e5f28ae420b5 secret_id=e2ea9f69-5e08-b381-d186-9a24e07e2e56
+
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain /path/to/your/certificate.crt
+
+
+
+### Start platform services with sample CKMS plugin
+
+Run the example
+
+```sh
+go run examples/ckms
+```
+
+#### 
+#### Add key based configuration using a new KAS key in the CKMS
+
+
+### Encrypt something
+
---
+++ examples/ckms/vault/index.go
@@ -0,0 +1,68 @@
+package vault
+
+import (
+	"context"
+	"fmt"
+
+	vault "github.com/hashicorp/vault/api"
+	auth "github.com/hashicorp/vault/api/auth/approle"
+)
+
+// Fetches a key-value secret (kv-v2) after authenticating via AppRole.
+func GetSecretWithAppRole() (string, error) {
+	config := vault.DefaultConfig() // modify for more granular configuration
+
+	client, err := vault.NewClient(config)
+	if err != nil {
+		return "", fmt.Errorf("unable to initialize Vault client: %w", err)
+	}
+
+	// A combination of a Role ID and Secret ID is required to log in to Vault
+	// with an AppRole.
+	// First, let's get the role ID given to us by our Vault administrator.
+	// roleID := os.Getenv("APPROLE_ROLE_ID")
+	// if roleID == "" {
+	// 	return "", fmt.Errorf("no role ID was provided in APPROLE_ROLE_ID env var")
+	// }
+
+	// // The Secret ID is a value that needs to be protected, so instead of the
+	// // app having knowledge of the secret ID directly, we have a trusted orchestrator (https://learn.hashicorp.com/tutorials/vault/secure-introduction?in=vault/app-integration#trusted-orchestrator)
+	// // give the app access to a short-lived response-wrapping token (https://developer.hashicorp.com/vault/docs/concepts/response-wrapping).
+	// // Read more at: https://learn.hashicorp.com/tutorials/vault/approle-best-practices?in=vault/auth-methods#secretid-delivery-best-practices
+	// secretID := &auth.SecretID{FromFile: "path/to/wrapping-token"}
+
+	roleID := "aa034a00-04c3-edd4-a9e2-e5f28ae420b5"
+	secretID := &auth.SecretID{FromString: "e2ea9f69-5e08-b381-d186-9a24e07e2e56"}
+
+	appRoleAuth, err := auth.NewAppRoleAuth(
+		roleID,
+		secretID,
+		auth.WithWrappingToken(), // Only required if the secret ID is response-wrapped.
+	)
+	if err != nil {
+		return "", fmt.Errorf("unable to initialize AppRole auth method: %w", err)
+	}
+
+	authInfo, err := client.Auth().Login(context.Background(), appRoleAuth)
+	if err != nil {
+		return "", fmt.Errorf("unable to login to AppRole auth method: %w", err)
+	}
+	if authInfo == nil {
+		return "", fmt.Errorf("no auth info was returned after login")
+	}
+
+	// get secret from the default mount path for KV v2 in dev mode, "secret"
+	secret, err := client.KVv2("secret").Get(context.Background(), "creds")
+	if err != nil {
+		return "", fmt.Errorf("unable to read secret: %w", err)
+	}
+
+	// data map can contain more than one key-value pair,
+	// in this case we're just grabbing one of them
+	value, ok := secret.Data["password"].(string)
+	if !ok {
+		return "", fmt.Errorf("value type assertion failed: %T %#v", secret.Data["password"], secret.Data["password"])
+	}
+
+	return value, nil
+}

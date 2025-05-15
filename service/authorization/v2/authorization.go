d0c7ba57e433795ead7a53b9f6357f5e8be69d91
jakedoublev <jake.vanvorhis@virtru.com>
2025-05-15 15:33:04 -0700

    rollup tests and implementation fixes

--- service/authorization/v2/authorization.go
+++ service/authorization/v2/authorization.go
@@ -7,16 +7,11 @@ import (
 	"log/slog"
 
 	"connectrpc.com/connect"
-	"github.com/go-viper/mapstructure/v2"
-	"github.com/opentdf/platform/protocol/go/authorization"
 	authzV2 "github.com/opentdf/platform/protocol/go/authorization/v2"
-	authzConnect "github.com/opentdf/platform/protocol/go/authorization/v2/authorizationv2connect"
-	ers "github.com/opentdf/platform/protocol/go/entityresolution"
-	"github.com/opentdf/platform/protocol/go/policy"
+	authzV2Connect "github.com/opentdf/platform/protocol/go/authorization/v2/authorizationv2connect"
 	otdf "github.com/opentdf/platform/sdk"
 	access "github.com/opentdf/platform/service/internal/access/v2"
 	"github.com/opentdf/platform/service/logger"
-	"github.com/opentdf/platform/service/pkg/config"
 	"github.com/opentdf/platform/service/pkg/serviceregistry"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
@@ -37,30 +32,15 @@ type AuthorizationService struct { //nolint:revive // AuthorizationService is a 
 
 type Config struct{}
 
-func OnConfigUpdate(as *AuthorizationService) serviceregistry.OnConfigUpdateHook {
-	return func(_ context.Context, cfg config.ServiceConfig) error {
-		err := mapstructure.Decode(cfg, as.config)
-		if err != nil {
-			return fmt.Errorf("invalid auth svc cfg [%v] %w", cfg, err)
-		}
-
-		as.logger.Info("authorization service config reloaded")
-
-		return nil
-	}
-}
-
-func NewRegistration() *serviceregistry.Service[authzConnect.AuthorizationServiceHandler] {
+func NewRegistration() *serviceregistry.Service[authzV2Connect.AuthorizationServiceHandler] {
 	as := new(AuthorizationService)
-	onUpdateConfig := OnConfigUpdate(as)
 
-	return &serviceregistry.Service[authzConnect.AuthorizationServiceHandler]{
-		ServiceOptions: serviceregistry.ServiceOptions[authzConnect.AuthorizationServiceHandler]{
+	return &serviceregistry.Service[authzV2Connect.AuthorizationServiceHandler]{
+		ServiceOptions: serviceregistry.ServiceOptions[authzV2Connect.AuthorizationServiceHandler]{
 			Namespace:      "authorization",
 			ServiceDesc:    &authzV2.AuthorizationService_ServiceDesc,
-			ConnectRPCFunc: authzConnect.NewAuthorizationServiceHandler,
-			OnConfigUpdate: onUpdateConfig,
-			RegisterFunc: func(srp serviceregistry.RegistrationParams) (authzConnect.AuthorizationServiceHandler, serviceregistry.HandlerServer) {
+			ConnectRPCFunc: authzV2Connect.NewAuthorizationServiceHandler,
+			RegisterFunc: func(srp serviceregistry.RegistrationParams) (authzV2Connect.AuthorizationServiceHandler, serviceregistry.HandlerServer) {
 				authZCfg := new(Config)
 
 				logger := srp.Logger
@@ -71,18 +51,6 @@ func NewRegistration() *serviceregistry.Service[authzConnect.AuthorizationServic
 				if err := srp.RegisterReadinessCheck("authorization", as.IsReady); err != nil {
 					logger.Error("failed to register authorization readiness check", slog.String("error", err.Error()))
 				}
-
-				// Read in config defaults only on first register
-				// if err := defaults.Set(authZCfg); err != nil {
-				// 	panic(fmt.Errorf("failed to set defaults for authorization service config: %w", err))
-				// }
-
-				// Only decode config if it exists
-				// if srp.Config != nil {
-				// 	if err := mapstructure.Decode(srp.Config, &authZCfg); err != nil {
-				// 		panic(fmt.Errorf("invalid auth svc cfg [%v] %w", srp.Config, err))
-				// 	}
-				// }
 
 				as.config = authZCfg
 				as.Tracer = srp.Tracer
@@ -111,14 +79,8 @@ func (as *AuthorizationService) GetEntitlements(ctx context.Context, req *connec
 	propagator := otel.GetTextMapPropagator()
 	ctx = propagator.Extract(ctx, propagation.HeaderCarrier(req.Header()))
 
-	entityChain := req.Msg.GetEntityChain()
+	entityIdentifier := req.Msg.GetEntityIdentifier()
 	withComprehensiveHierarchy := req.Msg.GetWithComprehensiveHierarchy()
-
-	// TODO: this should be moved to proto validation https://github.com/opentdf/platform/issues/1057
-	if entityChain == nil {
-		as.logger.ErrorContext(ctx, "requires an entity chain but was nil")
-		return nil, connect.NewError(connect.CodeInvalidArgument, errors.New("requires an entity chain"))
-	}
 
 	// When authorization service can consume cached policy, switch to the other PDP (process based on policy passed in)
 	pdp, err := access.NewJustInTimePDP(ctx, as.logger, as.sdk)
@@ -127,7 +89,7 @@ func (as *AuthorizationService) GetEntitlements(ctx context.Context, req *connec
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
 
-	entitlements, err := pdp.GetEntitlements(ctx, entityChain, withComprehensiveHierarchy)
+	entitlements, err := pdp.GetEntitlements(ctx, entityIdentifier, withComprehensiveHierarchy)
 	if err != nil {
 		// TODO: any bad request errors that aren't 500s?
 		as.logger.ErrorContext(ctx, "failed to get entitlements", slog.String("error", err.Error()))
@@ -135,45 +97,6 @@ func (as *AuthorizationService) GetEntitlements(ctx context.Context, req *connec
 	}
 
 	rsp := &authzV2.GetEntitlementsResponse{
-		Entitlements: entitlements,
-	}
-
-	return connect.NewResponse(rsp), nil
-}
-
-// GetEntitlementsByToken for an entity represented by an access token
-func (as *AuthorizationService) GetEntitlementsByToken(ctx context.Context, req *connect.Request[authzV2.GetEntitlementsByTokenRequest]) (*connect.Response[authzV2.GetEntitlementsByTokenResponse], error) {
-	as.logger.DebugContext(ctx, "getting entitlements by token")
-
-	ctx, span := as.Tracer.Start(ctx, "GetEntitlementsByToken")
-	defer span.End()
-
-	// Extract trace context from the incoming request
-	propagator := otel.GetTextMapPropagator()
-	ctx = propagator.Extract(ctx, propagation.HeaderCarrier(req.Header()))
-
-	token := req.Msg.GetToken()
-	entityChain, err := as.getOneEntityChainFromToken(ctx, token)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to get an entity chain from JWT", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-
-	withComprehensiveHierarchy := req.Msg.GetWithComprehensiveHierarchy()
-
-	pdp, err := access.NewJustInTimePDP(ctx, as.logger, as.sdk)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to create JIT PDP", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-
-	entitlements, err := pdp.GetEntitlements(ctx, entityChain, withComprehensiveHierarchy)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to get entitlements", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-
-	rsp := &authzV2.GetEntitlementsByTokenResponse{
 		Entitlements: entitlements,
 	}
 
@@ -197,38 +120,21 @@ func (as *AuthorizationService) GetDecision(ctx context.Context, req *connect.Re
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
 	request := req.Msg
-	entityChain := request.GetEntity()
+	entityIdentifier := request.GetEntityIdentifier()
 	action := request.GetAction()
 	resource := request.GetResource()
 
-	decisions, permitted, err := pdp.GetDecision(ctx, entityChain, action, []*authzV2.Resource{resource})
+	decisions, permitted, err := pdp.GetDecision(ctx, entityIdentifier, action, []*authzV2.Resource{resource})
 	if err != nil {
 		// TODO: any bad request errors that aren't 500s?
 		as.logger.ErrorContext(ctx, "failed to get decision", slog.String("error", err.Error()))
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
-	if len(decisions) == 0 {
-		as.logger.ErrorContext(ctx, "no decisions returned")
-		return nil, connect.NewError(connect.CodeInternal, errors.New("no decisions returned"))
+	resp, err := rollupSingleResourceDecision(permitted, decisions)
+	if err != nil {
+		as.logger.ErrorContext(ctx, "failed to rollup single resource decision", slog.String("error", err.Error()))
+		return nil, connect.NewError(connect.CodeInternal, err)
 	}
-	decision := decisions[0]
-	if len(decision.Results) == 0 {
-		as.logger.ErrorContext(ctx, "no decision results returned")
-		return nil, connect.NewError(connect.CodeInternal, errors.New("no decision results returned"))
-	}
-	result := decision.Results[0]
-	access := authzV2.Decision_DECISION_DENY
-	if permitted {
-		access = authzV2.Decision_DECISION_PERMIT
-	}
-
-	resp := &authzV2.GetDecisionResponse{
-		Decision: &authzV2.ResourceDecision{
-			Decision:            access,
-			EphemeralResourceId: result.ResourceID,
-		},
-	}
-
 	return connect.NewResponse(resp), nil
 }
 
@@ -249,18 +155,18 @@ func (as *AuthorizationService) GetDecisionMultiResource(ctx context.Context, re
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
 	request := req.Msg
-	ec := request.GetEntity()
+	entityIdentifier := request.GetEntityIdentifier()
 	action := request.GetAction()
 	resources := request.GetResources()
 
-	decisions, allPermitted, err := pdp.GetDecision(ctx, ec, action, resources)
+	decisions, allPermitted, err := pdp.GetDecision(ctx, entityIdentifier, action, resources)
 	if err != nil {
 		// TODO: any bad request errors that aren't 500s?
 		as.logger.ErrorContext(ctx, "failed to get decision", slog.String("error", err.Error()))
 		return nil, connect.NewError(connect.CodeInternal, err)
 	}
 
-	resourceDecisions, err := rollupMultiResourceDecision(ctx, decisions)
+	resourceDecisions, err := rollupMultiResourceDecision(decisions)
 	if err != nil {
 		as.logger.ErrorContext(ctx, "failed to rollup multi resource decision", slog.String("error", err.Error()))
 		return nil, err
@@ -276,165 +182,26 @@ func (as *AuthorizationService) GetDecisionMultiResource(ctx context.Context, re
 	return connect.NewResponse(resp), nil
 }
 
-// GetDecisionBulk for an entity represented by an access token and an action on a single resource
-func (as *AuthorizationService) GetDecisionByToken(ctx context.Context, req *connect.Request[authzV2.GetDecisionByTokenRequest]) (*connect.Response[authzV2.GetDecisionByTokenResponse], error) {
-	// Extract trace context from the incoming request
-	propagator := otel.GetTextMapPropagator()
-	ctx = propagator.Extract(ctx, propagation.HeaderCarrier(req.Header()))
-
-	ctx, span := as.Tracer.Start(ctx, "GetDecisionByToken")
-	defer span.End()
-
-	tok := req.Msg.GetToken()
-	entityChain, err := as.getOneEntityChainFromToken(ctx, tok)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to get an entity chain from JWT", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-
-	action := req.Msg.GetAction()
-	resource := req.Msg.GetResource()
-	pdp, err := access.NewJustInTimePDP(ctx, as.logger, as.sdk)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to create JIT PDP", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-
-	decisions, permitted, err := pdp.GetDecision(ctx, entityChain, action, []*authzV2.Resource{resource})
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to get decision", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-	if len(decisions) == 0 {
-		as.logger.ErrorContext(ctx, "no decisions returned")
-		return nil, connect.NewError(connect.CodeInternal, errors.New("no decisions returned"))
-	}
-	decision := decisions[0]
-	if len(decision.Results) == 0 {
-		as.logger.ErrorContext(ctx, "no decision results returned")
-		return nil, connect.NewError(connect.CodeInternal, errors.New("no decision results returned"))
-	}
-
-	result := decision.Results[0]
-	access := authzV2.Decision_DECISION_DENY
-	if permitted {
-		access = authzV2.Decision_DECISION_PERMIT
-	}
-	resp := &authzV2.GetDecisionByTokenResponse{
-		DecisionResponse: &authzV2.GetDecisionResponse{
-			Decision: &authzV2.ResourceDecision{
-				Decision:            access,
-				EphemeralResourceId: result.ResourceID,
-			},
-		},
-	}
-	return connect.NewResponse(resp), nil
-}
-
-// GetDecisionByTokenMultiResource for an entity represented by an access token and an action on multiple resources
-func (as *AuthorizationService) GetDecisionByTokenMultiResource(ctx context.Context, req *connect.Request[authzV2.GetDecisionByTokenMultiResourceRequest]) (*connect.Response[authzV2.GetDecisionByTokenMultiResourceResponse], error) {
-	// Extract trace context from the incoming request
-	propagator := otel.GetTextMapPropagator()
-	ctx = propagator.Extract(ctx, propagation.HeaderCarrier(req.Header()))
-
-	ctx, span := as.Tracer.Start(ctx, "GetDecisionByTokenMultiResource")
-	defer span.End()
-
-	as.logger.DebugContext(ctx, "getting decision by token for multiple resources")
-
-	token := req.Msg.GetToken()
-	entityChain, err := as.getOneEntityChainFromToken(ctx, token)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to get an entity chain from JWT", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-
-	action := req.Msg.GetAction()
-	resources := req.Msg.GetResources()
-
-	// Create policy decision point and get decisions
-	pdp, err := access.NewJustInTimePDP(ctx, as.logger, as.sdk)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to create JIT PDP", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-
-	decisions, allPermitted, err := pdp.GetDecision(ctx, entityChain, action, resources)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to get decision", slog.String("error", err.Error()))
-		return nil, connect.NewError(connect.CodeInternal, err)
-	}
-
-	// Build resource decisions
-	resourceDecisions, err := rollupMultiResourceDecision(ctx, decisions)
-	if err != nil {
-		as.logger.ErrorContext(ctx, "failed to rollup multi resource decision", slog.String("error", err.Error()))
-		return nil, err
-	}
-
-	// Construct and return response
-	resp := &authzV2.GetDecisionByTokenMultiResourceResponse{
-		AllPermitted: &wrapperspb.BoolValue{
-			Value: allPermitted,
-		},
-		ResourceDecisions: resourceDecisions,
-	}
-
-	return connect.NewResponse(resp), nil
-}
-
 // GetDecisionBulk for multiple requests, each comprising a combination of entity chain, action, and one or more resources
 func (as *AuthorizationService) GetDecisionBulk(ctx context.Context, req *connect.Request[authzV2.GetDecisionBulkRequest]) (*connect.Response[authzV2.GetDecisionBulkResponse], error) {
 	return nil, connect.NewError(connect.CodeUnimplemented, errors.New("GetDecisionBulk not implemented"))
 }
 
-// getOneEntityChainFromToken extracts the entity chain from the token
-func (as *AuthorizationService) getOneEntityChainFromToken(ctx context.Context, token *authorization.Token) (*authzV2.EntityChain, error) {
-	convertTokenRequest := &ers.CreateEntityChainFromJwtRequest{Tokens: []*authorization.Token{
-		token,
-	}}
-	entityChainFromJWT, err := as.sdk.EntityResoution.CreateEntityChainFromJwt(ctx, convertTokenRequest)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create entity chain from JWT: %w", err)
-	}
-	if entityChainFromJWT == nil {
-		return nil, errors.New("failed to create entity chain from JWT, nil response")
-	}
-
-	if len(entityChainFromJWT.GetEntityChains()) == 0 {
-		return nil, errors.New("failed to create entity chain from JWT, empty entity chain")
-	}
-
-	chain := entityChainFromJWT.GetEntityChains()[0]
-	if chain == nil {
-		return nil, errors.New("failed to create entity chain from JWT, nil entity chain")
-	}
-	
-	chainV2 := &authzV2.EntityChain{
-		EphemeralChainId: chain.GetId(),
-		Entities:         make([]*authzV2.Entity, 0, len(chain.GetEntities())),
-	}
-	for idx, entity := range chain.GetEntities() {
-		entityV2 := &authzV2.Entity{
-			EphemeralId: entity.GetId(),
-			EntityType:  entity.GetEntityType(),
-			Category:    entity.GetCategory(),
-		}
-		chainV2.Entities[idx] = entityV2
-	}
-
-	return chainV2, nil
-}
-
 // rollupMultiResourceDecision creates a standardized response for multi-resource decisions
 // by processing the decisions returned from the PDP.
 func rollupMultiResourceDecision(
-	ctx context.Context,
 	decisions []*access.Decision,
 ) ([]*authzV2.ResourceDecision, error) {
+	if len(decisions) == 0 {
+		return nil, errors.New("no decisions returned")
+	}
+
 	resourceDecisions := make([]*authzV2.ResourceDecision, 0, len(decisions))
 
 	for idx, decision := range decisions {
+		if decision == nil {
+			return nil, fmt.Errorf("nil decision at index %d", idx)
+		}
 		if len(decision.Results) == 0 {
 			return nil, errors.New("no decision results returned")
 		}
@@ -456,18 +223,20 @@ func rollupMultiResourceDecision(
 // rollupSingleResourceDecision creates a standardized response for a single resource decision
 // by processing the decision returned from the PDP.
 func rollupSingleResourceDecision(
-	ctx context.Context,
-	entityID string,
-	action *policy.Action,
 	permitted bool,
 	decisions []*access.Decision,
 ) (*authzV2.GetDecisionResponse, error) {
 	if len(decisions) == 0 {
-		return nil, connect.NewError(connect.CodeInternal, errors.New("no decisions returned"))
+		return nil, errors.New("no decisions returned")
 	}
+
 	decision := decisions[0]
+	if decision == nil {
+		return nil, errors.New("nil decision at index 0")
+	}
+
 	if len(decision.Results) == 0 {
-		return nil, connect.NewError(connect.CodeInternal, errors.New("no decision results returned"))
+		return nil, errors.New("no decision results returned")
 	}
 
 	result := decision.Results[0]
---
+++ service/authorization/v2/authorization_test.go
@@ -0,0 +1,283 @@
+package authorization
+
+import (
+	"errors"
+	"testing"
+
+	authzV2 "github.com/opentdf/platform/protocol/go/authorization/v2"
+	access "github.com/opentdf/platform/service/internal/access/v2"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRollupSingleResourceDecision(t *testing.T) {
+	tests := []struct {
+		name            string
+		permitted       bool
+		decisions       []*access.Decision
+		expectedResult  *authzV2.GetDecisionResponse
+		expectedError   error
+		errorMsgContain string
+	}{
+		{
+			name:      "should return permit decision when permitted is true",
+			permitted: true,
+			decisions: []*access.Decision{
+				{
+					Access: true,
+					Results: []access.ResourceDecision{
+						{
+							ResourceID: "resource-123",
+						},
+					},
+				},
+			},
+			expectedResult: &authzV2.GetDecisionResponse{
+				Decision: &authzV2.ResourceDecision{
+					Decision:            authzV2.Decision_DECISION_PERMIT,
+					EphemeralResourceId: "resource-123",
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			name:      "should return deny decision when permitted is false",
+			permitted: false,
+			decisions: []*access.Decision{
+				{
+					Access: true, // This is intentionally different from permitted to verify permitted takes precedence
+					Results: []access.ResourceDecision{
+						{
+							ResourceID: "resource-123",
+						},
+					},
+				},
+			},
+			expectedResult: &authzV2.GetDecisionResponse{
+				Decision: &authzV2.ResourceDecision{
+					Decision:            authzV2.Decision_DECISION_DENY,
+					EphemeralResourceId: "resource-123",
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			name:            "should return error when no decisions are provided",
+			permitted:       true,
+			decisions:       []*access.Decision{},
+			expectedResult:  nil,
+			expectedError:   errors.New("no decisions returned"),
+			errorMsgContain: "no decisions returned",
+		},
+		{
+			name:      "should return error when decision has no results",
+			permitted: true,
+			decisions: []*access.Decision{
+				{
+					Access:  true,
+					Results: []access.ResourceDecision{},
+				},
+			},
+			expectedResult:  nil,
+			expectedError:   errors.New("no decision results returned"),
+			errorMsgContain: "no decision results returned",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := rollupSingleResourceDecision(tc.permitted, tc.decisions)
+			
+			if tc.expectedError != nil {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), tc.errorMsgContain)
+				assert.Nil(t, result)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedResult, result)
+			}
+		})
+	}
+}
+
+func TestRollupMultiResourceDecision(t *testing.T) {
+	tests := []struct {
+		name            string
+		decisions       []*access.Decision
+		expectedResult  []*authzV2.ResourceDecision
+		expectedError   error
+		errorMsgContain string
+	}{
+		{
+			name: "should return multiple permit decisions",
+			decisions: []*access.Decision{
+				{
+					Access: true,
+					Results: []access.ResourceDecision{
+						{
+							Passed:     true,
+							ResourceID: "resource-123",
+						},
+					},
+				},
+				{
+					Access: true,
+					Results: []access.ResourceDecision{
+						{
+							Passed:     true,
+							ResourceID: "resource-456",
+						},
+					},
+				},
+			},
+			expectedResult: []*authzV2.ResourceDecision{
+				{
+					Decision:            authzV2.Decision_DECISION_PERMIT,
+					EphemeralResourceId: "resource-123",
+				},
+				{
+					Decision:            authzV2.Decision_DECISION_PERMIT,
+					EphemeralResourceId: "resource-456",
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			name: "should return mix of permit and deny decisions",
+			decisions: []*access.Decision{
+				{
+					Access: true,
+					Results: []access.ResourceDecision{
+						{
+							Passed:     true,
+							ResourceID: "resource-123",
+						},
+					},
+				},
+				{
+					Access: false,
+					Results: []access.ResourceDecision{
+						{
+							Passed:     false,
+							ResourceID: "resource-456",
+						},
+					},
+				},
+			},
+			expectedResult: []*authzV2.ResourceDecision{
+				{
+					Decision:            authzV2.Decision_DECISION_PERMIT,
+					EphemeralResourceId: "resource-123",
+				},
+				{
+					Decision:            authzV2.Decision_DECISION_DENY,
+					EphemeralResourceId: "resource-456",
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			name: "should return error when decision has no results",
+			decisions: []*access.Decision{
+				{
+					Access:  true,
+					Results: []access.ResourceDecision{},
+				},
+			},
+			expectedResult:  nil,
+			expectedError:   errors.New("no decision results returned"),
+			errorMsgContain: "no decision results returned",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := rollupMultiResourceDecision(tc.decisions)
+			
+			if tc.expectedError != nil {
+				assert.Error(t, err)
+				assert.Contains(t, err.Error(), tc.errorMsgContain)
+				assert.Nil(t, result)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tc.expectedResult, result)
+			}
+		})
+	}
+}
+
+func TestRollupMultiResourceDecisionSimple(t *testing.T) {
+	// This test checks the minimal viable structure to pass through rollupMultiResourceDecision
+	decision := &access.Decision{
+		Access: true,
+		Results: []access.ResourceDecision{
+			{
+				ResourceID: "resource-123",
+			},
+		},
+	}
+	
+	decisions := []*access.Decision{decision}
+	
+	result, err := rollupMultiResourceDecision(decisions)
+	
+	assert.NoError(t, err)
+	assert.Len(t, result, 1)
+	assert.Equal(t, "resource-123", result[0].EphemeralResourceId)
+	assert.Equal(t, authzV2.Decision_DECISION_PERMIT, result[0].Decision)
+}
+
+func TestRollupMultiResourceDecisionWithNilChecks(t *testing.T) {
+	t.Run("nil decisions array", func(t *testing.T) {
+		var decisions []*access.Decision = nil
+		_, err := rollupMultiResourceDecision(decisions)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "no decisions returned")
+	})
+	
+	t.Run("nil decision in array", func(t *testing.T) {
+		decisions := []*access.Decision{nil}
+		_, err := rollupMultiResourceDecision(decisions)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "nil decision at index 0")
+	})
+	
+	t.Run("nil Results field", func(t *testing.T) {
+		decisions := []*access.Decision{
+			{
+				Access:  true,
+				Results: nil,
+			},
+		}
+		_, err := rollupMultiResourceDecision(decisions)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "no decision results returned")
+	})
+}
+
+func TestRollupSingleResourceDecisionWithNilChecks(t *testing.T) {
+	t.Run("nil decisions array", func(t *testing.T) {
+		var decisions []*access.Decision = nil
+		_, err := rollupSingleResourceDecision(true, decisions)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "no decisions returned")
+	})
+	
+	t.Run("nil decision in array", func(t *testing.T) {
+		decisions := []*access.Decision{nil}
+		_, err := rollupSingleResourceDecision(true, decisions)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "nil decision at index 0")
+	})
+	
+	t.Run("nil Results field", func(t *testing.T) {
+		decisions := []*access.Decision{
+			{
+				Access:  true,
+				Results: nil,
+			},
+		}
+		_, err := rollupSingleResourceDecision(true, decisions)
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "no decision results returned")
+	})
+}

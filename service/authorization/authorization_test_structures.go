698696c0aaedaa7a767314797422bb0afed46cb3
Elizabeth Healy <ehealy@virtru.com>
2025-05-16 14:29:28 -0400

    linting

--- service/authorization/authorization_test_structures.go
+++ service/authorization/authorization_test_structures.go
@@ -28,10 +28,8 @@ var (
 	mockFqn3                         = fmt.Sprintf("https://%s/attr/%s/value/%s", mockNamespace, mockAttrName, mockAttrValue3)
 )
 
-////// Mock attributes client for testing /////
-
-type myAttributesClient struct {
-}
+// //// Mock attributes client for testing /////
+type myAttributesClient struct{}
 
 func (*myAttributesClient) ListAttributes(_ context.Context, _ *attr.ListAttributesRequest) (*attr.ListAttributesResponse, error) {
 	return &listAttributeResp, errListAttributes
@@ -44,51 +42,67 @@ func (*myAttributesClient) GetAttributeValuesByFqns(_ context.Context, _ *attr.G
 func (*myAttributesClient) ListAttributeValues(_ context.Context, _ *attr.ListAttributeValuesRequest) (*attr.ListAttributeValuesResponse, error) {
 	return &attr.ListAttributeValuesResponse{}, nil
 }
+
 func (*myAttributesClient) GetAttribute(_ context.Context, _ *attr.GetAttributeRequest) (*attr.GetAttributeResponse, error) {
 	return &attr.GetAttributeResponse{}, nil
 }
+
 func (*myAttributesClient) GetAttributeValue(_ context.Context, _ *attr.GetAttributeValueRequest) (*attr.GetAttributeValueResponse, error) {
 	return &attr.GetAttributeValueResponse{}, nil
 }
+
 func (*myAttributesClient) CreateAttribute(_ context.Context, _ *attr.CreateAttributeRequest) (*attr.CreateAttributeResponse, error) {
 	return &attr.CreateAttributeResponse{}, nil
 }
+
 func (*myAttributesClient) UpdateAttribute(_ context.Context, _ *attr.UpdateAttributeRequest) (*attr.UpdateAttributeResponse, error) {
 	return &attr.UpdateAttributeResponse{}, nil
 }
+
 func (*myAttributesClient) DeactivateAttribute(_ context.Context, _ *attr.DeactivateAttributeRequest) (*attr.DeactivateAttributeResponse, error) {
 	return &attr.DeactivateAttributeResponse{}, nil
 }
+
 func (*myAttributesClient) CreateAttributeValue(_ context.Context, _ *attr.CreateAttributeValueRequest) (*attr.CreateAttributeValueResponse, error) {
 	return &attr.CreateAttributeValueResponse{}, nil
 }
+
 func (*myAttributesClient) UpdateAttributeValue(_ context.Context, _ *attr.UpdateAttributeValueRequest) (*attr.UpdateAttributeValueResponse, error) {
 	return &attr.UpdateAttributeValueResponse{}, nil
 }
+
 func (*myAttributesClient) DeactivateAttributeValue(_ context.Context, _ *attr.DeactivateAttributeValueRequest) (*attr.DeactivateAttributeValueResponse, error) {
 	return &attr.DeactivateAttributeValueResponse{}, nil
 }
+
 func (*myAttributesClient) AssignKeyAccessServerToAttribute(_ context.Context, _ *attr.AssignKeyAccessServerToAttributeRequest) (*attr.AssignKeyAccessServerToAttributeResponse, error) {
 	return &attr.AssignKeyAccessServerToAttributeResponse{}, nil
 }
+
 func (*myAttributesClient) RemoveKeyAccessServerFromAttribute(_ context.Context, _ *attr.RemoveKeyAccessServerFromAttributeRequest) (*attr.RemoveKeyAccessServerFromAttributeResponse, error) {
 	return &attr.RemoveKeyAccessServerFromAttributeResponse{}, nil
 }
+
 func (*myAttributesClient) AssignKeyAccessServerToValue(_ context.Context, _ *attr.AssignKeyAccessServerToValueRequest) (*attr.AssignKeyAccessServerToValueResponse, error) {
 	return &attr.AssignKeyAccessServerToValueResponse{}, nil
 }
+
 func (*myAttributesClient) RemoveKeyAccessServerFromValue(_ context.Context, _ *attr.RemoveKeyAccessServerFromValueRequest) (*attr.RemoveKeyAccessServerFromValueResponse, error) {
 	return &attr.RemoveKeyAccessServerFromValueResponse{}, nil
 }
+
 func (*myAttributesClient) AssignPublicKeyToAttribute(_ context.Context, _ *attr.AssignPublicKeyToAttributeRequest) (*attr.AssignPublicKeyToAttributeResponse, error) {
 	return &attr.AssignPublicKeyToAttributeResponse{}, nil
 }
+
 func (*myAttributesClient) RemovePublicKeyFromAttribute(_ context.Context, _ *attr.RemovePublicKeyFromAttributeRequest) (*attr.RemovePublicKeyFromAttributeResponse, error) {
 	return &attr.RemovePublicKeyFromAttributeResponse{}, nil
 }
+
 func (*myAttributesClient) AssignPublicKeyToValue(_ context.Context, _ *attr.AssignPublicKeyToValueRequest) (*attr.AssignPublicKeyToValueResponse, error) {
 	return &attr.AssignPublicKeyToValueResponse{}, nil
 }
+
 func (*myAttributesClient) RemovePublicKeyFromValue(_ context.Context, _ *attr.RemovePublicKeyFromValueRequest) (*attr.RemovePublicKeyFromValueResponse, error) {
 	return &attr.RemovePublicKeyFromValueResponse{}, nil
 }
@@ -113,47 +127,47 @@ func (*mySubjectMappingClient) ListSubjectMappings(_ context.Context, _ *sm.List
 	return &listSubjectMappings, nil
 }
 
-func (*mySubjectMappingClient) MatchSubjectMappings(ctx context.Context, req *sm.MatchSubjectMappingsRequest) (*sm.MatchSubjectMappingsResponse, error) {
+func (*mySubjectMappingClient) MatchSubjectMappings(_ context.Context, _ *sm.MatchSubjectMappingsRequest) (*sm.MatchSubjectMappingsResponse, error) {
 	return &sm.MatchSubjectMappingsResponse{}, nil
 }
 
-func (*mySubjectMappingClient) GetSubjectMapping(ctx context.Context, req *sm.GetSubjectMappingRequest) (*sm.GetSubjectMappingResponse, error) {
+func (*mySubjectMappingClient) GetSubjectMapping(_ context.Context, _ *sm.GetSubjectMappingRequest) (*sm.GetSubjectMappingResponse, error) {
 	return &sm.GetSubjectMappingResponse{}, nil
 }
 
-func (*mySubjectMappingClient) CreateSubjectMapping(ctx context.Context, req *sm.CreateSubjectMappingRequest) (*sm.CreateSubjectMappingResponse, error) {
+func (*mySubjectMappingClient) CreateSubjectMapping(_ context.Context, _ *sm.CreateSubjectMappingRequest) (*sm.CreateSubjectMappingResponse, error) {
 	return &sm.CreateSubjectMappingResponse{}, nil
 }
 
-func (*mySubjectMappingClient) UpdateSubjectMapping(ctx context.Context, req *sm.UpdateSubjectMappingRequest) (*sm.UpdateSubjectMappingResponse, error) {
+func (*mySubjectMappingClient) UpdateSubjectMapping(_ context.Context, _ *sm.UpdateSubjectMappingRequest) (*sm.UpdateSubjectMappingResponse, error) {
 	return &sm.UpdateSubjectMappingResponse{}, nil
 }
 
-func (*mySubjectMappingClient) DeleteSubjectMapping(ctx context.Context, req *sm.DeleteSubjectMappingRequest) (*sm.DeleteSubjectMappingResponse, error) {
+func (*mySubjectMappingClient) DeleteSubjectMapping(_ context.Context, _ *sm.DeleteSubjectMappingRequest) (*sm.DeleteSubjectMappingResponse, error) {
 	return &sm.DeleteSubjectMappingResponse{}, nil
 }
 
-func (*mySubjectMappingClient) ListSubjectConditionSets(ctx context.Context, req *sm.ListSubjectConditionSetsRequest) (*sm.ListSubjectConditionSetsResponse, error) {
+func (*mySubjectMappingClient) ListSubjectConditionSets(_ context.Context, _ *sm.ListSubjectConditionSetsRequest) (*sm.ListSubjectConditionSetsResponse, error) {
 	return &sm.ListSubjectConditionSetsResponse{}, nil
 }
 
-func (*mySubjectMappingClient) GetSubjectConditionSet(ctx context.Context, req *sm.GetSubjectConditionSetRequest) (*sm.GetSubjectConditionSetResponse, error) {
+func (*mySubjectMappingClient) GetSubjectConditionSet(_ context.Context, _ *sm.GetSubjectConditionSetRequest) (*sm.GetSubjectConditionSetResponse, error) {
 	return &sm.GetSubjectConditionSetResponse{}, nil
 }
 
-func (*mySubjectMappingClient) CreateSubjectConditionSet(ctx context.Context, req *sm.CreateSubjectConditionSetRequest) (*sm.CreateSubjectConditionSetResponse, error) {
+func (*mySubjectMappingClient) CreateSubjectConditionSet(_ context.Context, _ *sm.CreateSubjectConditionSetRequest) (*sm.CreateSubjectConditionSetResponse, error) {
 	return &sm.CreateSubjectConditionSetResponse{}, nil
 }
 
-func (*mySubjectMappingClient) UpdateSubjectConditionSet(ctx context.Context, req *sm.UpdateSubjectConditionSetRequest) (*sm.UpdateSubjectConditionSetResponse, error) {
+func (*mySubjectMappingClient) UpdateSubjectConditionSet(_ context.Context, _ *sm.UpdateSubjectConditionSetRequest) (*sm.UpdateSubjectConditionSetResponse, error) {
 	return &sm.UpdateSubjectConditionSetResponse{}, nil
 }
 
-func (*mySubjectMappingClient) DeleteSubjectConditionSet(ctx context.Context, req *sm.DeleteSubjectConditionSetRequest) (*sm.DeleteSubjectConditionSetResponse, error) {
+func (*mySubjectMappingClient) DeleteSubjectConditionSet(_ context.Context, _ *sm.DeleteSubjectConditionSetRequest) (*sm.DeleteSubjectConditionSetResponse, error) {
 	return &sm.DeleteSubjectConditionSetResponse{}, nil
 }
 
-func (*mySubjectMappingClient) DeleteAllUnmappedSubjectConditionSets(ctx context.Context, req *sm.DeleteAllUnmappedSubjectConditionSetsRequest) (*sm.DeleteAllUnmappedSubjectConditionSetsResponse, error) {
+func (*mySubjectMappingClient) DeleteAllUnmappedSubjectConditionSets(_ context.Context, _ *sm.DeleteAllUnmappedSubjectConditionSetsRequest) (*sm.DeleteAllUnmappedSubjectConditionSetsResponse, error) {
 	return &sm.DeleteAllUnmappedSubjectConditionSetsResponse{}, nil
 }
 
@@ -182,47 +196,47 @@ func (*paginatedMockSubjectMappingClient) ListSubjectMappings(_ context.Context,
 	return &listSubjectMappings, nil
 }
 
-func (*paginatedMockSubjectMappingClient) MatchSubjectMappings(ctx context.Context, req *sm.MatchSubjectMappingsRequest) (*sm.MatchSubjectMappingsResponse, error) {
+func (*paginatedMockSubjectMappingClient) MatchSubjectMappings(_ context.Context, _ *sm.MatchSubjectMappingsRequest) (*sm.MatchSubjectMappingsResponse, error) {
 	return &sm.MatchSubjectMappingsResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) GetSubjectMapping(ctx context.Context, req *sm.GetSubjectMappingRequest) (*sm.GetSubjectMappingResponse, error) {
+func (*paginatedMockSubjectMappingClient) GetSubjectMapping(_ context.Context, _ *sm.GetSubjectMappingRequest) (*sm.GetSubjectMappingResponse, error) {
 	return &sm.GetSubjectMappingResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) CreateSubjectMapping(ctx context.Context, req *sm.CreateSubjectMappingRequest) (*sm.CreateSubjectMappingResponse, error) {
+func (*paginatedMockSubjectMappingClient) CreateSubjectMapping(_ context.Context, _ *sm.CreateSubjectMappingRequest) (*sm.CreateSubjectMappingResponse, error) {
 	return &sm.CreateSubjectMappingResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) UpdateSubjectMapping(ctx context.Context, req *sm.UpdateSubjectMappingRequest) (*sm.UpdateSubjectMappingResponse, error) {
+func (*paginatedMockSubjectMappingClient) UpdateSubjectMapping(_ context.Context, _ *sm.UpdateSubjectMappingRequest) (*sm.UpdateSubjectMappingResponse, error) {
 	return &sm.UpdateSubjectMappingResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) DeleteSubjectMapping(ctx context.Context, req *sm.DeleteSubjectMappingRequest) (*sm.DeleteSubjectMappingResponse, error) {
+func (*paginatedMockSubjectMappingClient) DeleteSubjectMapping(_ context.Context, _ *sm.DeleteSubjectMappingRequest) (*sm.DeleteSubjectMappingResponse, error) {
 	return &sm.DeleteSubjectMappingResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) ListSubjectConditionSets(ctx context.Context, req *sm.ListSubjectConditionSetsRequest) (*sm.ListSubjectConditionSetsResponse, error) {
+func (*paginatedMockSubjectMappingClient) ListSubjectConditionSets(_ context.Context, _ *sm.ListSubjectConditionSetsRequest) (*sm.ListSubjectConditionSetsResponse, error) {
 	return &sm.ListSubjectConditionSetsResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) GetSubjectConditionSet(ctx context.Context, req *sm.GetSubjectConditionSetRequest) (*sm.GetSubjectConditionSetResponse, error) {
+func (*paginatedMockSubjectMappingClient) GetSubjectConditionSet(_ context.Context, _ *sm.GetSubjectConditionSetRequest) (*sm.GetSubjectConditionSetResponse, error) {
 	return &sm.GetSubjectConditionSetResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) CreateSubjectConditionSet(ctx context.Context, req *sm.CreateSubjectConditionSetRequest) (*sm.CreateSubjectConditionSetResponse, error) {
+func (*paginatedMockSubjectMappingClient) CreateSubjectConditionSet(_ context.Context, _ *sm.CreateSubjectConditionSetRequest) (*sm.CreateSubjectConditionSetResponse, error) {
 	return &sm.CreateSubjectConditionSetResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) UpdateSubjectConditionSet(ctx context.Context, req *sm.UpdateSubjectConditionSetRequest) (*sm.UpdateSubjectConditionSetResponse, error) {
+func (*paginatedMockSubjectMappingClient) UpdateSubjectConditionSet(_ context.Context, _ *sm.UpdateSubjectConditionSetRequest) (*sm.UpdateSubjectConditionSetResponse, error) {
 	return &sm.UpdateSubjectConditionSetResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) DeleteSubjectConditionSet(ctx context.Context, req *sm.DeleteSubjectConditionSetRequest) (*sm.DeleteSubjectConditionSetResponse, error) {
+func (*paginatedMockSubjectMappingClient) DeleteSubjectConditionSet(_ context.Context, _ *sm.DeleteSubjectConditionSetRequest) (*sm.DeleteSubjectConditionSetResponse, error) {
 	return &sm.DeleteSubjectConditionSetResponse{}, nil
 }
 
-func (*paginatedMockSubjectMappingClient) DeleteAllUnmappedSubjectConditionSets(ctx context.Context, req *sm.DeleteAllUnmappedSubjectConditionSetsRequest) (*sm.DeleteAllUnmappedSubjectConditionSetsResponse, error) {
+func (*paginatedMockSubjectMappingClient) DeleteAllUnmappedSubjectConditionSets(_ context.Context, _ *sm.DeleteAllUnmappedSubjectConditionSetsRequest) (*sm.DeleteAllUnmappedSubjectConditionSetsResponse, error) {
 	return &sm.DeleteAllUnmappedSubjectConditionSetsResponse{}, nil
 }
 
@@ -258,51 +272,67 @@ func (*paginatedMockAttributesClient) GetAttributeValuesByFqns(_ context.Context
 func (*paginatedMockAttributesClient) ListAttributeValues(_ context.Context, _ *attr.ListAttributeValuesRequest) (*attr.ListAttributeValuesResponse, error) {
 	return &attr.ListAttributeValuesResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) GetAttribute(_ context.Context, _ *attr.GetAttributeRequest) (*attr.GetAttributeResponse, error) {
 	return &attr.GetAttributeResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) GetAttributeValue(_ context.Context, _ *attr.GetAttributeValueRequest) (*attr.GetAttributeValueResponse, error) {
 	return &attr.GetAttributeValueResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) CreateAttribute(_ context.Context, _ *attr.CreateAttributeRequest) (*attr.CreateAttributeResponse, error) {
 	return &attr.CreateAttributeResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) UpdateAttribute(_ context.Context, _ *attr.UpdateAttributeRequest) (*attr.UpdateAttributeResponse, error) {
 	return &attr.UpdateAttributeResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) DeactivateAttribute(_ context.Context, _ *attr.DeactivateAttributeRequest) (*attr.DeactivateAttributeResponse, error) {
 	return &attr.DeactivateAttributeResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) CreateAttributeValue(_ context.Context, _ *attr.CreateAttributeValueRequest) (*attr.CreateAttributeValueResponse, error) {
 	return &attr.CreateAttributeValueResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) UpdateAttributeValue(_ context.Context, _ *attr.UpdateAttributeValueRequest) (*attr.UpdateAttributeValueResponse, error) {
 	return &attr.UpdateAttributeValueResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) DeactivateAttributeValue(_ context.Context, _ *attr.DeactivateAttributeValueRequest) (*attr.DeactivateAttributeValueResponse, error) {
 	return &attr.DeactivateAttributeValueResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) AssignKeyAccessServerToAttribute(_ context.Context, _ *attr.AssignKeyAccessServerToAttributeRequest) (*attr.AssignKeyAccessServerToAttributeResponse, error) {
 	return &attr.AssignKeyAccessServerToAttributeResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) RemoveKeyAccessServerFromAttribute(_ context.Context, _ *attr.RemoveKeyAccessServerFromAttributeRequest) (*attr.RemoveKeyAccessServerFromAttributeResponse, error) {
 	return &attr.RemoveKeyAccessServerFromAttributeResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) AssignKeyAccessServerToValue(_ context.Context, _ *attr.AssignKeyAccessServerToValueRequest) (*attr.AssignKeyAccessServerToValueResponse, error) {
 	return &attr.AssignKeyAccessServerToValueResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) RemoveKeyAccessServerFromValue(_ context.Context, _ *attr.RemoveKeyAccessServerFromValueRequest) (*attr.RemoveKeyAccessServerFromValueResponse, error) {
 	return &attr.RemoveKeyAccessServerFromValueResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) AssignPublicKeyToAttribute(_ context.Context, _ *attr.AssignPublicKeyToAttributeRequest) (*attr.AssignPublicKeyToAttributeResponse, error) {
 	return &attr.AssignPublicKeyToAttributeResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) RemovePublicKeyFromAttribute(_ context.Context, _ *attr.RemovePublicKeyFromAttributeRequest) (*attr.RemovePublicKeyFromAttributeResponse, error) {
 	return &attr.RemovePublicKeyFromAttributeResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) AssignPublicKeyToValue(_ context.Context, _ *attr.AssignPublicKeyToValueRequest) (*attr.AssignPublicKeyToValueResponse, error) {
 	return &attr.AssignPublicKeyToValueResponse{}, nil
 }
+
 func (*paginatedMockAttributesClient) RemovePublicKeyFromValue(_ context.Context, _ *attr.RemovePublicKeyFromValueRequest) (*attr.RemovePublicKeyFromValueResponse, error) {
 	return &attr.RemovePublicKeyFromValueResponse{}, nil
 }

16236b885bce99aec2a86969d7cd90d51b1987ad
Dave Mihalcik <dmihalcik@virtru.com>
2025-05-28 17:04:10 -0400

    Update service/policy/db/grant_mappings.go

--- service/policy/db/grant_mappings.go
+++ service/policy/db/grant_mappings.go
@@ -44,7 +44,7 @@ func mapKasKeysToGrants(keys []*policy.KasKey, existingGrants []*policy.KeyAcces
 		kasURI := key.GetKasUri()
 		if kasURI == "" {
 			// Skip keys without a URI, as it's essential for mapping
-			l.Debug("skipping key without URI: %s", key.GetKey().GetKeyId())
+			l.Debug("skipping key without UR", "kid", key.GetKey().GetKeyId())
 			continue
 		}
 

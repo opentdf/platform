0ec5720c8eec1595a6014c6020ab42a64c17f232
Ryan Schumacher <j.r.schumacher@gmail.com>
2025-06-04 18:30:41 +0000

    More tweaks

--- service/policy/db/migrations/20240618000000_add_delete_cascade.sql
+++ service/policy/db/migrations/20240618000000_add_delete_cascade.sql
@@ -141,7 +141,8 @@ ON DELETE CASCADE;
 -- Do not cascade deletion of Attribute Definitions when their parent Namespace is deleted
 ALTER TABLE attribute_definitions
 DROP CONSTRAINT IF EXISTS attribute_definitions_namespace_id_fkey_cascades;
-
+ALTER TABLE attribute_definitions
+DROP CONSTRAINT IF EXISTS attribute_definitions_namespace_id_fkey; -- Add this
 ALTER TABLE attribute_definitions
 ADD CONSTRAINT attribute_definitions_namespace_id_fkey
 FOREIGN KEY (namespace_id)
@@ -150,7 +151,8 @@ REFERENCES attribute_namespaces (id);
 -- Do not cascade deletion of Attribute Values when their parent Definition is deleted
 ALTER TABLE attribute_values
 DROP CONSTRAINT IF EXISTS attribute_values_attribute_definition_id_fkey_cascades;
-
+ALTER TABLE attribute_values
+DROP CONSTRAINT IF EXISTS attribute_values_attribute_definition_id_fkey; -- Add this
 ALTER TABLE attribute_values
 ADD CONSTRAINT attribute_values_attribute_definition_id_fkey
 FOREIGN KEY (attribute_definition_id)
@@ -159,7 +161,8 @@ REFERENCES attribute_definitions (id);
 -- Do not cascade deletion of Resource Mappings when their parent Attribute Value is deleted
 ALTER TABLE resource_mappings
 DROP CONSTRAINT IF EXISTS resource_mappings_attribute_value_id_fkey_cascades;
-
+ALTER TABLE resource_mappings
+DROP CONSTRAINT IF EXISTS resource_mappings_attribute_value_id_fkey; -- Add this
 ALTER TABLE resource_mappings
 ADD CONSTRAINT resource_mappings_attribute_value_id_fkey
 FOREIGN KEY (attribute_value_id)
@@ -168,7 +171,8 @@ REFERENCES attribute_values (id);
 -- Do not cascade deletion of Subject Mappings when their parent Attribute Value is deleted
 ALTER TABLE subject_mappings
 DROP CONSTRAINT IF EXISTS subject_mappings_attribute_value_id_fkey_cascades;
-
+ALTER TABLE subject_mappings
+DROP CONSTRAINT IF EXISTS subject_mappings_attribute_value_id_fkey; -- Add this
 ALTER TABLE subject_mappings
 ADD CONSTRAINT subject_mappings_attribute_value_id_fkey
 FOREIGN KEY (attribute_value_id)
@@ -177,7 +181,8 @@ REFERENCES attribute_values (id);
 -- Do not cascade deletion of FQNs when their parent objects are deleted
 ALTER TABLE attribute_fqns
 DROP CONSTRAINT IF EXISTS attribute_fqns_namespace_id_fkey_cascades;
-
+ALTER TABLE attribute_fqns
+DROP CONSTRAINT IF EXISTS attribute_fqns_namespace_id_fkey; -- Add this
 ALTER TABLE attribute_fqns
 ADD CONSTRAINT attribute_fqns_namespace_id_fkey
 FOREIGN KEY (namespace_id)
@@ -185,7 +190,8 @@ REFERENCES attribute_namespaces (id);
 
 ALTER TABLE attribute_fqns
 DROP CONSTRAINT IF EXISTS attribute_fqns_attribute_id_fkey_cascades;
-
+ALTER TABLE attribute_fqns
+DROP CONSTRAINT IF EXISTS attribute_fqns_attribute_id_fkey; -- Add this
 ALTER TABLE attribute_fqns
 ADD CONSTRAINT attribute_fqns_attribute_id_fkey
 FOREIGN KEY (attribute_id)
@@ -193,17 +199,18 @@ REFERENCES attribute_definitions (id);
 
 ALTER TABLE attribute_fqns
 DROP CONSTRAINT IF EXISTS attribute_fqns_value_id_fkey_cascades;
-
+ALTER TABLE attribute_fqns
+DROP CONSTRAINT IF EXISTS attribute_fqns_value_id_fkey; -- Add this
 ALTER TABLE attribute_fqns
 ADD CONSTRAINT attribute_fqns_value_id_fkey
 FOREIGN KEY (value_id)
 REFERENCES attribute_values (id);
 
 -- Do not cascade deletion of KAS Registrations when an associated policy object is deleted
-
 ALTER TABLE attribute_definition_key_access_grants
 DROP CONSTRAINT IF EXISTS attr_def_key_access_gr_attr_def_id_fkey_cascades;
-
+ALTER TABLE attribute_definition_key_access_grants
+DROP CONSTRAINT IF EXISTS attribute_definition_key_access_gr_attribute_definition_id_fkey; -- Add this
 ALTER TABLE attribute_definition_key_access_grants
 ADD CONSTRAINT attribute_definition_key_access_gr_attribute_definition_id_fkey
 FOREIGN KEY (attribute_definition_id)
@@ -211,7 +218,8 @@ REFERENCES attribute_definitions (id);
 
 ALTER TABLE attribute_definition_key_access_grants
 DROP CONSTRAINT IF EXISTS attr_def_key_access_grant_kas_id_fkey_cascades;
-
+ALTER TABLE attribute_definition_key_access_grants
+DROP CONSTRAINT IF EXISTS attribute_definition_key_access_grant_key_access_server_id_fkey; -- Add this
 ALTER TABLE attribute_definition_key_access_grants
 ADD CONSTRAINT attribute_definition_key_access_grant_key_access_server_id_fkey
 FOREIGN KEY (key_access_server_id)
@@ -219,7 +227,8 @@ REFERENCES key_access_servers (id);
 
 ALTER TABLE attribute_value_key_access_grants
 DROP CONSTRAINT IF EXISTS attr_val_key_access_grants_kas_id_fkey_cascades;
-
+ALTER TABLE attribute_value_key_access_grants
+DROP CONSTRAINT IF EXISTS attribute_value_key_access_grants_key_access_server_id_fkey; -- Add this
 ALTER TABLE attribute_value_key_access_grants
 ADD CONSTRAINT attribute_value_key_access_grants_key_access_server_id_fkey
 FOREIGN KEY (key_access_server_id)
@@ -227,7 +236,8 @@ REFERENCES key_access_servers (id);
 
 ALTER TABLE attribute_value_key_access_grants
 DROP CONSTRAINT IF EXISTS attr_val_key_access_grants_attr_val_id_fkey_cascades;
-
+ALTER TABLE attribute_value_key_access_grants
+DROP CONSTRAINT IF EXISTS attribute_value_key_access_grants_attribute_value_id_fkey; -- Add this
 ALTER TABLE attribute_value_key_access_grants
 ADD CONSTRAINT attribute_value_key_access_grants_attribute_value_id_fkey
 FOREIGN KEY (attribute_value_id)
@@ -235,7 +245,8 @@ REFERENCES attribute_values (id);
 
 ALTER TABLE attribute_value_members
 DROP CONSTRAINT IF EXISTS attr_val_members_value_id_fkey_cascades;
-
+ALTER TABLE attribute_value_members
+DROP CONSTRAINT IF EXISTS attribute_value_members_value_id_fkey; -- Add this
 ALTER TABLE attribute_value_members
 ADD CONSTRAINT attribute_value_members_value_id_fkey
 FOREIGN KEY (value_id)
@@ -243,7 +254,8 @@ REFERENCES attribute_values (id);
 
 ALTER TABLE attribute_value_members
 DROP CONSTRAINT IF EXISTS attr_val_members_member_id_fkey_cascades;
-
+ALTER TABLE attribute_value_members
+DROP CONSTRAINT IF EXISTS attribute_value_members_member_id_fkey; -- Add this
 ALTER TABLE attribute_value_members
 ADD CONSTRAINT attribute_value_members_member_id_fkey
 FOREIGN KEY (member_id)

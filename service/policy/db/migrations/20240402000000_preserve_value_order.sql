f2e88074e3bb49332ec20520065096d755c68a67
Ryan Schumacher <j.r.schumacher@gmail.com>
2025-06-04 18:43:14 +0000

    Fix down migrations

--- service/policy/db/migrations/20240402000000_preserve_value_order.sql
+++ service/policy/db/migrations/20240402000000_preserve_value_order.sql
@@ -36,13 +36,21 @@ EXECUTE FUNCTION update_definition_delete_values_order();
 -- +goose Down
 
 -- +goose StatementBegin
+DROP FUNCTION IF EXISTS update_definition_add_values_order();
+-- +goose StatementEnd
 
-DROP FUNCTION update_definition_add_values_order;
-DROP TRIGGER trigger_update_definition_add_values_order;
+-- +goose StatementBegin
+DROP TRIGGER IF EXISTS trigger_update_definition_add_values_order ON attribute_values;
+-- +goose StatementEnd
 
-DROP FUNCTION update_definition_delete_values_order;
-DROP TRIGGER trigger_update_definition_delete_values_order;
+-- +goose StatementBegin
+DROP FUNCTION IF EXISTS update_definition_delete_values_order();
+-- +goose StatementEnd
 
-ALTER TABLE attribute_definitions DROP COLUMN values_order;
+-- +goose StatementBegin
+DROP TRIGGER IF EXISTS trigger_update_definition_delete_values_order ON attribute_values;
+-- +goose StatementEnd
 
+-- +goose StatementBegin
+ALTER TABLE attribute_definitions DROP COLUMN IF EXISTS values_order;
 -- +goose StatementEnd

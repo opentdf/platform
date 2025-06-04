fe5e6f002a136ee1ba3c6713e4d9ebcf6bf71bc4
Ryan Schumacher <j.r.schumacher@gmail.com>
2025-06-04 18:26:53 +0000

    Fix down migrations

--- service/policy/db/migrations/20240618000000_add_delete_cascade.sql
+++ service/policy/db/migrations/20240618000000_add_delete_cascade.sql
@@ -140,7 +140,7 @@ ON DELETE CASCADE;
 
 -- Do not cascade deletion of Attribute Definitions when their parent Namespace is deleted
 ALTER TABLE attribute_definitions
-DROP CONSTRAINT attribute_definitions_namespace_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attribute_definitions_namespace_id_fkey_cascades;
 
 ALTER TABLE attribute_definitions
 ADD CONSTRAINT attribute_definitions_namespace_id_fkey
@@ -149,7 +149,7 @@ REFERENCES attribute_namespaces (id);
 
 -- Do not cascade deletion of Attribute Values when their parent Definition is deleted
 ALTER TABLE attribute_values
-DROP CONSTRAINT attribute_values_attribute_definition_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attribute_values_attribute_definition_id_fkey_cascades;
 
 ALTER TABLE attribute_values
 ADD CONSTRAINT attribute_values_attribute_definition_id_fkey
@@ -158,7 +158,7 @@ REFERENCES attribute_definitions (id);
 
 -- Do not cascade deletion of Resource Mappings when their parent Attribute Value is deleted
 ALTER TABLE resource_mappings
-DROP CONSTRAINT resource_mappings_attribute_value_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS resource_mappings_attribute_value_id_fkey_cascades;
 
 ALTER TABLE resource_mappings
 ADD CONSTRAINT resource_mappings_attribute_value_id_fkey
@@ -167,7 +167,7 @@ REFERENCES attribute_values (id);
 
 -- Do not cascade deletion of Subject Mappings when their parent Attribute Value is deleted
 ALTER TABLE subject_mappings
-DROP CONSTRAINT subject_mappings_attribute_value_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS subject_mappings_attribute_value_id_fkey_cascades;
 
 ALTER TABLE subject_mappings
 ADD CONSTRAINT subject_mappings_attribute_value_id_fkey
@@ -176,7 +176,7 @@ REFERENCES attribute_values (id);
 
 -- Do not cascade deletion of FQNs when their parent objects are deleted
 ALTER TABLE attribute_fqns
-DROP CONSTRAINT attribute_fqns_namespace_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attribute_fqns_namespace_id_fkey_cascades;
 
 ALTER TABLE attribute_fqns
 ADD CONSTRAINT attribute_fqns_namespace_id_fkey
@@ -184,7 +184,7 @@ FOREIGN KEY (namespace_id)
 REFERENCES attribute_namespaces (id);
 
 ALTER TABLE attribute_fqns
-DROP CONSTRAINT attribute_fqns_attribute_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attribute_fqns_attribute_id_fkey_cascades;
 
 ALTER TABLE attribute_fqns
 ADD CONSTRAINT attribute_fqns_attribute_id_fkey
@@ -192,7 +192,7 @@ FOREIGN KEY (attribute_id)
 REFERENCES attribute_definitions (id);
 
 ALTER TABLE attribute_fqns
-DROP CONSTRAINT attribute_fqns_value_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attribute_fqns_value_id_fkey_cascades;
 
 ALTER TABLE attribute_fqns
 ADD CONSTRAINT attribute_fqns_value_id_fkey
@@ -202,7 +202,7 @@ REFERENCES attribute_values (id);
 -- Do not cascade deletion of KAS Registrations when an associated policy object is deleted
 
 ALTER TABLE attribute_definition_key_access_grants
-DROP CONSTRAINT attr_def_key_access_gr_attr_def_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attr_def_key_access_gr_attr_def_id_fkey_cascades;
 
 ALTER TABLE attribute_definition_key_access_grants
 ADD CONSTRAINT attribute_definition_key_access_gr_attribute_definition_id_fkey
@@ -210,7 +210,7 @@ FOREIGN KEY (attribute_definition_id)
 REFERENCES attribute_definitions (id);
 
 ALTER TABLE attribute_definition_key_access_grants
-DROP CONSTRAINT attr_def_key_access_grant_kas_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attr_def_key_access_grant_kas_id_fkey_cascades;
 
 ALTER TABLE attribute_definition_key_access_grants
 ADD CONSTRAINT attribute_definition_key_access_grant_key_access_server_id_fkey
@@ -218,7 +218,7 @@ FOREIGN KEY (key_access_server_id)
 REFERENCES key_access_servers (id);
 
 ALTER TABLE attribute_value_key_access_grants
-DROP CONSTRAINT attr_val_key_access_grants_kas_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attr_val_key_access_grants_kas_id_fkey_cascades;
 
 ALTER TABLE attribute_value_key_access_grants
 ADD CONSTRAINT attribute_value_key_access_grants_key_access_server_id_fkey
@@ -226,7 +226,7 @@ FOREIGN KEY (key_access_server_id)
 REFERENCES key_access_servers (id);
 
 ALTER TABLE attribute_value_key_access_grants
-DROP CONSTRAINT attr_val_key_access_grants_attr_val_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attr_val_key_access_grants_attr_val_id_fkey_cascades;
 
 ALTER TABLE attribute_value_key_access_grants
 ADD CONSTRAINT attribute_value_key_access_grants_attribute_value_id_fkey
@@ -234,7 +234,7 @@ FOREIGN KEY (attribute_value_id)
 REFERENCES attribute_values (id);
 
 ALTER TABLE attribute_value_members
-DROP CONSTRAINT attr_val_members_value_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attr_val_members_value_id_fkey_cascades;
 
 ALTER TABLE attribute_value_members
 ADD CONSTRAINT attribute_value_members_value_id_fkey
@@ -242,7 +242,7 @@ FOREIGN KEY (value_id)
 REFERENCES attribute_values (id);
 
 ALTER TABLE attribute_value_members
-DROP CONSTRAINT attr_val_members_member_id_fkey_cascades;
+DROP CONSTRAINT IF EXISTS attr_val_members_member_id_fkey_cascades;
 
 ALTER TABLE attribute_value_members
 ADD CONSTRAINT attribute_value_members_member_id_fkey

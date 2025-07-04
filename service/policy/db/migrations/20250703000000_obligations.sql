de7106f934f61f81cd7b3d82780c261623d07b25
Krish Suchak <suchak.krish@gmail.com>
2025-07-04 13:06:26 -0400

    obligation fulfillers

--- service/policy/db/migrations/20250703000000_obligations.sql
+++ service/policy/db/migrations/20250703000000_obligations.sql
@@ -21,10 +21,40 @@ CREATE TABLE IF NOT EXISTS obligation_values_standard
     UNIQUE (obligation_definition_id, value),
     metadata JSONB
 );
+
+CREATE TABLE IF NOT EXISTS obligation_triggers
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    attribute_value_id UUID NOT NULL REFERENCES attribute_values(id),
+
+    obligation_value_id UUID NOT NULL REFERENCES obligation_values_standard(id),
+    -- obligation_definition_id UUID NOT NULL REFERENCES obligation_definitions(id),
+    -- -- trigger is a JSONB field that can hold any structured data for the trigger
+    -- trigger JSONB NOT NULL,
+    -- -- implicit index on unique obligation_definition_id, trigger
+    -- UNIQUE (obligation_definition_id, trigger),
+    metadata JSONB
+
+)
+
+CREATE TABLE IF NOT EXISTS obligation_fulfillers
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    -- obligation_definition_id UUID NOT NULL REFERENCES obligation_definitions(id),
+    -- -- trigger is a JSONB field that can hold any structured data for the trigger
+    -- trigger JSONB NOT NULL,
+    -- -- implicit index on unique obligation_definition_id, trigger
+    -- UNIQUE (obligation_definition_id, trigger),
+    obligation_value_id UUID NOT NULL REFERENCES obligation_values_standard(id),
+    conditionals JSONB,
+    metadata JSONB
+)
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE IF EXISTS obligation_definitions;
 DROP TABLE IF EXISTS obligation_values_standard;
+DROP TABLE IF EXISTS obligation_triggers;
+DROP TABLE IF EXISTS obligation_fulfillers;
 -- +goose StatementEnd

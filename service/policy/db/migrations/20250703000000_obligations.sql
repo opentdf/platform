6911ac8bc1f8e07ac7202faf69461dd78d63ffeb
Krish Suchak <suchak.krish@gmail.com>
2025-07-03 22:47:21 -0400

    obligation_definitions table

---
+++ service/policy/db/migrations/20250703000000_obligations.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+
+CREATE TABLE IF NOT EXISTS obligation_definitions
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    namespace_id UUID NOT NULL REFERENCES attribute_namespaces(id),
+    -- name is a unique identifier for the obligation within the namespace
+    name VARCHAR NOT NULL,
+    UNIQUE (namespace_id, name),
+    metadata JSONB,
+);
+
+CREATE UNIQUE INDEX comp_key ON obligation_definitions (
+    namespace_id,
+    name
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS obligation_definitions;
+-- +goose StatementEnd

1b88775bb6676edddaaf72921beb05c46c974244
Krish Suchak <suchak.krish@gmail.com>
2025-07-04 13:12:07 -0400

    clean up and add comments

--- service/policy/db/migrations/20250703000000_obligations.sql
+++ service/policy/db/migrations/20250703000000_obligations.sql
@@ -5,9 +5,10 @@ CREATE TABLE IF NOT EXISTS obligation_definitions
 (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     namespace_id UUID NOT NULL REFERENCES attribute_namespaces(id),
-    -- name is a unique identifier for the obligation within the namespace
+    -- name is a unique identifier for the obligation definition within the namespace
     name VARCHAR NOT NULL,
-    -- implicit index on unique namespace_id, name
+    -- implicit index on unique (namespace_id, name) combo
+    -- index name: obligation_definitions_namespace_id_name_key
     UNIQUE (namespace_id, name),
     metadata JSONB
 );
@@ -16,8 +17,10 @@ CREATE TABLE IF NOT EXISTS obligation_values_standard
 (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     obligation_definition_id UUID NOT NULL REFERENCES obligation_definitions(id),
+    -- value is a unique identifier for the obligation value within the definition
     value VARCHAR NOT NULL,
-    -- implicit index on unique obligation_definition_id, value
+    -- implicit index on unique (obligation_definition_id, value) combo
+    -- index name: obligation_values_standard_obligation_definition_id_value_key
     UNIQUE (obligation_definition_id, value),
     metadata JSONB
 );
@@ -40,11 +43,6 @@ CREATE TABLE IF NOT EXISTS obligation_triggers
 CREATE TABLE IF NOT EXISTS obligation_fulfillers
 (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    -- obligation_definition_id UUID NOT NULL REFERENCES obligation_definitions(id),
-    -- -- trigger is a JSONB field that can hold any structured data for the trigger
-    -- trigger JSONB NOT NULL,
-    -- -- implicit index on unique obligation_definition_id, trigger
-    -- UNIQUE (obligation_definition_id, trigger),
     obligation_value_id UUID NOT NULL REFERENCES obligation_values_standard(id),
     conditionals JSONB,
     metadata JSONB

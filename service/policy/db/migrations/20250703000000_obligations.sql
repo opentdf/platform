2f0f40571ac2d01046bfa23c2f75ebc97d6baf92
Krish Suchak <suchak.krish@gmail.com>
2025-07-04 00:05:18 -0400

    implicit index

--- service/policy/db/migrations/20250703000000_obligations.sql
+++ service/policy/db/migrations/20250703000000_obligations.sql
@@ -7,14 +7,17 @@ CREATE TABLE IF NOT EXISTS obligation_definitions
     namespace_id UUID NOT NULL REFERENCES attribute_namespaces(id),
     -- name is a unique identifier for the obligation within the namespace
     name VARCHAR NOT NULL,
+    -- implicit index on unique namespace_id, name
     UNIQUE (namespace_id, name),
-    metadata JSONB,
+    metadata JSONB
 );
 
-CREATE UNIQUE INDEX comp_key ON obligation_definitions (
-    namespace_id,
-    name
-);
+-- CREATE UNIQUE INDEX comp_key ON obligation_definitions (
+--     namespace_id,
+--     name
+-- );
+
+-- obligation_values_standard
 -- +goose StatementEnd
 
 -- +goose Down

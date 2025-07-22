6ce61e07a0d406fb0279063d548a3bed77b397c4
Krish Suchak <suchak.krish@gmail.com>
2025-07-22 16:33:37 -0400

    close fx

--- service/policy/obligations/obligations.go
+++ service/policy/obligations/obligations.go
@@ -11,3 +11,9 @@ type ObligationsService struct { //nolint:revive // ObligationsService is a vali
 	logger   *logger.Logger
 	config   *policyconfig.Config
 }
+
+// Close gracefully shuts down the service, closing the database client.
+func (s *ObligationsService) Close() {
+	s.logger.Info("gracefully shutting down obligations service")
+	s.dbClient.Close()
+}

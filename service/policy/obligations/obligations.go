9e546c1add5d5ebaec2a0d7a10964c23adf35fe4
Krish Suchak <suchak.krish@gmail.com>
2025-07-22 16:35:36 -0400

    isready fx

--- service/policy/obligations/obligations.go
+++ service/policy/obligations/obligations.go
@@ -1,6 +1,8 @@
 package obligations
 
 import (
+	"context"
+
 	"github.com/opentdf/platform/service/logger"
 	policyconfig "github.com/opentdf/platform/service/policy/config"
 	policydb "github.com/opentdf/platform/service/policy/db"
@@ -10,6 +12,17 @@ type ObligationsService struct { //nolint:revive // ObligationsService is a vali
 	dbClient policydb.PolicyDBClient
 	logger   *logger.Logger
 	config   *policyconfig.Config
+}
+
+// IsReady checks if the service is ready to serve requests.
+// Without a database connection, the service is not ready.
+func (s *ObligationsService) IsReady(ctx context.Context) error {
+	s.logger.TraceContext(ctx, "checking readiness of obligations service")
+	if err := s.dbClient.SQLDB.PingContext(ctx); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // Close gracefully shuts down the service, closing the database client.

c885126c830ae23803c1eb27e4e780df5a9d518b
Krish Suchak <suchak.krish@gmail.com>
2025-07-28 16:51:15 -0400

    again

--- service/policy/db/obligations.go
+++ service/policy/db/obligations.go
@@ -1,31 +1,33 @@
 package db
 
 import (
+	"context"
 	"errors"
 
 	"github.com/opentdf/platform/protocol/go/policy"
+	"github.com/opentdf/platform/protocol/go/policy/obligations"
 )
 
 ///
 /// Obligation Definitions
 ///
 
-func (c PolicyDBClient) CreateObligationDefinition() (*policy.Obligation, error) {
+func (c PolicyDBClient) CreateObligationDefinition(ctx context.Context, r *obligations.CreateObligationRequest) (*policy.Obligation, error) {
 	return &policy.Obligation{}, errors.New("CreateObligationDefinition is not implemented in PolicyDBClient")
 }
 
-func (c PolicyDBClient) GetObligationDefinition() (*policy.Obligation, error) {
+func (c PolicyDBClient) GetObligationDefinition(ctx context.Context, r *obligations.GetObligationRequest) (*policy.Obligation, error) {
 	return nil, errors.New("GetObligationDefinition is not implemented in PolicyDBClient")
 }
 
-func (c PolicyDBClient) ListObligationDefinitions() ([]*policy.Obligation, error) {
+func (c PolicyDBClient) ListObligationDefinitions(ctx context.Context, r *obligations.ListObligationsRequest) ([]*policy.Obligation, error) {
 	return nil, errors.New("ListObligationDefinitions is not implemented in PolicyDBClient")
 }
 
-func (c PolicyDBClient) UpdateObligationDefinition() (*policy.Obligation, error) {
+func (c PolicyDBClient) UpdateObligationDefinition(ctx context.Context, r *obligations.UpdateObligationRequest) (*policy.Obligation, error) {
 	return nil, errors.New("UpdateObligationDefinition is not implemented in PolicyDBClient")
 }
 
-func (c PolicyDBClient) DeleteObligationDefinition() (*policy.Obligation, error) {
+func (c PolicyDBClient) DeleteObligationDefinition(ctx context.Context, r *obligations.DeleteObligationRequest) (*policy.Obligation, error) {
 	return nil, errors.New("DeleteObligationDefinition is not implemented in PolicyDBClient")
 }

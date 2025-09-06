73b1d698f66924aedbfcb09c801b5dcedd7ad72a
Krish Suchak <suchak.krish@gmail.com>
2025-09-06 01:23:09 -0400

    proto tests

--- service/policy/obligations/obligations_test.go
+++ service/policy/obligations/obligations_test.go
@@ -1,1 +1,59 @@
 package obligations
+
+import (
+	"testing"
+
+	"buf.build/go/protovalidate"
+	"github.com/google/uuid"
+	"github.com/opentdf/platform/protocol/go/policy/obligations"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	invalidUUID    = "invalid-uuid"
+	invalidFQN     = "invalid-fqn"
+	errMessageUUID = "string.uuid"
+	errMessageURI  = "string.uri"
+)
+
+func getValidator() protovalidate.Validator {
+	v, err := protovalidate.New()
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+func Test_GetObligation_Succeeds(t *testing.T) {
+	validUUID := uuid.NewString()
+	req := &obligations.GetObligationRequest{
+		Id: validUUID,
+	}
+	v := getValidator()
+	err := v.Validate(req)
+	require.NoError(t, err)
+
+	validFQN := "https://namespace.com/obl/drm/value/watermark"
+	req = &obligations.GetObligationRequest{
+		Fqn: validFQN,
+	}
+	err = v.Validate(req)
+	require.NoError(t, err)
+}
+
+func Test_GetObligation_Fails(t *testing.T) {
+	req := &obligations.GetObligationRequest{
+		Id: invalidUUID,
+	}
+	v := getValidator()
+	err := v.Validate(req)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), errMessageUUID)
+
+	req = &obligations.GetObligationRequest{
+		Fqn: invalidFQN,
+	}
+	err = v.Validate(req)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), errMessageURI)
+}

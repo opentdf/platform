d62bbbecce5c0f5f1930f6f06b92c6c2ab5b51b1
jakedoublev <jake.vanvorhis@virtru.com>
2025-10-02 08:13:23 -0700

    put back extraneous SDK client interceptor change

--- sdk/audit/metadata_adding_interceptor.go
+++ sdk/audit/metadata_adding_interceptor.go
@@ -21,26 +21,31 @@ func MetadataAddingClientInterceptor(
 	invoker grpc.UnaryInvoker,
 	opts ...grpc.CallOption,
 ) error {
+	newMetadata := make([]string, 0)
+
 	// Get any existing request ID from context
 	requestID, ok := ctx.Value(RequestIDContextKey).(uuid.UUID)
 	if !ok || requestID == uuid.Nil {
 		requestID = uuid.New()
 	}
-	ctx = metadata.AppendToOutgoingContext(ctx, string(RequestIDHeaderKey), requestID.String())
+	newMetadata = append(newMetadata, string(RequestIDHeaderKey), requestID.String())
 
 	// Add the request IP to a custom header so it is preserved
 	requestIP, isOK := realip.FromContext(ctx)
 	if isOK {
-		ctx = metadata.AppendToOutgoingContext(ctx, string(RequestIPHeaderKey), requestIP.String())
+		newMetadata = append(newMetadata, string(RequestIPHeaderKey), requestIP.String())
 	}
 
 	// Add the actor ID from the request so it is preserved if we need it
 	actorID, isOK := ctx.Value(ActorIDContextKey).(string)
 	if isOK {
-		ctx = metadata.AppendToOutgoingContext(ctx, string(ActorIDHeaderKey), actorID)
+		newMetadata = append(newMetadata, string(ActorIDHeaderKey), actorID)
 	}
 
-	return invoker(ctx, method, req, reply, cc, opts...)
+	newCtx := metadata.AppendToOutgoingContext(ctx, newMetadata...)
+	err := invoker(newCtx, method, req, reply, cc, opts...)
+
+	return err
 }
 
 func MetadataAddingConnectInterceptor() connect.UnaryInterceptorFunc {

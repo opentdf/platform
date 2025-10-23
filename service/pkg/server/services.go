52f174d378ec04b269490dabe5b6643f0b069dce
Scott Hamrick <2623452+cshamrick@users.noreply.github.com>
2025-10-23 14:10:42 -0500

    style: fix format

--- service/pkg/server/services.go
+++ service/pkg/server/services.go
@@ -162,7 +162,7 @@ func startServices(ctx context.Context, params startServicesParams) (func(), err
 		if err == nil {
 			if extractedLogLevel != cfg.Logger.Level {
 				slog.Debug("configuring logger")
-				var newLoggerConfig = cfg.Logger
+				newLoggerConfig := cfg.Logger
 				newLoggerConfig.Level = extractedLogLevel
 				newSvcLogger, err := logging.NewLogger(newLoggerConfig)
 				// only assign if logger successfully created
@@ -190,7 +190,7 @@ func startServices(ctx context.Context, params startServicesParams) (func(), err
 			}
 
 			// Function to create a cache given cache options
-			var createCacheClient = func(options cache.Options) (*cache.Cache, error) {
+			createCacheClient := func(options cache.Options) (*cache.Cache, error) {
 				slog.Info("creating cache client for",
 					slog.String("namespace", ns),
 					slog.String("service", svc.GetServiceDesc().ServiceName),

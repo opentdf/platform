784210fb4eeb34ae530d414b831ca3be68b23f75
Elizabeth Healy <ehealy@virtru.com>
2026-08-04 13:26:42 -0400

    replace print with count

--- service/rttests/rt_test.go
+++ service/rttests/rt_test.go
@@ -273,7 +273,7 @@ func (s *RoundtripSuite) CreateTestData() error {
 		slog.Error("could not list attributes", slog.String("error", err.Error()))
 		return err
 	}
-	slog.Info("list attributes", slog.String("response", protojson.Format(allAttr)))
+	slog.Info("listed attributes", slog.Int("count", len(allAttr.GetAttributes())))
 
 	slog.Info("##################################\n#######################################")
 
@@ -350,10 +350,7 @@ func (s *RoundtripSuite) CreateTestData() error {
 			break
 		}
 	}
-	resp := &subjectmapping.ListSubjectMappingsResponse{
-		SubjectMappings: smList,
-	}
-	slog.InfoContext(ctx, "list all subject mappings", slog.String("subject_mappings", protojson.Format(resp)))
+	slog.InfoContext(ctx, "listed all subject mappings", slog.Int("count", len(smList)))
 
 	return nil
 }

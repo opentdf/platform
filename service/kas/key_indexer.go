a6e6d9773f3ed7c1485c9e31502b601a52c7b2a8
Chris Reed <creed@virtru.com>
2026-09-15 15:09:25 -0500

    fix(kas): clarify rewrap URI logging and indexer context
    Signed-off-by: Chris Reed <creed@virtru.com>

--- service/kas/key_indexer.go
+++ service/kas/key_indexer.go
@@ -39,11 +39,12 @@ type KeyAdapter struct {
 }
 
 func NewPlatformKeyIndexer(sdk *sdk.SDK, kasURI string, l *logger.Logger) *KeyIndexer {
-	return &KeyIndexer{
+	p := &KeyIndexer{
 		sdk:    sdk,
 		kasURI: kasURI,
-		log:    l,
 	}
+	p.log = l.With("key_indexer", p.String())
+	return p
 }
 
 func (p *KeyIndexer) String() string {
@@ -123,7 +124,6 @@ func (p *KeyIndexer) FindKeyWith(ctx context.Context, opts trust.FindKeyOptions)
 
 	p.log.DebugContext(ctx, "finding KAS key",
 		slog.String("kas_uri", req.GetKey().GetUri()),
-		slog.String("requested_kas_uri", opts.KASURI),
 		slog.String("key_id", req.GetKey().GetKid()))
 	resp, err := p.sdk.KeyAccessServerRegistry.GetKey(ctx, req)
 	if err != nil {
@@ -156,7 +156,6 @@ func (p *KeyIndexer) ListKeysWith(ctx context.Context, opts trust.ListKeyOptions
 	}
 	p.log.DebugContext(ctx, "listing KAS keys",
 		slog.String("kas_uri", req.GetKasUri()),
-		slog.String("requested_kas_uri", opts.KASURI),
 		slog.Bool("legacy_only", opts.LegacyOnly))
 	resp, err := p.sdk.KeyAccessServerRegistry.ListKeys(ctx, req)
 	if err != nil {
